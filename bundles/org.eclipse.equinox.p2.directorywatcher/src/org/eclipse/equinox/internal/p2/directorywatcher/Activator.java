1748b3cf16f9422d0efaf793d214247cf81978e1
Jeff McAffer <jeff>
2007-08-30 13:21:14 +0000

    update copyrights etc

--- bundles/org.eclipse.equinox.p2.directorywatcher/src/org/eclipse/equinox/internal/p2/directorywatcher/Activator.java
+++ bundles/org.eclipse.equinox.p2.directorywatcher/src/org/eclipse/equinox/internal/p2/directorywatcher/Activator.java
@@ -4,8 +4,9 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: aQute - initial implementation and ideas IBM Corporation -
- * initial adaptation to Equinox provisioning use
+ * Contributors: 
+ * aQute - initial implementation and ideas 
+ * IBM Corporation - initial adaptation to Equinox provisioning use
  ******************************************************************************/
 package org.eclipse.equinox.internal.p2.directorywatcher;
 
