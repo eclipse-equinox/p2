1a4d2d982914fc928d2155f8ef8e0c9d643678cc
John Arthorne <johna>
2007-09-05 14:53:24 +0000

    fixed malformed javadoc

--- bundles/org.eclipse.equinox.p2.jarprocessor/src/org/eclipse/equinox/p2/jarprocessor/IProcessStep.java
+++ bundles/org.eclipse.equinox.p2.jarprocessor/src/org/eclipse/equinox/p2/jarprocessor/IProcessStep.java
@@ -25,33 +25,32 @@ public interface IProcessStep {
 	 * Return null if this step will not do anything with this entry.
 	 * Return the new entryName if this step will modify this entry on recursion.
 	 * @param entryName
-	 * @return
+	 * @return The new entry name, or <code>null</code>
 	 */
 	public String recursionEffect(String entryName);
 
 	/**
 	 * Perform some processing on the input file before the JarProcessor considers the entries for recursion.
-	 *  return the file containing the result of the processing
 	 * @param input
 	 * @param workingDirectory
 	 * @param containers inf properties for containing jars, innermost jar is first on the list
-	 * @return
+	 * @return the file containing the result of the processing
 	 */
 	public File preProcess(File input, File workingDirectory, List containers);
 
 	/**
-	 * Perform some processing on the input file after the JarProcessor returns from recursion
-	 * return the file containing the result of the processing
+	 * Perform some processing on the input file after the JarProcessor returns from recursion.
+	 *
 	 * @param input
 	 * @param workingDirectory
 	 * @param containers inf properties for containing jars, innermost jar is first on the list
-	 * @return
+	 * @return the file containing the result of the processing
 	 */
 	public File postProcess(File input, File workingDirectory, List containers);
 
 	/**
 	 * Return the name of this process step
-	 * @return
+	 * @return the name of this process step
 	 */
 	public String getStepName();
 
