565153f945fe81cc65163cb17d9cc0f710f637de
John Arthorne <johna>
2007-10-09 15:30:52 +0000

    Removed tests requiring manual setup from automated tests

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/ManualTests.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/ManualTests.java
@@ -4,25 +4,23 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package org.eclipse.equinox.p2.tests.engine;
+package org.eclipse.equinox.p2.tests;
 
 import junit.framework.*;
 
 /**
- * Performs all engine tests.
+ * This is the master test suite for all automated provisioning tests that require some
+ * manual set. These tests can't be run automatically as part of a build.
  */
-public class AllTests extends TestCase {
-
+public class ManualTests extends TestCase {
 	public static Test suite() {
-		TestSuite suite = new TestSuite(AllTests.class.getName());
-		suite.addTestSuite(PhaseSetTest.class);
-		suite.addTestSuite(PhaseTest.class);
-		suite.addTestSuite(ProfileTest.class);
+		TestSuite suite = new TestSuite(AutomatedTests.class.getName());
+		suite.addTestSuite(org.eclipse.equinox.p2.tests.engine.EngineTest.class);
+		suite.addTestSuite(org.eclipse.equinox.p2.tests.full.DirectorTest.class);
 		return suite;
 	}
-
 }
