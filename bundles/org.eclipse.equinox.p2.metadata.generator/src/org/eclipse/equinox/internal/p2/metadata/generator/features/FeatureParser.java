75919e7bf1d09c1afef056efd98fa102b18c31a6
Andrew Niefer <aniefer>
2007-10-17 20:16:47 +0000

    bug 206679

--- bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/p2/metadata/generator/features/FeatureParser.java
+++ bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/p2/metadata/generator/features/FeatureParser.java
@@ -223,7 +223,7 @@ public class FeatureParser extends DefaultHandler {
 	}
 
 	public void endElement(String uri, String localName, String qName) throws SAXException {
-		if (characters != null)
+		if (characters == null)
 			return;
 		if ("description".equals(localName)) { //$NON-NLS-1$
 			result.setDescription(characters.toString().trim());
