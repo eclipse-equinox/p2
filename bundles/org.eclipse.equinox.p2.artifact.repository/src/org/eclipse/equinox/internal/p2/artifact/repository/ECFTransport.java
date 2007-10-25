60c248de13b31c5341fb28bf506e1ec98b80fac5
John Arthorne <johna>
2007-10-25 21:57:11 +0000

    cleaned up progress messages

--- bundles/org.eclipse.equinox.p2.artifact.repository/src/org/eclipse/equinox/internal/p2/artifact/repository/ECFTransport.java
+++ bundles/org.eclipse.equinox.p2.artifact.repository/src/org/eclipse/equinox/internal/p2/artifact/repository/ECFTransport.java
@@ -82,10 +82,9 @@ public class ECFTransport extends Transport {
 				}
 				if (event instanceof IIncomingFileTransferReceiveDataEvent) {
 					IIncomingFileTransfer source = ((IIncomingFileTransferReceiveDataEvent) event).getSource();
-					ID id = source.getID();
 					// TODO do proper monitor things here.
 					if (monitor != null) {
-						monitor.subTask("Transferring " + (id == null ? "" : id.getName()) + " (" + (int) (source.getPercentComplete() * 100) + "% complete)");
+						monitor.subTask("Transferring " + prettyName(source.getID()) + " (" + (int) (source.getPercentComplete() * 100) + "% complete)");
 						if (monitor.isCanceled())
 							source.cancel();
 					}
@@ -97,6 +96,23 @@ public class ECFTransport extends Transport {
 						result.notify();
 					}
 				}
+			}
+
+			/**
+			 * Create a nice, simple name for the given id, suitable for progress messages.
+			 */
+			private String prettyName(ID id) {
+				if (id == null)
+					return ""; //$NON-NLS-1$
+				String simpleName = id.getName();
+				//if the name is not too long, just return it
+				if (simpleName.length() < 30)
+					return simpleName;
+				int lastSlash = simpleName.lastIndexOf('/');
+				//if it is a segmented name, use only the last segment for a cleaner task message
+				if (lastSlash > 0 && lastSlash < (simpleName.length() - 1))
+					simpleName = simpleName.substring(lastSlash + 1);
+				return simpleName;
 			}
 		};
 
