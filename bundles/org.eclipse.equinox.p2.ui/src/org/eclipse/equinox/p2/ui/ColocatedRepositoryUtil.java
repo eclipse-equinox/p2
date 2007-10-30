4fbc2e28980bb28da5666220e585b2a602349e1c
Susan Franklin <sfranklin>
2007-10-30 19:55:14 +0000

    Bug 204209 - [prov] Generated repo directory names don't match what colocated repo UI assumes

--- bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/p2/ui/ColocatedRepositoryUtil.java
+++ bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/p2/ui/ColocatedRepositoryUtil.java
@@ -11,9 +11,7 @@
 
 package org.eclipse.equinox.p2.ui;
 
-import java.net.MalformedURLException;
 import java.net.URL;
-import org.eclipse.core.runtime.Path;
 
 /**
  * Utility methods for manipulating colocated repository URLS
@@ -21,47 +19,18 @@ import org.eclipse.core.runtime.Path;
  * @since 3.4
  */
 public class ColocatedRepositoryUtil {
-	private static final String DEFAULT_ARTIFACTREPO_NAME = "artifactRepository/"; //$NON-NLS-1$
-	private static final String DEFAULT_METADATAREPO_NAME = "metadataRepository/"; //$NON-NLS-1$
 
 	public static URL makeArtifactRepositoryURL(URL url) {
-		String urlSpec = url.toExternalForm();
-		URL newURL;
-		try {
-			if (!urlSpec.endsWith("/")) //$NON-NLS-1$
-				urlSpec += "/"; //$NON-NLS-1$
-			newURL = new URL(urlSpec + DEFAULT_ARTIFACTREPO_NAME);
-		} catch (MalformedURLException e) {
-			return null;
-		}
-		return newURL;
+		return url;
 	}
 
 	public static URL makeMetadataRepositoryURL(URL url) {
-		String urlSpec = url.toExternalForm();
-		URL newURL;
-		try {
-			if (!urlSpec.endsWith("/")) //$NON-NLS-1$
-				urlSpec += "/"; //$NON-NLS-1$
-			newURL = new URL(urlSpec + DEFAULT_METADATAREPO_NAME);
-		} catch (MalformedURLException e) {
-			return null;
-		}
-		return newURL;
+		return url;
 	}
 
 	// A colocated repository URL is one level above the metadata or artifact
 	// repository URL.  For example, foo/servers/ instead of foo/servers/metadataRepository
 	public static URL makeColocatedRepositoryURL(URL metadataURL) {
-		String protocol = metadataURL.getProtocol();
-		String host = metadataURL.getHost();
-		int port = metadataURL.getPort();
-		Path path = new Path(metadataURL.getPath());
-		String pathString = path.removeLastSegments(1).toString();
-		try {
-			return new URL(protocol, host, port, pathString);
-		} catch (MalformedURLException e) {
-			return metadataURL;
-		}
+		return metadataURL;
 	}
 }
