6625391e00ad1bf189ba5c14488ec9b4c42d9419
John Arthorne <johna>
2007-11-07 01:36:24 +0000

    Sort members

--- bundles/org.eclipse.equinox.p2.metadata/src/org/eclipse/equinox/p2/metadata/InstallableUnit.java
+++ bundles/org.eclipse.equinox.p2.metadata/src/org/eclipse/equinox/p2/metadata/InstallableUnit.java
@@ -15,27 +15,27 @@ import org.osgi.framework.Version;
 
 public class InstallableUnit implements IInstallableUnitConstants, IInstallableUnit, InternalInstallableUnit {
 
+	private static final OrderedProperties NO_PROPERTIES = new OrderedProperties();
 	private static final ProvidedCapability[] NO_PROVIDES = new ProvidedCapability[0];
 	private static final RequiredCapability[] NO_REQUIRES = new RequiredCapability[0];
-	private static final OrderedProperties NO_PROPERTIES = new OrderedProperties();
-
-	private String id;
-	private Version version;
-	private boolean singleton;
-
-	private OrderedProperties properties;
-
-	private IArtifactKey[] artifacts;
-	private TouchpointType touchpointType;
-	private ArrayList touchpointData = null;
-
-	private RequiredCapability[] requires;
-
-	private String filter;
 
 	String applicabilityFilter;
+	private IArtifactKey[] artifacts;
+	private String filter;
 
+	private String id;
+
+	private OrderedProperties properties;
 	ProvidedCapability[] providedCapabilities = NO_PROVIDES;
+	private RequiredCapability[] requires;
+
+	private boolean singleton;
+
+	private ArrayList touchpointData = null;
+
+	private TouchpointType touchpointType;
+
+	private Version version;
 
 	public InstallableUnit() {
 		super();
@@ -48,58 +48,14 @@ public class InstallableUnit implements IInstallableUnitConstants, IInstallableU
 		this.singleton = singleton;
 	}
 
-	public TouchpointType getTouchpointType() {
-		return touchpointType != null ? touchpointType : TouchpointType.NONE;
+	public void accept(IMetadataVisitor visitor) {
+		visitor.visitInstallableUnit(this);
 	}
 
-	public String getId() {
-		return id;
-	}
-
-	public String getFilter() {
-		return filter;
-	}
-
-	public Version getVersion() {
-		return version;
-	}
-
-	public IArtifactKey[] getArtifacts() {
-		return artifacts;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public void setVersion(Version newVersion) {
-		this.version = (newVersion != null ? newVersion : Version.emptyVersion);
-	}
-
-	public void setTouchpointType(TouchpointType type) {
-		this.touchpointType = (type != TouchpointType.NONE ? type : null);
-	}
-
-	public void setArtifacts(IArtifactKey[] value) {
-		artifacts = value;
-	}
-
-	public RequiredCapability[] getRequiredCapabilities() {
-		return requires != null ? requires : NO_REQUIRES;
-
-	}
-
-	public void setRequiredCapabilities(RequiredCapability[] capabilities) {
-		if (capabilities == NO_REQUIRES) {
-			this.requires = null;
-		} else {
-			//copy array for safety
-			this.requires = (RequiredCapability[]) capabilities.clone();
-		}
-	}
-
-	public ProvidedCapability[] getProvidedCapabilities() {
-		return (providedCapabilities != null ? providedCapabilities : NO_PROVIDES);
+	public void addProperties(OrderedProperties newProperties) {
+		if (properties == null)
+			properties = new OrderedProperties(newProperties.size());
+		properties.putAll(newProperties);
 	}
 
 	protected void addProvidedCapability(ProvidedCapability capability) {
@@ -113,44 +69,29 @@ public class InstallableUnit implements IInstallableUnitConstants, IInstallableU
 		}
 	}
 
-	public void setCapabilities(ProvidedCapability[] exportedCapabilities) {
-		providedCapabilities = exportedCapabilities;
+	public void addTouchpointData(TouchpointData[] newData) {
+		ensureTouchpointDataCapacity(newData.length);
+		for (int i = 0; i < newData.length; i++) {
+			touchpointData.add(newData[i]);
+		}
 	}
 
-	public void accept(IMetadataVisitor visitor) {
-		visitor.visitInstallableUnit(this);
+	public int compareTo(Object toCompareTo) {
+		if (!(toCompareTo instanceof IInstallableUnit)) {
+			return -1;
+		}
+		IInstallableUnit other = (IInstallableUnit) toCompareTo;
+		if (getId().compareTo(other.getId()) == 0)
+			return (getVersion().compareTo(other.getVersion()));
+		return getId().compareTo(other.getId());
 	}
 
-	public boolean isSingleton() {
-		return singleton;
-	}
-
-	public void setSingleton(boolean singleton) {
-		this.singleton = singleton;
-	}
-
-	public String getProperty(String key) {
-		return getProperties().getProperty(key);
-	}
-
-	public String setProperty(String key, String value) {
-		if (value == null)
-			return (properties != null ? (String) properties.remove(key) : null);
-		if (properties == null)
-			properties = new OrderedProperties();
-		return (String) properties.setProperty(key, value);
-	}
-
-	public String toString() {
-		return id + ' ' + getVersion();
-	}
-
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((getVersion() == null) ? 0 : getVersion().hashCode());
-		return result;
+	private void ensureTouchpointDataCapacity(int size) {
+		if (touchpointData != null) {
+			touchpointData.ensureCapacity(size);
+		} else {
+			touchpointData = new ArrayList(size);
+		}
 	}
 
 	public boolean equals(Object obj) {
@@ -174,39 +115,20 @@ public class InstallableUnit implements IInstallableUnitConstants, IInstallableU
 		return true;
 	}
 
-	public void setFilter(String filter) {
-		this.filter = filter;
+	public String getApplicabilityFilter() {
+		return applicabilityFilter;
 	}
 
-	public TouchpointData[] getTouchpointData() {
-		return (touchpointData == null ? TouchpointData.NO_TOUCHPOINT_DATA //
-				: (TouchpointData[]) touchpointData.toArray(new TouchpointData[touchpointData.size()]));
+	public IArtifactKey[] getArtifacts() {
+		return artifacts;
 	}
 
-	// TODO: resolve the schizophrenia between the singleton immutable data
-	//	   	 and the public returned touchpoint data array.
-	public void setImmutableTouchpointData(TouchpointData immutableData) {
-		ensureTouchpointDataCapacity(4);
-		touchpointData.add(immutableData);
+	public String getFilter() {
+		return filter;
 	}
 
-	public void addTouchpointData(TouchpointData[] newData) {
-		ensureTouchpointDataCapacity(newData.length);
-		for (int i = 0; i < newData.length; i++) {
-			touchpointData.add(newData[i]);
-		}
-	}
-
-	private void ensureTouchpointDataCapacity(int size) {
-		if (touchpointData != null) {
-			touchpointData.ensureCapacity(size);
-		} else {
-			touchpointData = new ArrayList(size);
-		}
-	}
-
-	private OrderedProperties properties() {
-		return (properties != null ? properties : NO_PROPERTIES);
+	public String getId() {
+		return id;
 	}
 
 	/**
@@ -219,35 +141,113 @@ public class InstallableUnit implements IInstallableUnitConstants, IInstallableU
 		return OrderedProperties.unmodifiableProperties(properties());
 	}
 
-	public void addProperties(OrderedProperties newProperties) {
-		if (properties == null)
-			properties = new OrderedProperties(newProperties.size());
-		properties.putAll(newProperties);
+	public String getProperty(String key) {
+		return getProperties().getProperty(key);
 	}
 
-	public boolean isFragment() {
-		return false;
+	public ProvidedCapability[] getProvidedCapabilities() {
+		return (providedCapabilities != null ? providedCapabilities : NO_PROVIDES);
 	}
 
-	public void setApplicabilityFilter(String ldapFilter) {
-		applicabilityFilter = ldapFilter;
-	}
+	public RequiredCapability[] getRequiredCapabilities() {
+		return requires != null ? requires : NO_REQUIRES;
 
-	public String getApplicabilityFilter() {
-		return applicabilityFilter;
 	}
 
 	public IResolvedInstallableUnit getResolved() {
 		return new ResolvedInstallableUnit(this);
 	}
 
-	public int compareTo(Object toCompareTo) {
-		if (!(toCompareTo instanceof IInstallableUnit)) {
-			return -1;
+	public TouchpointData[] getTouchpointData() {
+		return (touchpointData == null ? TouchpointData.NO_TOUCHPOINT_DATA //
+				: (TouchpointData[]) touchpointData.toArray(new TouchpointData[touchpointData.size()]));
+	}
+
+	public TouchpointType getTouchpointType() {
+		return touchpointType != null ? touchpointType : TouchpointType.NONE;
+	}
+
+	public Version getVersion() {
+		return version;
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((getVersion() == null) ? 0 : getVersion().hashCode());
+		return result;
+	}
+
+	public boolean isFragment() {
+		return false;
+	}
+
+	public boolean isSingleton() {
+		return singleton;
+	}
+
+	private OrderedProperties properties() {
+		return (properties != null ? properties : NO_PROPERTIES);
+	}
+
+	public void setApplicabilityFilter(String ldapFilter) {
+		applicabilityFilter = ldapFilter;
+	}
+
+	public void setArtifacts(IArtifactKey[] value) {
+		artifacts = value;
+	}
+
+	public void setCapabilities(ProvidedCapability[] exportedCapabilities) {
+		providedCapabilities = exportedCapabilities;
+	}
+
+	public void setFilter(String filter) {
+		this.filter = filter;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	// TODO: resolve the schizophrenia between the singleton immutable data
+	//	   	 and the public returned touchpoint data array.
+	public void setImmutableTouchpointData(TouchpointData immutableData) {
+		ensureTouchpointDataCapacity(4);
+		touchpointData.add(immutableData);
+	}
+
+	public String setProperty(String key, String value) {
+		if (value == null)
+			return (properties != null ? (String) properties.remove(key) : null);
+		if (properties == null)
+			properties = new OrderedProperties();
+		return (String) properties.setProperty(key, value);
+	}
+
+	public void setRequiredCapabilities(RequiredCapability[] capabilities) {
+		if (capabilities == NO_REQUIRES) {
+			this.requires = null;
+		} else {
+			//copy array for safety
+			this.requires = (RequiredCapability[]) capabilities.clone();
 		}
-		IInstallableUnit other = (IInstallableUnit) toCompareTo;
-		if (getId().compareTo(other.getId()) == 0)
-			return (getVersion().compareTo(other.getVersion()));
-		return getId().compareTo(other.getId());
+	}
+
+	public void setSingleton(boolean singleton) {
+		this.singleton = singleton;
+	}
+
+	public void setTouchpointType(TouchpointType type) {
+		this.touchpointType = (type != TouchpointType.NONE ? type : null);
+	}
+
+	public void setVersion(Version newVersion) {
+		this.version = (newVersion != null ? newVersion : Version.emptyVersion);
+	}
+
+	public String toString() {
+		return id + ' ' + getVersion();
 	}
 }
