1217931a3ce49ca543aa5bac5cc18d0ac5b23bc0
Simon Kaegi <skaegi>
2007-11-19 05:34:47 +0000

    Adding convenience class

--- bundles/org.eclipse.equinox.p2.directorywatcher/src/org/eclipse/equinox/p2/directorywatcher/BundleFolderRepositorySynchronizer.java
+++ bundles/org.eclipse.equinox.p2.directorywatcher/src/org/eclipse/equinox/p2/directorywatcher/BundleFolderRepositorySynchronizer.java
@@ -1,22 +1,32 @@
 package org.eclipse.equinox.p2.directorywatcher;
 
 import java.io.File;
+import org.eclipse.equinox.internal.p2.directorywatcher.Activator;
 
 public class BundleFolderRepositorySynchronizer {
 
-	public BundleFolderRepositorySynchronizer(File directory, int pollFrequency) {
+	private final DirectoryWatcher watcher;
 
+	public BundleFolderRepositorySynchronizer(File directory) {
+		this(new File[] {directory});
 	}
 
-	public void start() {
+	public BundleFolderRepositorySynchronizer(File[] directories) {
+		watcher = new DirectoryWatcher(directories);
+		RepositoryListener listener = new RepositoryListener(Activator.getContext(), Integer.toString(directories[0].hashCode()));
+		watcher.addListener(listener);
+	}
 
+	public void start(long pollFrequency) {
+		watcher.start(pollFrequency);
 	}
 
 	public void stop() {
-
+		watcher.stop();
 	}
 
 	public void poll() {
+		watcher.poll();
 
 	}
 }
