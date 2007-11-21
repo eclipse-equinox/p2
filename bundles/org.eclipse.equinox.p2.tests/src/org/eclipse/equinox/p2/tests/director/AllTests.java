2400f84231f75f9739684d0aebaa44a332bbdd86
John Arthorne <johna>
2007-11-21 20:14:48 +0000

    Test for Bug 210564 ClassCastException updating

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/director/AllTests.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/director/AllTests.java
@@ -26,6 +26,7 @@ public class AllTests extends TestCase {
 		suite.addTestSuite(OracleTest2.class);
 		suite.addTestSuite(PickerTest.class);
 		suite.addTestSuite(RecommendationTest.class);
+		suite.addTestSuite(ReplacePlanTest.class);
 		suite.addTestSuite(ResolutionHelperTest.class);
 		suite.addTestSuite(RollbackTest.class);
 		suite.addTestSuite(SingletonTest.class);
