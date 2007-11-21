968aa06f4e9a7fb6120354d57d1ee5679474b2f4
Simon Kaegi <skaegi>
2007-11-21 16:51:40 +0000

    adding copyright

--- bundles/org.eclipse.equinox.p2.directorywatcher/src/org/eclipse/equinox/p2/directorywatcher/RepositoryListener.java
+++ bundles/org.eclipse.equinox.p2.directorywatcher/src/org/eclipse/equinox/p2/directorywatcher/RepositoryListener.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ * IBM Corporation - initial implementation and ideas 
+ ******************************************************************************/
 package org.eclipse.equinox.p2.directorywatcher;
 
 import java.io.File;
@@ -221,8 +230,8 @@ public class RepositoryListener extends DirectoryChangeListener {
 				if (descriptor != null) {
 					artifactRepository.addDescriptor(descriptor);
 					modified = true;
+				}
 			}
-		}
 		}
 
 		for (Iterator it = snapshot.iterator(); it.hasNext();) {
