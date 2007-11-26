234eef73a8780b0b2db00dd98a3acde87ac328eb
John Arthorne <johna>
2007-11-26 20:47:43 +0000

    Add type javadoc, sort members

--- bundles/org.eclipse.equinox.p2.artifact.repository/src/org/eclipse/equinox/p2/artifact/repository/IArtifactRepository.java
+++ bundles/org.eclipse.equinox.p2.artifact.repository/src/org/eclipse/equinox/p2/artifact/repository/IArtifactRepository.java
@@ -15,11 +15,26 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.equinox.p2.core.repository.IRepository;
 import org.eclipse.equinox.p2.metadata.IArtifactKey;
+import org.eclipse.equinox.spi.p2.artifact.repository.AbstractArtifactRepository;
 
 /**
- * TODO: Convert to abstract class
+ * A repository containing artifacts.
+ * <p>
+ * This interface is not intended to be implemented by clients.  Artifact repository
+ * implementations must subclass {@link AbstractArtifactRepository} rather than 
+ * implementing this interface directly.
+ * </p>
  */
 public interface IArtifactRepository extends IRepository {
+	/**
+	 * Add the given descriptor to the set of descriptors in this repository.  This is 
+	 * a relatively low-level operation that should be used only when the actual related 
+	 * content is in this repository and the given descriptor accurately describes 
+	 * that content.
+	 * @param descriptor the descriptor to add.
+	 */
+	public void addDescriptor(IArtifactDescriptor descriptor);
+
 	/** 
 	 * Returns true if this repository contains the given descriptor.
 	 * @param descriptor the descriptor to query
@@ -35,14 +50,9 @@ public interface IArtifactRepository extends IRepository {
 	public boolean contains(IArtifactKey key);
 
 	/**
-	 * Executes the given artifact requests on this byte server.
-	 * @param requests The artifact requests
-	 * @param monitor
-	 * @return a status object that is <code>OK</code> if requests were
-	 * processed successfully. Otherwise, a status indicating information,
-	 * warnings, or errors that occurred while executing the artifact requests
+	 * Fill the given stream with the described artifact. Sets status accordingly. 
 	 */
-	public IStatus getArtifacts(IArtifactRequest[] requests, IProgressMonitor monitor);
+	public IStatus getArtifact(IArtifactDescriptor descriptor, OutputStream destination, IProgressMonitor monitor);
 
 	/**
 	 * Return the set of artifact descriptors describing the ways that this repository
@@ -53,15 +63,20 @@ public interface IArtifactRepository extends IRepository {
 	public IArtifactDescriptor[] getArtifactDescriptors(IArtifactKey key);
 
 	/**
-	 * Fill the given stream with the described artifact. Sets status accordingly. 
-	 */
-	public IStatus getArtifact(IArtifactDescriptor descriptor, OutputStream destination, IProgressMonitor monitor);
-
-	/**
 	 * Returns the list of artifact keys managed by this repository
 	 * @return list of artifact keys
 	 */
 	public IArtifactKey[] getArtifactKeys();
+
+	/**
+	 * Executes the given artifact requests on this byte server.
+	 * @param requests The artifact requests
+	 * @param monitor
+	 * @return a status object that is <code>OK</code> if requests were
+	 * processed successfully. Otherwise, a status indicating information,
+	 * warnings, or errors that occurred while executing the artifact requests
+	 */
+	public IStatus getArtifacts(IArtifactRequest[] requests, IProgressMonitor monitor);
 
 	/**
 	 * Open an output stream to which a client can write the data for the given 
@@ -73,13 +88,9 @@ public interface IArtifactRepository extends IRepository {
 	public OutputStream getOutputStream(IArtifactDescriptor descriptor);
 
 	/**
-	 * Add the given descriptor to the set of descriptors in this repository.  This is 
-	 * a relatively low-level operation that should be used only when the actual related 
-	 * content is in this repository and the given descriptor accurately describes 
-	 * that content.
-	 * @param descriptor the descriptor to add.
+	 * Remove the all keys, descriptors, and contents from this repository.
 	 */
-	public void addDescriptor(IArtifactDescriptor descriptor);
+	public void removeAll();
 
 	/**
 	 * Remove the given descriptor and its corresponding content in this repository.  
@@ -92,10 +103,5 @@ public interface IArtifactRepository extends IRepository {
 	 * @param key the key to remove.
 	 */
 	public void removeDescriptor(IArtifactKey key);
-
-	/**
-	 * Remove the all keys, descriptors, and contents from this repository.
-	 */
-	public void removeAll();
 
 }
