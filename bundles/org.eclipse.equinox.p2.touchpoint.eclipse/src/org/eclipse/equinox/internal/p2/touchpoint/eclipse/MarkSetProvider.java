ce16107d5f10e93e1679a895907dac367736856d
Simon Kaegi <skaegi>
2007-12-05 04:00:48 +0000

    An iu might have no associated keys. Adding a check to prevent an NPE.

--- bundles/org.eclipse.equinox.p2.touchpoint.eclipse/src/org/eclipse/equinox/internal/p2/touchpoint/eclipse/MarkSetProvider.java
+++ bundles/org.eclipse.equinox.p2.touchpoint.eclipse/src/org/eclipse/equinox/internal/p2/touchpoint/eclipse/MarkSetProvider.java
@@ -26,6 +26,8 @@ public class MarkSetProvider implements IMarkSetProvider {
 		Iterator installableUnits = aProfile.getInstallableUnits();
 		while (installableUnits.hasNext()) {
 			IArtifactKey[] keys = ((IInstallableUnit) installableUnits.next()).getArtifacts();
+			if (keys == null)
+				return;
 			for (int i = 0; i < keys.length; i++) {
 				artifactKeyList.add(keys[i]);
 			}
