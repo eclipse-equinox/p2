ff81394f106120ee4b386dd0bc6fce79410ee1ce
sliebig <>
2007-12-05 10:30:39 +0000

    new test

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/artifact/repository/processing/AllTests.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/artifact/repository/processing/AllTests.java
@@ -20,6 +20,7 @@ public class AllTests extends TestCase {
 	public static Test suite() {
 		TestSuite suite = new TestSuite(AllTests.class.getName());
 		suite.addTestSuite(ProcessingStepHandlerTest.class);
+		suite.addTestSuite(ProcessingStepTest.class);
 		return suite;
 	}
 
