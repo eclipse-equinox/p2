ab02ecd54d2cbcad4d1688d7312019ee29ed59da
DJ Houghton <dj>
2007-12-07 18:01:33 +0000

    Added missing copyrights.

--- bundles/org.eclipse.equinox.p2.reconciler.dropins/src/org/eclipse/equinox/p2/reconciler/dropins/Activator.java
+++ bundles/org.eclipse.equinox.p2.reconciler.dropins/src/org/eclipse/equinox/p2/reconciler/dropins/Activator.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ * IBM Corporation - initial implementation and ideas 
+ ******************************************************************************/
 package org.eclipse.equinox.p2.reconciler.dropins;
 
 import java.io.File;
@@ -21,8 +30,10 @@ public class Activator implements BundleActivator {
 	private static final String OSGI_INSTALL_AREA = "osgi.install.area"; //$NON-NLS-1$
 	private static final String DROPINS = "dropins"; //$NON-NLS-1$
 
-	public void start(BundleContext context) throws Exception {
+	private static BundleContext bundleContext;
 
+	public void start(BundleContext context) throws Exception {
+		bundleContext = context;
 		Profile profile = getCurrentProfile(context);
 		if (profile == null)
 			return;
@@ -57,7 +68,11 @@ public class Activator implements BundleActivator {
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		// do nothing
+		bundleContext = null;
+	}
+
+	public static BundleContext getContext() {
+		return bundleContext;
 	}
 
 	public static File getWatchedDirectory(BundleContext context) {
