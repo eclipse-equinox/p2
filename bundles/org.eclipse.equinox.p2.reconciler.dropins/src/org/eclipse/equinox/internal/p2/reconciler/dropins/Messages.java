c624213fd32d2009adef63f8bd8a62825b2522c5
DJ Houghton <dj>
2008-02-04 21:04:29 +0000

    Externalized strings.

--- bundles/org.eclipse.equinox.p2.reconciler.dropins/src/org/eclipse/equinox/internal/p2/reconciler/dropins/Messages.java
+++ bundles/org.eclipse.equinox.p2.reconciler.dropins/src/org/eclipse/equinox/internal/p2/reconciler/dropins/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,24 +10,20 @@
  *******************************************************************************/
 package org.eclipse.equinox.internal.p2.reconciler.dropins;
 
-import java.io.File;
-import org.eclipse.equinox.p2.directorywatcher.DirectoryWatcher;
+import org.eclipse.osgi.util.NLS;
 
 /**
  * @since 1.0
  */
-public class SiteWatcher extends DirectoryWatcher {
-
-	static File PLUGINS_DIR;
-	static File FEATURES_DIR;
-
-	/**
-	 * @param directory
-	 */
-	public SiteWatcher(File directory) {
-		super(directory);
-		PLUGINS_DIR = new File(directory, "plugins"); //$NON-NLS-1$
-		FEATURES_DIR = new File(directory, "features"); //$NON-NLS-1$
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.equinox.internal.p2.reconciler.dropins.messages"; //$NON-NLS-1$
+	public static String errorProcessingConfg;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
 	}
 
+	private Messages() {
+		// prevent instantiation
+	}
 }
--- bundles/org.eclipse.equinox.p2.reconciler/src/org/eclipse/equinox/internal/p2/reconciler/dropins/PlatformXmlListener.java
+++ bundles/org.eclipse.equinox.p2.reconciler/src/org/eclipse/equinox/internal/p2/reconciler/dropins/PlatformXmlListener.java
@@ -131,7 +131,7 @@ public class PlatformXmlListener extends DirectoryChangeListener {
 				try {
 					url = FileLocator.resolve(url);
 				} catch (IOException e) {
-					throw new ProvisionException("Exception while processing configuration.", e);
+					throw new ProvisionException(Messages.errorProcessingConfg, e);
 				}
 				File file = new File(url.getPath(), "plugins"); //$NON-NLS-1$
 				DirectoryWatcher watcher = new DirectoryWatcher(file);
@@ -140,7 +140,7 @@ public class PlatformXmlListener extends DirectoryChangeListener {
 				watcher.poll();
 				sites.put(site.getUrl(), new SiteInfo(site, watcher, listener));
 			} catch (MalformedURLException e) {
-				throw new ProvisionException("Exception while processing configuration.", e);
+				throw new ProvisionException(Messages.errorProcessingConfg, e);
 			}
 		}
 	}
