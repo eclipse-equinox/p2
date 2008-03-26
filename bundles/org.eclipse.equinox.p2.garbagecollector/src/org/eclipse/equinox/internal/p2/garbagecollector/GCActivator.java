94eb9948d3bb44b3aea50013ceb0ad9be56b1d25
DJ Houghton <dj>
2008-03-26 23:05:27 +0000

    Bug 224249 - [gc] Garbage collector too aggressive accessing prefs

--- bundles/org.eclipse.equinox.p2.garbagecollector/src/org/eclipse/equinox/internal/p2/garbagecollector/GCActivator.java
+++ bundles/org.eclipse.equinox.p2.garbagecollector/src/org/eclipse/equinox/internal/p2/garbagecollector/GCActivator.java
@@ -10,16 +10,17 @@
  *******************************************************************************/
 package org.eclipse.equinox.internal.p2.garbagecollector;
 
-import java.util.EventObject;
+import java.util.*;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.preferences.IPreferencesService;
+import org.eclipse.core.runtime.preferences.*;
 import org.eclipse.equinox.internal.p2.core.helpers.LogHelper;
 import org.eclipse.equinox.internal.provisional.p2.core.eventbus.IProvisioningEventBus;
 import org.eclipse.equinox.internal.provisional.p2.core.eventbus.SynchronousProvisioningListener;
 import org.eclipse.equinox.internal.provisional.p2.engine.*;
 import org.eclipse.osgi.service.debug.DebugOptions;
 import org.osgi.framework.*;
+import org.osgi.service.prefs.Preferences;
 
 public class GCActivator implements BundleActivator {
 	public static final String ID = "org.eclipse.equinox.p2.garbagecollector"; //$NON-NLS-1$
@@ -69,8 +70,7 @@ public class GCActivator implements BundleActivator {
 				} else if (o instanceof CommitOperationEvent) {
 					if (uninstallEventOccurred == true) {
 						CommitOperationEvent event = (CommitOperationEvent) o;
-						IPreferencesService prefService = (IPreferencesService) getService(context, IPreferencesService.class.getName());
-						if (!(prefService.getBoolean(ID, GC_ENABLED, false, null))) {
+						if (getBooleanPreference(GC_ENABLED, true)) {
 							new GarbageCollector().runGC(event.getProfile());
 						}
 						uninstallEventOccurred = false;
@@ -80,6 +80,17 @@ public class GCActivator implements BundleActivator {
 				}
 			}
 		});
+	}
+
+	protected boolean getBooleanPreference(String key, boolean defaultValue) {
+		IPreferencesService prefService = (IPreferencesService) getService(context, IPreferencesService.class.getName());
+		if (prefService == null)
+			return defaultValue;
+		List nodes = new ArrayList();
+		// todo we should look in the instance scope as well but have to be careful that the instance location has been set
+		nodes.add(new ConfigurationScope().getNode(ID));
+		nodes.add(new DefaultScope().getNode(ID));
+		return Boolean.valueOf(prefService.get(key, Boolean.toString(defaultValue), (Preferences[]) nodes.toArray(new Preferences[nodes.size()]))).booleanValue();
 	}
 
 	private void unregisterGCTrigger() {
