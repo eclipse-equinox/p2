0d951a89119ed1454d75f44fb580a065627b11b5
DJ Houghton <dj>
2008-03-27 21:52:51 +0000

    Bug 224480 - [gc] Garbage collector removes bundles too agressively

--- bundles/org.eclipse.equinox.p2.touchpoint.eclipse/src/org/eclipse/equinox/internal/p2/touchpoint/eclipse/MarkSetProvider.java
+++ bundles/org.eclipse.equinox.p2.touchpoint.eclipse/src/org/eclipse/equinox/internal/p2/touchpoint/eclipse/MarkSetProvider.java
@@ -93,7 +93,7 @@ public class MarkSetProvider implements IMarkSetProvider {
 		while (installableUnits.hasNext()) {
 			IArtifactKey[] keys = ((IInstallableUnit) installableUnits.next()).getArtifacts();
 			if (keys == null)
-				return;
+				continue;
 			for (int i = 0; i < keys.length; i++) {
 				artifactKeyList.add(keys[i]);
 			}
