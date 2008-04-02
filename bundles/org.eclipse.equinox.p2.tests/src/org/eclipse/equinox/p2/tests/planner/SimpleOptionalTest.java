336c22994f6744a97d7a67631f506b23e7f3623f
Pascal Rapicault <prapicau>
2008-04-02 01:09:49 +0000

    New tests on optionality

---
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/SimpleOptionalTest.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.equinox.p2.tests.planner;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.equinox.internal.provisional.p2.director.*;
+import org.eclipse.equinox.internal.provisional.p2.engine.IProfile;
+import org.eclipse.equinox.internal.provisional.p2.metadata.*;
+import org.eclipse.equinox.p2.tests.AbstractProvisioningTest;
+import org.eclipse.osgi.service.resolver.VersionRange;
+import org.osgi.framework.Version;
+
+public class SimpleOptionalTest extends AbstractProvisioningTest {
+	//A dep B op, C, D op 
+	//X dep B
+
+	private IInstallableUnit a1;
+	private IInstallableUnit b1;
+
+	private IProfile profile;
+	private IPlanner planner;
+
+	protected void setUp() throws Exception {
+		b1 = createIU("B", new Version("1.0.0"), true);
+
+		//B's dependency is missing
+		RequiredCapability[] reqA = new RequiredCapability[1];
+		reqA[0] = MetadataFactory.createRequiredCapability(IInstallableUnit.NAMESPACE_IU_ID, "B", VersionRange.emptyRange, null, true, false, true);
+		a1 = createIU("A", new Version("1.0.0"), reqA);
+
+		createTestMetdataRepository(new IInstallableUnit[] {a1, b1});
+
+		profile = createProfile("TestProfile." + getName());
+		planner = createPlanner();
+	}
+
+	public void testInstallation() {
+		//Ensure that D's installation does not fail because of C's absence
+		ProfileChangeRequest req = new ProfileChangeRequest(profile);
+		req.addInstallableUnits(new IInstallableUnit[] {a1});
+		ProvisioningPlan plan = planner.getProvisioningPlan(req, null, null);
+		assertEquals(IStatus.OK, plan.getStatus().getSeverity());
+		assertInstallOperand(plan, a1);
+		assertInstallOperand(plan, b1);
+	}
+}
