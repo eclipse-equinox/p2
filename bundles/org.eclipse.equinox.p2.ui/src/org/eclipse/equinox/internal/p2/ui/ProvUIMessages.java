683072167e6f86ea576eb89b10890c77ac99ffe7
Susan Franklin <sfranklin>
2008-04-08 17:11:55 +0000

    Bug 226116 - [ui] clean up unused messages

--- bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/p2/ui/ProvUIMessages.java
+++ bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/p2/ui/ProvUIMessages.java
@@ -36,7 +36,6 @@ public class ProvUIMessages extends NLS {
 	public static String AcceptLicensesWizardPage_Title;
 	public static String ApplyProfileChangesDialog_ApplyChanges;
 	public static String CategoryElementCollector_Uncategorized;
-	public static String ChooseProfileDialog_DefaultDialogTitle;
 	public static String InstallDialog_InstallSelectionMessage;
 	public static String IUCopyrightPropertyPage_NoCopyright;
 	public static String IUCopyrightPropertyPage_ViewLinkLabel;
@@ -55,35 +54,22 @@ public class ProvUIMessages extends NLS {
 	public static String IULicensePropertyPage_NoLicense;
 	public static String IULicensePropertyPage_ViewLicenseLabel;
 	public static String ProfileElement_InvalidProfile;
-	public static String ProfileModificationAction_NullPlan;
 	public static String ProfileModificationAction_UnexpectedError;
 	public static String ProfileModificationWizardPage_DetailsLabel;
 	public static String ProfileModificationWizardPage_NothingSelected;
-	public static String ProfileModificationWizardPage_ProfileNotFound;
 	public static String ProfileModificationWizardPage_UnexpectedError;
 	// utility error messages
 	public static String ProvisioningUtil_NoRepositoryManager;
-	public static String ProvisioningUtil_AddRepositoryFailure;
-	public static String ProvisioningUtil_CreateRepositoryFailure;
-	public static String ProvisioningUtil_InstallManyTask;
-	public static String ProvisioningUtil_InstallOneTask;
 	public static String ProvisioningUtil_LoadRepositoryFailure;
-	public static String ProvisioningUtil_RepoNotWritable;
-	public static String ProvisioningUtil_RepositoryNotFound;
 	public static String ProvisioningUtil_NoProfileRegistryFound;
 	public static String ProvisioningUtil_NoPlannerFound;
 	public static String ProvisioningUtil_NoDirectorFound;
 	public static String ProvisioningUtil_NoEngineFound;
-	public static String ProvisioningUtil_NoIUFound;
-	public static String ProvisioningUtil_NoInstallRegistryFound;
-	public static String ProvisioningUtil_NoProfileInstallRegistryFound;
-	public static String ProvisioningUtil_RepositoriesSearched;
 
 	// viewer support
 	public static String ProvDropAdapter_InvalidDropTarget;
 	public static String ProvDropAdapter_NoIUsToDrop;
 	public static String ProvDropAdapter_UnsupportedDropOperation;
-	public static String ProvElementQueryResult_CouldNotInstantiateElement;
 
 	// Provisioning operations
 	public static String ProvisioningOperation_ExecuteErrorTitle;
@@ -91,23 +77,19 @@ public class ProvUIMessages extends NLS {
 	public static String ProvisioningOperation_UndoErrorTitle;
 	public static String ProvisioningOperationRunner_ErrorExecutingOperation;
 	public static String InstallIUOperationLabel;
-	public static String InstallIUOperationLabelWithMnemonic;
 	public static String InstallIUCommandLabel;
 	public static String InstallIUCommandTooltip;
 	public static String InstallIUProgress;
 	public static String UninstallDialog_UninstallMessage;
 	public static String UninstallIUOperationLabel;
-	public static String UninstallIUOperationLabelWithMnemonic;
 	public static String UninstallIUCommandLabel;
 	public static String UninstallIUCommandTooltip;
 	public static String UninstallIUProgress;
 	public static String UpdateIUOperationLabel;
-	public static String UpdateIUOperationLabelWithMnemonic;
 	public static String UpdateIUCommandLabel;
 	public static String UpdateIUCommandTooltip;
 	public static String UpdateIUProgress;
 	public static String RevertIUOperationLabel;
-	public static String RevertIUOperationLabelWithMnemonic;
 	public static String RevertIUCommandLabel;
 	public static String RevertIUCommandTooltip;
 	public static String RevertIUProgress;
@@ -133,19 +115,12 @@ public class ProvUIMessages extends NLS {
 	public static String AddRepositoryDialog_DuplicateURL;
 	public static String AddRepositoryDialog_InvalidURL;
 	public static String AddRepositoryDialog_Title;
-	public static String AddRepositoryDialog_URLValidationError;
-	public static String AvailableIUContentProvider_FailureRetrievingContents;
-	public static String AvailableIUContentProvider_JobName;
-	public static String AvailableIUContentProvider_PlaceholderLabel;
 	public static String AvailableIUElement_ProfileNotFound;
 	public static String AvailableIUGroup_ViewByToolTipText;
 	public static String Label_Profiles;
 	public static String Label_Repositories;
 	public static String MetadataRepositoryElement_RepositoryLoadError;
-	public static String UpdateAction_ExceptionDuringUpdateCheck;
-	public static String UpdateAction_UpdateInformationTitle;
 	public static String UpdateAction_UpdatesAvailableMessage;
-	public static String UpdateDialog_AssemblingUpdatesProgress;
 	public static String UpdateAction_UpdatesAvailableTitle;
 	public static String PlatformUpdateTitle;
 	public static String PlatformRestartMessage;
@@ -154,7 +129,6 @@ public class ProvUIMessages extends NLS {
 	public static String ProvUI_NameColumnTitle;
 	public static String ProvUI_SizeColumnTitle;
 	public static String ProvUI_VersionColumnTitle;
-	public static String ProvUI_IDColumnTitle;
 	public static String ProvUI_WarningTitle;
 	public static String ProvUIActivator_ExceptionDuringProfileChange;
 	public static String ProvUILicenseManager_ParsingError;
@@ -171,13 +145,10 @@ public class ProvUIMessages extends NLS {
 	public static String RevertDialog_PageTitle;
 	public static String RevertDialog_RevertError;
 	public static String RevertDialog_RevertOperationLabel;
-	public static String RevertDialog_SelectMessage;
 	public static String RevertDialog_Title;
 	public static String RevertProfileWizardPage_ErrorRetrievingHistory;
 
 	// Operations
-	public static String UpdateAndInstallSelectionDialog_DeselectAllLabel;
-	public static String UpdateAndInstallSelectionDialog_SelectAllLabel;
 	public static String URLValidator_UnrecognizedURL;
 	public static String UpdateManagerCompatibility_UnableToOpenFindAndInstall;
 	public static String UpdateManagerCompatibility_UnableToOpenManageConfiguration;
