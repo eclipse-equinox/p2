73b09e4b1b8233cc9d37939b38eab93e6984d89f
sliebig <>
2008-04-15 12:13:45 +0000

    better error detection for bug 226144

--- bundles/org.eclipse.equinox.p2.artifact.optimizers/src/org/eclipse/equinox/internal/p2/artifact/optimizers/pack200/Pack200OptimizerStep.java
+++ bundles/org.eclipse.equinox.p2.artifact.optimizers/src/org/eclipse/equinox/internal/p2/artifact/optimizers/pack200/Pack200OptimizerStep.java
@@ -17,6 +17,7 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.equinox.internal.p2.artifact.optimizers.AbstractBufferingStep;
 import org.eclipse.equinox.internal.p2.artifact.optimizers.Activator;
 import org.eclipse.equinox.internal.p2.core.helpers.FileUtils;
+import org.eclipse.internal.provisional.equinox.p2.jarprocessor.JarProcessor;
 import org.eclipse.internal.provisional.equinox.p2.jarprocessor.JarProcessorExecutor;
 import org.eclipse.internal.provisional.equinox.p2.jarprocessor.JarProcessorExecutor.Options;
 
@@ -26,6 +27,18 @@ import org.eclipse.internal.provisional.equinox.p2.jarprocessor.JarProcessorExec
 public class Pack200OptimizerStep extends AbstractBufferingStep {
 	private static final String PACKED_SUFFIX = ".pack.gz"; //$NON-NLS-1$
 	private File incoming;
+	private static boolean pack200Available;
+
+	static {
+		try {
+			// TODO This is not very elegant. But there is currently no other way to 
+			//      find it out. PackStep.canPack() is not visible due to restrictions.
+			JarProcessor.getPackProcessor(null);
+			pack200Available = true;
+		} catch (UnsupportedOperationException e) {
+			pack200Available = false;
+		}
+	}
 
 	protected OutputStream createIncomingStream() throws IOException {
 		incoming = File.createTempFile(INCOMING_ROOT, JAR_SUFFIX);
@@ -56,6 +69,9 @@ public class Pack200OptimizerStep extends AbstractBufferingStep {
 	}
 
 	protected File process() throws IOException {
+		if (!pack200Available)
+			// TODO Slightly abused API!!
+			throw new FileNotFoundException("Could not find pack200 command.");
 		// unpack
 		Options options = new Options();
 		options.pack = true;
