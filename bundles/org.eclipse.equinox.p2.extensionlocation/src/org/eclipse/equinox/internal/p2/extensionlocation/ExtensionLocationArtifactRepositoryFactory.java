17ca0da091c28e10177c8a64e2245396de21f13f
cvs2svn <platform-releng-dev@eclipse.org>
2008-05-30 16:19:58 +0000

    This commit was manufactured by cvs2svn to create branch 'publisher_start'.
    Cherrypick from master 2008-05-30 16:19:57 UTC John Arthorne <johna> 'Bug 234154 Names in .properties files are not used for site categories':
        bundles/org.eclipse.equinox.p2.extensionlocation/META-INF/MANIFEST.MF
        bundles/org.eclipse.equinox.p2.extensionlocation/plugin.xml
        bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/Activator.java
        bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/Constants.java
        bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/ExtensionLocationArtifactRepository.java
        bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/ExtensionLocationArtifactRepositoryFactory.java
        bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/ExtensionLocationMetadataRepository.java
        bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/ExtensionLocationMetadataRepositoryFactory.java
        bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/Messages.java
        bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/SiteListener.java
        bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/messages.properties
        bundles/org.eclipse.equinox.p2.metadata.generator/Metadata Generator Plugins and Features.launch
        bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/p2/metadata/generator/LocalizationHelper.java
        bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/p2/metadata/generator/features/DefaultSiteParser.java
        bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/p2/metadata/generator/features/FeatureParser.java
        bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/p2/metadata/generator/features/ProductFile.java
        bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/p2/metadata/generator/features/SiteCategory.java
        bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/p2/metadata/generator/features/SiteModel.java
        bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/provisional/p2/metadata/generator/BundleDescriptionFactory.java
        bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/provisional/p2/metadata/generator/EclipseInstallGeneratorInfoProvider.java
        bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/provisional/p2/metadata/generator/Feature.java
        bundles/org.eclipse.equinox.p2.metadata.generator/src/org/eclipse/equinox/internal/provisional/p2/metadata/generator/MetadataGeneratorHelper.java
        bundles/org.eclipse.equinox.p2.metadata.generator/src_ant/org/eclipse/equinox/internal/p2/metadata/generator/ant/GeneratorTask.java

---
+++ bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/ExtensionLocationArtifactRepositoryFactory.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.equinox.internal.p2.extensionlocation;
+
+import java.net.URL;
+import java.util.Map;
+import org.eclipse.core.runtime.*;
+import org.eclipse.equinox.internal.provisional.p2.artifact.repository.IArtifactRepository;
+import org.eclipse.equinox.internal.provisional.p2.artifact.repository.IFileArtifactRepository;
+import org.eclipse.equinox.internal.provisional.p2.core.ProvisionException;
+import org.eclipse.equinox.internal.provisional.spi.p2.artifact.repository.IArtifactRepositoryFactory;
+import org.eclipse.equinox.internal.provisional.spi.p2.artifact.repository.SimpleArtifactRepositoryFactory;
+import org.eclipse.osgi.util.NLS;
+
+public class ExtensionLocationArtifactRepositoryFactory implements IArtifactRepositoryFactory {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.internal.provisional.spi.p2.artifact.repository.IArtifactRepositoryFactory#create(java.net.URL, java.lang.String, java.lang.String, java.util.Map)
+	 */
+	public IArtifactRepository create(URL location, String name, String type, Map properties) throws ProvisionException {
+		// TODO proper progress monitoring
+		IStatus status = validate(location, null);
+		if (!status.isOK())
+			throw new ProvisionException(status);
+		URL repoLocation = ExtensionLocationArtifactRepository.getLocalRepositoryLocation(location);
+		// unexpected
+		if (repoLocation == null)
+			throw new ProvisionException(new Status(IStatus.ERROR, Activator.ID, Messages.failed_create_local_artifact_repository));
+		// make sure that we aren't trying to create a repo at a location
+		// where one already exists
+		boolean failed = false;
+		try {
+			new SimpleArtifactRepositoryFactory().load(repoLocation, null);
+			failed = true;
+		} catch (ProvisionException e) {
+			// expected
+		}
+		if (failed) {
+			String msg = NLS.bind(Messages.repo_already_exists, location.toExternalForm());
+			throw new ProvisionException(new Status(IStatus.ERROR, Activator.ID, ProvisionException.REPOSITORY_EXISTS, msg, null));
+		}
+		IFileArtifactRepository repo = (IFileArtifactRepository) new SimpleArtifactRepositoryFactory().create(repoLocation, name, type, properties);
+		return new ExtensionLocationArtifactRepository(location, repo, null);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.internal.provisional.spi.p2.artifact.repository.IArtifactRepositoryFactory#load(java.net.URL, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public IArtifactRepository load(URL location, IProgressMonitor monitor) throws ProvisionException {
+		// TODO proper progress monitoring
+		IStatus status = validate(location, null);
+		if (!status.isOK())
+			throw new ProvisionException(status);
+		URL repoLocation = ExtensionLocationArtifactRepository.getLocalRepositoryLocation(location);
+		// unexpected
+		if (repoLocation == null)
+			throw new ProvisionException(new Status(IStatus.ERROR, Activator.ID, Messages.failed_create_local_artifact_repository));
+		// TODO proper progress monitoring
+		try {
+			IFileArtifactRepository repo = (IFileArtifactRepository) new SimpleArtifactRepositoryFactory().load(repoLocation, null);
+			return new ExtensionLocationArtifactRepository(location, repo, monitor);
+		} catch (ProvisionException e) {
+			return create(location, Activator.getRepositoryName(location), ExtensionLocationArtifactRepository.TYPE, null);
+		}
+	}
+
+	public IStatus validate(URL location, IProgressMonitor monitor) {
+		try {
+			ExtensionLocationArtifactRepository.validate(location, monitor);
+		} catch (ProvisionException e) {
+			return e.getStatus();
+		}
+		return Status.OK_STATUS;
+	}
+
+}
---
+++ bundles/org.eclipse.equinox.p2.extensionlocation/src/org/eclipse/equinox/internal/p2/extensionlocation/ExtensionLocationMetadataRepositoryFactory.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.equinox.internal.p2.extensionlocation;
+
+import java.net.URL;
+import java.util.Map;
+import org.eclipse.core.runtime.*;
+import org.eclipse.equinox.internal.provisional.p2.core.ProvisionException;
+import org.eclipse.equinox.internal.provisional.p2.metadata.repository.IMetadataRepository;
+import org.eclipse.equinox.internal.provisional.spi.p2.metadata.repository.IMetadataRepositoryFactory;
+import org.eclipse.equinox.internal.provisional.spi.p2.metadata.repository.SimpleMetadataRepositoryFactory;
+import org.eclipse.osgi.util.NLS;
+
+public class ExtensionLocationMetadataRepositoryFactory implements IMetadataRepositoryFactory {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.internal.provisional.spi.p2.metadata.repository.IMetadataRepositoryFactory#create(java.net.URL, java.lang.String, java.lang.String, java.util.Map)
+	 */
+	public IMetadataRepository create(URL location, String name, String type, Map properties) throws ProvisionException {
+		// TODO proper progress monitoring
+		IStatus status = validate(location, null);
+		if (!status.isOK())
+			throw new ProvisionException(status);
+		URL repoLocation = ExtensionLocationMetadataRepository.getLocalRepositoryLocation(location);
+		// unexpected
+		if (repoLocation == null)
+			throw new ProvisionException(new Status(IStatus.ERROR, Activator.ID, Messages.failed_create_local_artifact_repository));
+		// ensure that we aren't trying to create a repository at a location
+		// where one already exists
+		boolean failed = false;
+		try {
+			new SimpleMetadataRepositoryFactory().load(repoLocation, null);
+			failed = true;
+		} catch (ProvisionException e) {
+			// expected
+		}
+		if (failed) {
+			String msg = NLS.bind(Messages.repo_already_exists, location.toExternalForm());
+			throw new ProvisionException(new Status(IStatus.ERROR, Activator.ID, ProvisionException.REPOSITORY_EXISTS, msg, null));
+		}
+		IMetadataRepository repository = new SimpleMetadataRepositoryFactory().create(repoLocation, name, null, properties);
+		return new ExtensionLocationMetadataRepository(location, repository, null);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.internal.provisional.spi.p2.metadata.repository.IMetadataRepositoryFactory#load(java.net.URL, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public IMetadataRepository load(URL location, IProgressMonitor monitor) throws ProvisionException {
+		// TODO proper progress monitoring
+		IStatus status = validate(location, null);
+		if (!status.isOK())
+			throw new ProvisionException(status);
+		URL repoLocation = ExtensionLocationMetadataRepository.getLocalRepositoryLocation(location);
+		// unexpected
+		if (repoLocation == null)
+			throw new ProvisionException(new Status(IStatus.ERROR, Activator.ID, Messages.failed_create_local_artifact_repository));
+		// TODO proper progress monitoring
+		try {
+			IMetadataRepository repository = new SimpleMetadataRepositoryFactory().load(repoLocation, null);
+			return new ExtensionLocationMetadataRepository(location, repository, monitor);
+		} catch (ProvisionException e) {
+			return create(location, Activator.getRepositoryName(location), ExtensionLocationMetadataRepository.TYPE, null);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.internal.provisional.spi.p2.metadata.repository.IMetadataRepositoryFactory#validate(java.net.URL, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public IStatus validate(URL location, IProgressMonitor monitor) {
+		try {
+			ExtensionLocationMetadataRepository.validate(location, monitor);
+		} catch (ProvisionException e) {
+			return e.getStatus();
+		}
+		return Status.OK_STATUS;
+	}
+
+}
