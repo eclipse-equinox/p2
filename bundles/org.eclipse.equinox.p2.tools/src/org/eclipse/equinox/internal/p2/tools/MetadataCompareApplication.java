d82c9116cc425d3bebcb8e83b573476079f9b174
Jeff McAffer <jeff>
2008-06-03 19:37:35 +0000

    add in a combined metadata/artifact repository replicator application and reconstitued the compare application

--- bundles/org.eclipse.equinox.p2.tools/src/org/eclipse/equinox/internal/p2/tools/MetadataCompareApplication.java
+++ bundles/org.eclipse.equinox.p2.tools/src/org/eclipse/equinox/internal/p2/tools/MetadataCompareApplication.java
@@ -10,216 +10,229 @@
  *******************************************************************************/
 package org.eclipse.equinox.internal.p2.tools;
 
-//import java.net.MalformedURLException;
-//import java.net.URL;
-//import java.util.*;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Comparator;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.equinox.internal.p2.metadata.InstallableUnit;
+import org.eclipse.equinox.internal.p2.metadata.repository.MetadataRepositoryManager;
+import org.eclipse.equinox.internal.provisional.p2.core.ProvisionException;
+import org.eclipse.equinox.internal.provisional.p2.metadata.IInstallableUnit;
+import org.eclipse.equinox.internal.provisional.p2.metadata.RequiredCapability;
+import org.eclipse.equinox.internal.provisional.p2.metadata.query.InstallableUnitQuery;
+import org.eclipse.equinox.internal.provisional.p2.metadata.repository.IMetadataRepository;
+import org.eclipse.equinox.internal.provisional.p2.metadata.repository.IMetadataRepositoryManager;
+import org.eclipse.equinox.internal.provisional.p2.query.Collector;
 
 public class MetadataCompareApplication implements IApplication {
 
-	//	private String source;
-	//	private String target;
-	//	private boolean compare = false;
-	//	private boolean list = false;
-	//	private String type = "meta";
+	private MetadataRepositoryManager repoManager = new MetadataRepositoryManager();
+	private IMetadataRepository sourceRepo = null;
+	private IMetadataRepository targetRepo = null;
+
+	private static Comparator iuIdComparator = new Comparator() {
+		public int compare(Object source, Object target) {
+			IInstallableUnit sourceIU = (IInstallableUnit) source;
+			IInstallableUnit targetIU = (IInstallableUnit) target;
+			int id = sourceIU.getId().compareTo(targetIU.getId());
+			if (id != 0)
+				return id;
+			return sourceIU.getVersion().compareTo(targetIU.getVersion());
+		}
+	};
+
+	private URL sourceLocation;
+	private URL targetLocation;
+	private boolean compare = false;
+	private boolean list = false;
 
 	public Object start(IApplicationContext context) throws Exception {
-		//		initializeFromArguments((String[]) context.getArguments().get("application.args"));
-		//
-		//		if (compare)
-		//			compareMetadataRepositories(new URL(source), new URL(target));
-		//		if (list) {
-		//			list(source);
-		//			list(target);
-		//		}
+		initializeFromArguments((String[]) context.getArguments().get("application.args")); //$NON-NLS-1$
+		initRepositories();
+
+		if (compare) {
+			compareMetadataRepositories();
+		} else if (list) {
+			list(sourceLocation);
+			list(targetLocation);
+		}
 		return IApplication.EXIT_OK;
 	}
 
-	//	private void list(String sourceLocation) throws ProvisionException {
-	//		if (sourceLocation == null)
-	//			return;
-	//		IQueryable source = null;
-	//		if (type.equals("meta")) {
-	//			URL location;
-	//			try {
-	//				location = new URL(sourceLocation);
-	//			} catch (MalformedURLException e) {
-	//				return;
-	//			}
-	//			ProvisioningHelper.addMetadataRepository(location);
-	//			source = ProvisioningHelper.getMetadataRepository(location);
-	//			if (source == null)
-	//				return;
-	//		} else if (type.equals("profile")) {
-	//			source = ProvisioningHelper.getProfile(sourceLocation);
-	//			if (source == null)
-	//				return;
-	//		}
-	//		IInstallableUnit[] sourceIUs = source.query(null, null, null, false, null);
-	//		sourceIUs = sort(sourceIUs, true);
-	//		for (int i = 0; i < sourceIUs.length; i++) {
-	//			System.out.print(sourceIUs[i]);
-	//			System.out.println(sourceIUs[i].isFragment() ? " (fragment)" : "");
-	//		}
-	//		System.out.println("Total: " + sourceIUs.length);
-	//	}
-	//
-	//	private void compareMetadataRepositories(URL source, URL target) throws ProvisionException {
-	//		ProvisioningHelper.addMetadataRepository(source);
-	//		IMetadataRepository sourceRepo = ProvisioningHelper.getMetadataRepository(source);
-	//		if (sourceRepo == null)
-	//			return;
-	//		ProvisioningHelper.addMetadataRepository(target);
-	//		IMetadataRepository targetRepo = ProvisioningHelper.getMetadataRepository(target);
-	//		if (targetRepo == null)
-	//			return;
-	//
-	//		System.out.println("\n" + source.toExternalForm() + " -> " + target.toExternalForm());
-	//		compare(sourceRepo, targetRepo);
-	//
-	//		System.out.println("\n" + target.toExternalForm() + " -> " + source.toExternalForm());
-	//		compare(targetRepo, sourceRepo);
-	//	}
-	//
-	//	private void compare(IQueryable sourceRepo, IQueryable targetRepo) {
-	//		IQueryable[] target = new IQueryable[] {targetRepo};
-	//		IInstallableUnit[] ius = sourceRepo.query(null, null, null, false, null);
-	//		ius = sort(ius, true);
-	//		for (int i = 0; i < ius.length; i++) {
-	//			IInstallableUnit iu = ius[i];
-	//			Iterator result = Query.getIterator(target, iu.getId(), new VersionRange(iu.getVersion(), true, iu.getVersion(), true), null, false);
-	//			if (!result.hasNext())
-	//				System.out.println(iu);
-	//			else {
-	//				String comparison = compare(iu, (IInstallableUnit) result.next());
-	//				if (comparison.length() > 0)
-	//					System.out.println(iu + comparison);
-	//			}
-	//		}
-	//	}
-	//
-	//	private boolean compare(Object a, Object b) {
-	//		if (a == null)
-	//			return b == null;
-	//		return a.equals(b);
-	//	}
-	//
-	//	private boolean compare(Object[] a, Object b[]) {
-	//		if (a == null)
-	//			return b == null;
-	//		return Arrays.equals(a, b);
-	//	}
-	//
-	//	private String compare(IInstallableUnit iu, IInstallableUnit next) {
-	//		String result = "";
-	//		if (!iu.equals(next))
-	//			result += " iu";
-	//		if (!compare(iu.getApplicabilityFilter(), next.getApplicabilityFilter()))
-	//			result += " applicabilityFilter";
-	//		if (!compare(iu.getArtifacts(), next.getArtifacts()))
-	//			result += " artifactLocators";
-	//		if (!compare(iu.getProvidedCapabilities(), next.getProvidedCapabilities()))
-	//			result += " providedCapabilities";
-	//		if (!compareRequires(iu.getRequiredCapabilities(), next.getRequiredCapabilities()))
-	//			result += " requiredCapabilities";
-	//		if (!compare(iu.getTouchpointType(), next.getTouchpointType()))
-	//			result += " touchpointType";
-	//
-	//		if (iu.isFragment()) {
-	//			if (((InstallableUnitFragment) iu).getHostId() == null || ((InstallableUnitFragment) iu).getHostVersionRange() == null)
-	//				return result;
-	//			if (!((InstallableUnitFragment) iu).getHostId().equals(((InstallableUnitFragment) next).getHostId()))
-	//				result += " hostid";
-	//			if (!((InstallableUnitFragment) iu).getHostVersionRange().equals(((InstallableUnitFragment) next).getHostVersionRange()))
-	//				result += " hostversionRange";
-	//		}
-	//		return result;
-	//	}
-	//
-	//	private boolean compareRequires(RequiredCapability[] a, RequiredCapability[] b) {
-	//		if (a == null)
-	//			return b == null;
-	//		if (a.length != b.length)
-	//			return false;
-	//		if (a == b)
-	//			return true;
-	//		for (int i = 0; i < a.length; i++)
-	//			if (findCapability(a[i], b) == null)
-	//				return false;
-	//		return true;
-	//	}
-	//
-	//	private RequiredCapability findCapability(RequiredCapability target, RequiredCapability[] b) {
-	//		for (int i = 0; i < b.length; i++) {
-	//			RequiredCapability capability = b[i];
-	//			if (target.equals(capability))
-	//				return capability;
-	//		}
-	//		return null;
-	//	}
-	//
-	//	private IInstallableUnit[] sort(IInstallableUnit[] ius, boolean clone) {
-	//		Comparator comparator = new Comparator() {
-	//			public int compare(Object source, Object target) {
-	//				IInstallableUnit sourceIU = (IInstallableUnit) source;
-	//				IInstallableUnit targetIU = (IInstallableUnit) target;
-	//				int id = sourceIU.getId().compareTo(targetIU.getId());
-	//				if (id != 0)
-	//					return id;
-	//				int version = sourceIU.getVersion().compareTo(targetIU.getVersion());
-	//				if (version != 0)
-	//					return version;
-	//				return 0;
-	//			}
-	//		};
-	//		IInstallableUnit[] result = ius;
-	//		if (clone) {
-	//			result = new InstallableUnit[ius.length];
-	//			System.arraycopy(ius, 0, result, 0, ius.length);
-	//		}
-	//		Arrays.sort(result, comparator);
-	//		return result;
-	//	}
-	//
-	//	public void initializeFromArguments(String[] args) throws Exception {
-	//		if (args == null)
-	//			return;
-	//		for (int i = 0; i < args.length; i++) {
-	//			// check for args without parameters (i.e., a flag arg)
-	//
-	//			//			if (args[i].equalsIgnoreCase("-sort"))
-	//			//				sort = true;
-	//
-	//			if (args[i].equalsIgnoreCase("-meta"))
-	//				type = "meta";
-	//
-	//			if (args[i].equalsIgnoreCase("-compare"))
-	//				compare = true;
-	//
-	//			if (args[i].equalsIgnoreCase("-profile"))
-	//				type = "profile";
-	//
-	//			if (args[i].equalsIgnoreCase("-list"))
-	//				list = true;
-	//
-	//			// check for args with parameters. If we are at the last argument or
-	//			// if the next one
-	//			// has a '-' as the first character, then we can't have an arg with
-	//			// a parm so continue.
-	//			if (i == args.length - 1 || args[i + 1].startsWith("-")) //$NON-NLS-1$
-	//				continue;
-	//
-	//			String arg = args[++i];
-	//
-	//			if (args[i - 1].equalsIgnoreCase("-source"))
-	//				source = arg;
-	//			if (args[i - 1].equalsIgnoreCase("-target"))
-	//				target = arg;
-	//		}
-	//	}
-	//
-	public void stop() {
+	private void list(URL location) throws ProvisionException {
+		if (location == null)
+			return;
+		IMetadataRepository locationRepo = repoManager.getRepository(location);
+		if (locationRepo == null)
+			return;
+		Collector sourceRoots = locationRepo.query(InstallableUnitQuery.ANY, new Collector(), new NullProgressMonitor());
+		IInstallableUnit[] sourceIUs = (IInstallableUnit[]) sourceRoots.toArray(IInstallableUnit.class);
+
+		sourceIUs = sort(sourceIUs, true);
+		for (int i = 0; i < sourceIUs.length; i++) {
+			System.out.print(sourceIUs[i]);
+			System.out.println(sourceIUs[i].isFragment() ? " (fragment)" : ""); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		System.out.println("Total: " + sourceIUs.length); //$NON-NLS-1$
 	}
 
+	private void compareMetadataRepositories() throws ProvisionException {
+		System.out.println("\n" + sourceLocation.toExternalForm() + " -> " + targetLocation.toExternalForm()); //$NON-NLS-1$ //$NON-NLS-2$
+		compare(sourceRepo, targetRepo);
+	}
+
+	private void initRepositories() throws ProvisionException {
+		if (targetLocation == null || sourceLocation == null)
+			throw new IllegalStateException("Must specify a source and target"); //$NON-NLS-1$
+		sourceRepo = repoManager.loadRepository(sourceLocation, null);
+		targetRepo = initializeTarget();
+	}
+
+	private IMetadataRepository initializeTarget() throws ProvisionException {
+		try {
+			IMetadataRepository repository = repoManager.loadRepository(targetLocation, null);
+			if (!repository.isModifiable())
+				throw new IllegalArgumentException("Metadata repository not modifiable: " + targetLocation); //$NON-NLS-1$
+			return repository;
+		} catch (ProvisionException e) {
+			//fall through and create repo
+		}
+		String repositoryName = targetLocation + " - metadata"; //$NON-NLS-1$
+		return repoManager.createRepository(targetLocation, repositoryName, IMetadataRepositoryManager.TYPE_SIMPLE_REPOSITORY, null);
+	}
+
+	private void compare(IMetadataRepository sourceRepo, IMetadataRepository targetRepo) {
+		Collector sourceRoots = sourceRepo.query(InstallableUnitQuery.ANY, new Collector(), new NullProgressMonitor());
+		Collector targetRoots = targetRepo.query(InstallableUnitQuery.ANY, new Collector(), new NullProgressMonitor());
+		IInstallableUnit[] sourceIUs = (IInstallableUnit[]) sourceRoots.toArray(IInstallableUnit.class);
+		sourceIUs = sort(sourceIUs, true);
+		IInstallableUnit[] targetIUs = (IInstallableUnit[]) targetRoots.toArray(IInstallableUnit.class);
+		targetIUs = sort(targetIUs, true);
+
+		int targetIndex = 0;
+		for (int i = 0; i < sourceIUs.length; i++)
+			targetIndex = compareUsingTargets(sourceIUs[i], targetIUs, targetIndex);
+	}
+
+	private int compareUsingTargets(IInstallableUnit sourceIU, IInstallableUnit[] targetIUs, int targetIndex) {
+		while (targetIndex < targetIUs.length) {
+			int difference = iuIdComparator.compare(sourceIU, targetIUs[targetIndex]);
+			if (difference < 0) {
+				System.out.println(sourceIU + " is not found in target repository"); //$NON-NLS-1$
+				return targetIndex;
+			} else if (difference == 0) {
+				String comparison = compare(sourceIU, targetIUs[targetIndex]);
+				if (comparison.length() > 0)
+					System.out.println(sourceIU + comparison);
+				return targetIndex + 1;
+			} else {
+				System.out.println(targetIUs[targetIndex++] + " is not found in source repository"); //$NON-NLS-1$
+			}
+		}
+		System.out.println(sourceIU + " is not found in target repository"); //$NON-NLS-1$
+		return targetIndex;
+	}
+
+	private boolean compare(Object a, Object b) {
+		if (a == null)
+			return b == null;
+		return a.equals(b);
+	}
+
+	private boolean compare(Object[] a, Object b[]) {
+		if (a == null)
+			return b == null;
+		return Arrays.equals(a, b);
+	}
+
+	private String compare(IInstallableUnit iu, IInstallableUnit next) {
+		if (next == null)
+			return " iu artifactLocators providedCapabilities requiredCapabilities touchpointType"; //$NON-NLS-1$
+		String result = ""; //$NON-NLS-1$
+		if (!iu.equals(next))
+			result += " iu"; //$NON-NLS-1$
+		//		if (!compare(iu.getApplicabilityFilter(), next.getApplicabilityFilter()))
+		//			result += " applicabilityFilter";
+		if (!compare(iu.getArtifacts(), next.getArtifacts()))
+			result += " artifactLocators"; //$NON-NLS-1$
+		if (!compare(iu.getProvidedCapabilities(), next.getProvidedCapabilities()))
+			result += " providedCapabilities"; //$NON-NLS-1$
+		if (!compareRequires(iu.getRequiredCapabilities(), next.getRequiredCapabilities()))
+			result += " requiredCapabilities"; //$NON-NLS-1$
+		if (!compare(iu.getTouchpointType(), next.getTouchpointType()))
+			result += " touchpointType"; //$NON-NLS-1$
+
+		if (iu.isFragment()) {
+			//			if (((InstallableUnitFragment) iu).getHost() == null || ((InstallableUnitFragment) iu).getVersion() == null)
+			//				return result;
+			//			if (!((InstallableUnitFragment) iu).getHost().equals(((InstallableUnitFragment) next).getHost()))
+			//				result += " hostid";
+			//			if (!((InstallableUnitFragment) iu).getVersion().equals(((InstallableUnitFragment) next).getVersion()))
+			//				result += " hostversionRange";
+		}
+		return result;
+	}
+
+	private boolean compareRequires(RequiredCapability[] a, RequiredCapability[] b) {
+		if (a == null)
+			return b == null;
+		if (a.length != b.length)
+			return false;
+		if (a == b)
+			return true;
+		for (int i = 0; i < a.length; i++)
+			if (findCapability(a[i], b) == null)
+				return false;
+		return true;
+	}
+
+	private RequiredCapability findCapability(RequiredCapability target, RequiredCapability[] b) {
+		for (int i = 0; i < b.length; i++) {
+			RequiredCapability capability = b[i];
+			if (target.equals(capability))
+				return capability;
+		}
+		return null;
+	}
+
+	private IInstallableUnit[] sort(IInstallableUnit[] ius, boolean clone) {
+		IInstallableUnit[] result = ius;
+		if (clone) {
+			result = new InstallableUnit[ius.length];
+			System.arraycopy(ius, 0, result, 0, ius.length);
+		}
+		Arrays.sort(result, iuIdComparator);
+		return result;
+	}
+
+	public void initializeFromArguments(String[] args) throws Exception {
+		if (args == null)
+			return;
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equalsIgnoreCase("-compare")) //$NON-NLS-1$
+				compare = true;
+			if (args[i].equalsIgnoreCase("-list")) //$NON-NLS-1$
+				list = true;
+
+			// check for args with parameters. If we are at the last argument or
+			// if the next one
+			// has a '-' as the first character, then we can't have an arg with
+			// a parm so continue.
+			if (i == args.length - 1 || args[i + 1].startsWith("-")) //$NON-NLS-1$
+				continue;
+
+			String arg = args[++i];
+
+			if (args[i - 1].equalsIgnoreCase("-source")) //$NON-NLS-1$
+				sourceLocation = new URL(arg);
+			if (args[i - 1].equalsIgnoreCase("-target")) //$NON-NLS-1$
+				targetLocation = new URL(arg);
+		}
+	}
+
+	public void stop() {
+		//do nothing
+	}
 }
---
+++ bundles/org.eclipse.equinox.p2.tools/src/org/eclipse/equinox/internal/p2/tools/mirror/MirrorApplication.java
@@ -0,0 +1,140 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Code 9 and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Code 9 - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.equinox.internal.p2.tools.mirror;
+
+import java.net.URL;
+import java.util.*;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+/**
+ * An application that performs mirroring of artifacts between repositories.
+ */
+public class MirrorApplication implements IApplication {
+
+	private static final String EMPTY_STRING = ""; //$NON-NLS-1$
+	private static final String COMMA_SEPARATOR = ","; //$NON-NLS-1$
+	private String[] iuSpecs;
+	private String[] artifactSpecs;
+	private URL metadataSourceLocation;
+	private URL metadataDestinationLocation;
+	private URL artifactSourceLocation;
+	private URL artifactDestinationLocation;
+	private boolean transitive = false;
+	private boolean raw = false;
+	private boolean overwrite = false;
+	private boolean verbose = false;
+
+	/**
+	 * Convert a list of tokens into an array. The list separator has to be
+	 * specified.
+	 */
+	public static String[] getArrayArgsFromString(String list, String separator) {
+		if (list == null || list.trim().equals(EMPTY_STRING))
+			return new String[0];
+		List result = new ArrayList();
+		for (StringTokenizer tokens = new StringTokenizer(list, separator); tokens.hasMoreTokens();) {
+			String token = tokens.nextToken().trim();
+			if (!token.equals(EMPTY_STRING)) {
+				if ((token.indexOf('[') >= 0 || token.indexOf('(') >= 0) && tokens.hasMoreTokens())
+					result.add(token + separator + tokens.nextToken());
+				else
+					result.add(token);
+			}
+		}
+		return (String[]) result.toArray(new String[result.size()]);
+	}
+
+	private static String[] getArrayArgsFromString(String list, String delimiterStart, String delimiterEnd, String separator) {
+		if (list == null || list.trim().equals(EMPTY_STRING))
+			return new String[0];
+		List result = new ArrayList();
+		for (StringTokenizer tokens = new StringTokenizer(list, delimiterStart); tokens.hasMoreTokens();) {
+			String token = tokens.nextToken().trim();
+			if (token.endsWith(delimiterEnd))
+				token = token.substring(0, token.length() - delimiterEnd.length());
+			if (token.endsWith(delimiterEnd + separator))
+				token = token.substring(0, token.length() - delimiterEnd.length() - separator.length());
+			if (!token.equals(EMPTY_STRING))
+				result.add(token);
+		}
+		return (String[]) result.toArray(new String[result.size()]);
+	}
+
+	public Object start(IApplicationContext context) throws Exception {
+		long time = -System.currentTimeMillis();
+		initializeFromArguments((String[]) context.getArguments().get("application.args")); //$NON-NLS-1$
+		RepositoryMirroring operation = new RepositoryMirroring(metadataSourceLocation, metadataDestinationLocation, artifactSourceLocation, artifactDestinationLocation);
+		operation.setTransitive(transitive);
+		operation.setRaw(raw);
+		operation.setOverwrite(overwrite);
+		operation.setVerbose(verbose);
+		operation.mirror(iuSpecs, artifactSpecs);
+		time += System.currentTimeMillis();
+		if (verbose)
+			System.out.println("Operation completed in " + new Long(time) + " ms."); //$NON-NLS-1$//$NON-NLS-2$
+		return IApplication.EXIT_OK;
+	}
+
+	public void stop() {
+		//do nothing
+	}
+
+	public void initializeFromArguments(String[] args) throws Exception {
+		if (args == null)
+			return;
+		for (int i = 0; i < args.length; i++) {
+			// check for args without parameters (i.e., a flag arg)
+			if (args[i].equalsIgnoreCase("-referencedIUs")) //$NON-NLS-1$
+				transitive = true;
+			if (args[i].equalsIgnoreCase("-raw")) //$NON-NLS-1$
+				raw = true;
+			if (args[i].equalsIgnoreCase("-overwrite")) //$NON-NLS-1$
+				overwrite = true;
+			if (args[i].equalsIgnoreCase("-verbose")) //$NON-NLS-1$
+				verbose = true;
+
+			// check for args with parameters. If we are at the last argument or 
+			// if the next one has a '-' as the first character, then we can't have 
+			// an arg with a param so continue.
+			if (i == args.length - 1 || args[i + 1].startsWith("-")) //$NON-NLS-1$
+				continue;
+			String arg = args[++i];
+
+			if (args[i - 1].equalsIgnoreCase("-source")) { //$NON-NLS-1$
+				metadataSourceLocation = new URL(arg);
+				artifactSourceLocation = new URL(arg);
+			}
+			if (args[i - 1].equalsIgnoreCase("-destination")) { //$NON-NLS-1$
+				metadataDestinationLocation = new URL(arg);
+				artifactDestinationLocation = new URL(arg);
+			}
+			if (args[i - 1].equalsIgnoreCase("-metadatadestination")) //$NON-NLS-1$
+				metadataDestinationLocation = new URL(arg);
+			if (args[i - 1].equalsIgnoreCase("-metadatasource")) //$NON-NLS-1$
+				metadataSourceLocation = new URL(arg);
+			if (args[i - 1].equalsIgnoreCase("-artifactdestination")) //$NON-NLS-1$
+				artifactDestinationLocation = new URL(arg);
+			if (args[i - 1].equalsIgnoreCase("-artifactsource")) //$NON-NLS-1$
+				artifactSourceLocation = new URL(arg);
+			if (args[i - 1].equalsIgnoreCase("-ius")) //$NON-NLS-1$
+				if (arg.equalsIgnoreCase("all") || arg.equalsIgnoreCase("*")) //$NON-NLS-1$ //$NON-NLS-2$
+					iuSpecs = new String[0];
+				else
+					iuSpecs = getArrayArgsFromString(arg, COMMA_SEPARATOR);
+			if (args[i - 1].equalsIgnoreCase("-artifacts")) //$NON-NLS-1$
+				if (arg.equalsIgnoreCase("all") || arg.equalsIgnoreCase("*")) //$NON-NLS-1$ //$NON-NLS-2$
+					artifactSpecs = new String[0];
+				else
+					artifactSpecs = getArrayArgsFromString(arg, "{", "}", COMMA_SEPARATOR); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+	}
+}
