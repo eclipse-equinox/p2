4cdcd6fefe939c645e0853bdc127a1e0116e5428
Jeff McAffer <jeff>
2008-09-11 00:39:00 +0000

    Bug 246461 [publisher] NPE when trying to add subversive update site

--- bundles/org.eclipse.equinox.p2.updatesite/src/org/eclipse/equinox/internal/p2/updatesite/SiteXMLAction.java
+++ bundles/org.eclipse.equinox.p2.updatesite/src/org/eclipse/equinox/internal/p2/updatesite/SiteXMLAction.java
@@ -125,7 +125,9 @@ public class SiteXMLAction extends AbstractPublisherAction {
 			if (index != -1)
 				mirrors = mirrors.substring(0, index) + mirrors.substring(index + "site.xml".length()); //$NON-NLS-1$
 			info.getMetadataRepository().setProperty(IRepository.PROP_MIRRORS_URL, mirrors);
-			info.getArtifactRepository().setProperty(IRepository.PROP_MIRRORS_URL, mirrors);
+			// there does not really need to be an artifact repo but if there is, setup its mirrors.
+			if (info.getArtifactRepository() != null)
+				info.getArtifactRepository().setProperty(IRepository.PROP_MIRRORS_URL, mirrors);
 		}
 
 		//publish associate sites as repository references
