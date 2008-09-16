97bb5145e0623ef04a6d5ccddc7d721394d10272
Pascal Rapicault <prapicau>
2008-09-16 01:49:24 +0000

    Install and uninstall a complete SDK 3.4

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/full/End2EndTest.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/full/End2EndTest.java
@@ -8,12 +8,19 @@
  ******************************************************************************/
 package org.eclipse.equinox.p2.tests.full;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.equinox.internal.p2.core.helpers.ServiceHelper;
-import org.eclipse.equinox.internal.provisional.p2.director.*;
-import org.eclipse.equinox.internal.provisional.p2.engine.*;
+import org.eclipse.equinox.internal.provisional.p2.artifact.repository.IArtifactRepositoryManager;
+import org.eclipse.equinox.internal.provisional.p2.core.ProvisionException;
+import org.eclipse.equinox.internal.provisional.p2.director.IDirector;
+import org.eclipse.equinox.internal.provisional.p2.director.ProfileChangeRequest;
+import org.eclipse.equinox.internal.provisional.p2.engine.IProfile;
+import org.eclipse.equinox.internal.provisional.p2.engine.IProfileRegistry;
 import org.eclipse.equinox.internal.provisional.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.internal.provisional.p2.metadata.query.InstallableUnitQuery;
 import org.eclipse.equinox.internal.provisional.p2.metadata.repository.IMetadataRepositoryManager;
@@ -27,109 +34,91 @@ import org.osgi.framework.Version;
 
 public class End2EndTest extends AbstractProvisioningTest {
 
-	private IMetadataRepositoryManager repoManager;
+	private IMetadataRepositoryManager metadataRepoManager;
+
+	private IArtifactRepositoryManager artifactRepoManager;
 	private IDirector director;
-	private IPlanner planner;
 
 	protected void setUp() throws Exception {
 		ServiceReference sr = TestActivator.context.getServiceReference(IDirector.class.getName());
 		if (sr == null)
 			throw new RuntimeException("Director service not available");
 		director = createDirector();
-		planner = createPlanner();
+		//		planner = createPlanner();
 		ServiceReference sr2 = TestActivator.context.getServiceReference(IMetadataRepositoryManager.class.getName());
-		repoManager = (IMetadataRepositoryManager) TestActivator.context.getService(sr2);
-		if (repoManager == null)
-			throw new RuntimeException("Repository manager could not be loaded");
+		metadataRepoManager = (IMetadataRepositoryManager) TestActivator.context.getService(sr2);
+		if (metadataRepoManager == null)
+			throw new RuntimeException("Metadata repository manager could not be loaded");
+
+		ServiceReference sr3 = TestActivator.context.getServiceReference(IArtifactRepositoryManager.class.getName());
+		artifactRepoManager = (IArtifactRepositoryManager) TestActivator.context.getService(sr3);
+		if (artifactRepoManager == null)
+			throw new RuntimeException("Artifact repo manager could not be loaded");
 	}
 
-	protected IProfile createProfile(String profileId) {
-		String installFolder = System.getProperty(IProfile.PROP_INSTALL_FOLDER);
+	protected IProfile createProfile(String profileId, String installFolder) {
 		ServiceReference profileRegSr = TestActivator.context.getServiceReference(IProfileRegistry.class.getName());
 		IProfileRegistry profileRegistry = (IProfileRegistry) TestActivator.context.getService(profileRegSr);
 		if (profileRegistry == null) {
 			throw new RuntimeException("Profile registry service not available");
 		}
 
-		String newFlavor = System.getProperty("eclipse.p2.configurationFlavor");
-		boolean doUninstall = (Boolean.TRUE.equals(Boolean.valueOf(System.getProperty("eclipse.p2.doUninstall"))));
+		Map properties = new HashMap();
+		properties.put(IProfile.PROP_INSTALL_FOLDER, installFolder + '/' + profileId);
+		EnvironmentInfo info = (EnvironmentInfo) ServiceHelper.getService(TestActivator.getContext(), EnvironmentInfo.class.getName());
+		if (info != null)
+			properties.put(IProfile.PROP_ENVIRONMENTS, "osgi.os=" + info.getOS() + ",osgi.ws=" + info.getWS() + ",osgi.arch=" + info.getOSArch());
 
-		IProfile p = null;
-		if (doUninstall) {
-			p = profileRegistry.getProfile(profileId);
-			if (p == null)
-				throw new RuntimeException("Uninstalling from a nonexistent profile");
-		} else {
-			Map properties = new HashMap();
-			properties.put(IProfile.PROP_INSTALL_FOLDER, installFolder + '/' + profileId);
-			properties.put(IProfile.PROP_FLAVOR, newFlavor);
-			EnvironmentInfo info = (EnvironmentInfo) ServiceHelper.getService(TestActivator.getContext(), EnvironmentInfo.class.getName());
-			if (info != null)
-				properties.put(IProfile.PROP_ENVIRONMENTS, "osgi.os=" + info.getOS() + ",osgi.ws=" + info.getWS() + ",osgi.arch=" + info.getOSArch());
-
-			p = createProfile(profileId, null, properties);
-		}
-		return p;
+		return createProfile(profileId, null, properties);
 	}
 
 	public void testInstallSDK() {
-		IProfile profile2 = createProfile("profile2");
+		File installFolder = TestActivator.getContext().getDataFile(End2EndTest.class.getName());
+		IProfile profile2 = createProfile("End2EndProfile", installFolder.getAbsolutePath());
+
+		//Add repository of the release
+		try {
+			metadataRepoManager.loadRepository(new URL("http://download.eclipse.org/eclipse/updates/3.4"), new NullProgressMonitor());
+		} catch (ProvisionException e) {
+			return;
+		} catch (MalformedURLException e) {
+			return;
+		}
+
+		final String sdkID = "org.eclipse.sdk.ide";
+		final Version sdkVersion = new Version("3.4.0.I20080617-2000");
+
 		//First we install the sdk
 		ProfileChangeRequest request = new ProfileChangeRequest(profile2);
-		request.addInstallableUnits(new IInstallableUnit[] {getIU("sdk", new Version("3.3.0"))});
+		request.addInstallableUnits(new IInstallableUnit[] {getIU(sdkID, sdkVersion)});
 		IStatus s = director.provision(request, null, new NullProgressMonitor());
 		if (!s.isOK())
-			fail("Installation failed");
-		IInstallableUnit firstSnapshot = getIU("profile2"); //This should represent the empty profile
-		assertNotNull(firstSnapshot);
-		assertNotNull(firstSnapshot.getProperty("profileIU"));
+			fail("Installation of the " + sdkID + " " + sdkVersion + " failed.");
 
 		//Uninstall the SDK
 		request = new ProfileChangeRequest(profile2);
-		request.removeInstallableUnits(new IInstallableUnit[] {getIU("sdk", new Version("3.3.0"))});
+		request.removeInstallableUnits(new IInstallableUnit[] {getIU(sdkID, sdkVersion)});
 		s = director.provision(request, null, new NullProgressMonitor());
 		if (!s.isOK())
-			fail("The uninstallation has failed and it was not expected");
+			fail("The uninstallation of the " + sdkID + " " + sdkVersion + " failed.");
 
 		assertEquals(false, getInstallableUnits(profile2).hasNext()); //the profile should be empty since we uninstalled everything
-		IInstallableUnit[] snapshots = getIUs("profile2");
-		assertTrue("snap" + snapshots.length, snapshots.length >= 2);//TODO Normally here it should be 2!!!
-		assertTrue(profile2.query(new InstallableUnitQuery("sdk", VersionRange.emptyRange), new Collector(), null).isEmpty());
-
-		// Now test the rollback to a previous state, in this case we reinstall the SDK
-		s = director.revert(snapshots[0].equals(firstSnapshot) ? snapshots[1] : snapshots[0], profile2, new ProvisioningContext(), new NullProgressMonitor());
-		if (!s.isOK())
-			fail("The become operation failed");
-
-		assertNotNull(getIU("sdk"));
-
-		//Test replace
-		request = new ProfileChangeRequest(profile2);
-		request.addInstallableUnits(planner.updatesFor(getIU("sdk", new Version("3.3.0")), null, null));
-		request.removeInstallableUnits(new IInstallableUnit[] {getIU("sdk", new Version("3.3.0"))});
-		assertOK("2.0", director.provision(request, null, null));
-		assertProfileContainsAll("", profile2, new IInstallableUnit[] {getIU("sdk", new Version("3.4.0"))});
-		assertNotIUs(new IInstallableUnit[] {getIU("sdk", new Version("3.3.0"))}, getInstallableUnits(profile2));
-
-		//Remove everything from the profile by becoming an empty profile
-		s = director.revert(firstSnapshot, profile2, new ProvisioningContext(), new NullProgressMonitor());
-		assertOK("2.1", s);
-		assertEmptyProfile(profile2);
+		assertTrue(profile2.query(new InstallableUnitQuery(sdkID, VersionRange.emptyRange), new Collector(), null).isEmpty());
 	}
 
 	public IInstallableUnit[] getIUs(String id) {
-		return (IInstallableUnit[]) repoManager.query(new InstallableUnitQuery(id, VersionRange.emptyRange), new Collector(), null).toArray(IInstallableUnit.class);
+		return (IInstallableUnit[]) metadataRepoManager.query(new InstallableUnitQuery(id, VersionRange.emptyRange), new Collector(), null).toArray(IInstallableUnit.class);
 	}
 
 	public IInstallableUnit getIU(String id) {
-		Iterator it = repoManager.query(new InstallableUnitQuery(id, VersionRange.emptyRange), new Collector(), null).iterator();
+		Iterator it = metadataRepoManager.query(new InstallableUnitQuery(id, VersionRange.emptyRange), new Collector(), null).iterator();
 		if (it.hasNext())
 			return (IInstallableUnit) it.next();
 		return null;
 	}
 
 	public IInstallableUnit getIU(String id, Version v) {
-		Iterator it = repoManager.query(new InstallableUnitQuery(id, v), new Collector(), null).iterator();
+		Iterator it = metadataRepoManager.query(new InstallableUnitQuery(id, v), new Collector(), null).iterator();
 		if (it.hasNext())
 			return (IInstallableUnit) it.next();
 		return null;
