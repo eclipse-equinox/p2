54acc2bc2021495cbcd6628f122440ab715cd4df
Simon Kaegi <skaegi>
2008-09-17 17:57:20 +0000

    Updaing Downloadmanager tests

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/engine/DownloadManagerTest.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/engine/DownloadManagerTest.java
@@ -10,14 +10,22 @@
  *******************************************************************************/
 package org.eclipse.equinox.p2.tests.engine;
 
-import junit.framework.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import junit.framework.Test;
+import junit.framework.TestSuite;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.equinox.internal.p2.engine.DownloadManager;
+import org.eclipse.equinox.internal.provisional.p2.artifact.repository.IArtifactRequest;
+import org.eclipse.equinox.internal.provisional.p2.engine.ProvisioningContext;
+import org.eclipse.equinox.internal.provisional.p2.metadata.IArtifactKey;
+import org.eclipse.equinox.p2.tests.AbstractProvisioningTest;
 
 /**
  * Simple tests of {@link DownloadManager} API.
  */
-public class DownloadManagerTest extends TestCase {
+public class DownloadManagerTest extends AbstractProvisioningTest {
 	public static Test suite() {
 		return new TestSuite(DownloadManagerTest.class);
 	}
@@ -31,4 +39,161 @@ public class DownloadManagerTest extends TestCase {
 		assertTrue("1.0", result.isOK());
 	}
 
+	/**
+	 * Tests invocation of DownloadManager when there is nothing to download.
+	 */
+	public void testEmptyWithContext() {
+		ProvisioningContext context = new ProvisioningContext();
+		DownloadManager manager = new DownloadManager(context);
+		IStatus result = manager.start(null);
+		assertTrue("1.0", result.isOK());
+	}
+
+	/**
+	 * Tests invocation of DownloadManager when there is nothing to download.
+	 */
+	public void testAddNullArtifactRequest() {
+		ProvisioningContext context = new ProvisioningContext();
+		DownloadManager manager = new DownloadManager(context);
+		try {
+			manager.add((IArtifactRequest) null);
+		} catch (NullPointerException e) {
+			return;
+		}
+		fail("1.0");
+	}
+
+	public void testAddNullArtifactRequestArray() {
+		ProvisioningContext context = new ProvisioningContext();
+		DownloadManager manager = new DownloadManager(context);
+		try {
+			manager.add((IArtifactRequest[]) null);
+		} catch (NullPointerException e) {
+			return;
+		}
+		fail("1.0");
+	}
+
+	public void testAddEmptyArtifactRequestArray() {
+		ProvisioningContext context = new ProvisioningContext();
+		DownloadManager manager = new DownloadManager(context);
+		manager.add(new IArtifactRequest[0]);
+		IStatus result = manager.start(null);
+		assertTrue("1.0", result.isOK());
+	}
+
+	public void testAddArtifactRequestArrayContainingNull() {
+		ProvisioningContext context = new ProvisioningContext();
+		DownloadManager manager = new DownloadManager(context);
+		try {
+			IArtifactRequest[] requests = new IArtifactRequest[] {null};
+			manager.add(requests);
+		} catch (NullPointerException e) {
+			return;
+		}
+		fail("1.0");
+	}
+
+	public void testAddArtifactRequest() {
+		ProvisioningContext context = new ProvisioningContext();
+		DownloadManager manager = new DownloadManager(context);
+
+		IArtifactRequest request = createArtifactRequest();
+		manager.add(request);
+		IStatus result = manager.start(null);
+		assertTrue("1.0", result.isOK());
+
+	}
+
+	public void testAddArtifactRequestArray() {
+		ProvisioningContext context = new ProvisioningContext();
+		DownloadManager manager = new DownloadManager(context);
+
+		IArtifactRequest[] requests = new IArtifactRequest[] {createArtifactRequest()};
+		manager.add(requests);
+		IStatus result = manager.start(null);
+		assertTrue("1.0", result.isOK());
+
+	}
+
+	private IArtifactRequest createArtifactRequest() {
+		IArtifactRequest request = new IArtifactRequest() {
+			public IArtifactKey getArtifactKey() {
+				return null;
+			}
+
+			public IStatus getResult() {
+				return Status.OK_STATUS;
+			}
+		};
+		return request;
+	}
+
+	public void testEmptyArtifactRepositoryListContext() {
+		ProvisioningContext context = new ProvisioningContext();
+		context.setArtifactRepositories(new URL[0]);
+		DownloadManager manager = new DownloadManager(context);
+
+		IArtifactRequest[] requests = new IArtifactRequest[] {createArtifactRequest()};
+		manager.add(requests);
+		IStatus result = manager.start(null);
+		assertEquals("1.0", IStatus.ERROR, result.getSeverity());
+	}
+
+	public void testFileFirstArtifactRepositoryListContext() {
+		ProvisioningContext context = new ProvisioningContext();
+		URL[] artifactRepos = new URL[2];
+		try {
+			artifactRepos[0] = new URL("file:/test");
+			artifactRepos[1] = new URL("jar:file:/test!/");
+		} catch (MalformedURLException e) {
+			fail(e.getMessage());
+		}
+
+		context.setArtifactRepositories(artifactRepos);
+		DownloadManager manager = new DownloadManager(context);
+
+		IArtifactRequest[] requests = new IArtifactRequest[] {createArtifactRequest()};
+		manager.add(requests);
+		IStatus result = manager.start(null);
+		assertTrue("1.0", result.isOK());
+	}
+
+	public void testFileLastArtifactRepositoryListContext() {
+		ProvisioningContext context = new ProvisioningContext();
+		URL[] artifactRepos = new URL[2];
+		try {
+			artifactRepos[0] = new URL("jar:file:/test!/");
+			artifactRepos[1] = new URL("file:/test");
+		} catch (MalformedURLException e) {
+			fail(e.getMessage());
+		}
+
+		context.setArtifactRepositories(artifactRepos);
+		DownloadManager manager = new DownloadManager(context);
+
+		IArtifactRequest[] requests = new IArtifactRequest[] {createArtifactRequest()};
+		manager.add(requests);
+		IStatus result = manager.start(null);
+		assertTrue("1.0", result.isOK());
+	}
+
+	public void testNoFileArtifactRepositoryListContext() {
+		ProvisioningContext context = new ProvisioningContext();
+		URL[] artifactRepos = new URL[2];
+		try {
+			artifactRepos[0] = new URL("jar:file:/test1!/");
+			artifactRepos[1] = new URL("jar:file:/test2!/");
+		} catch (MalformedURLException e) {
+			fail(e.getMessage());
+		}
+
+		context.setArtifactRepositories(artifactRepos);
+		DownloadManager manager = new DownloadManager(context);
+
+		IArtifactRequest[] requests = new IArtifactRequest[] {createArtifactRequest()};
+		manager.add(requests);
+		IStatus result = manager.start(null);
+		assertTrue("1.0", result.isOK());
+	}
 }
