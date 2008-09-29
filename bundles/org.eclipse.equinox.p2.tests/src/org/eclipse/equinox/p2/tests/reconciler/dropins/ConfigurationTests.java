b93c2053cca07d37c5b85ec4151dd6899049015c
DJ Houghton <dj>
2008-09-29 19:02:12 +0000

    Bug 247095 - p2 reconciler not deleting lines associated with removed features/plugins/sites from USER-INCLUDE sites in platform.xml from bundles.info

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/reconciler/dropins/ConfigurationTests.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/reconciler/dropins/ConfigurationTests.java
@@ -41,6 +41,7 @@ public class ConfigurationTests extends AbstractReconcilerTest {
 		TestSuite suite = new ReconcilerTestSuite();
 		suite.addTest(new ConfigurationTests("testDiscoverOne"));
 		suite.addTest(new ConfigurationTests("test_247095"));
+		suite.addTest(new ConfigurationTests("test_247095b"));
 		return suite;
 	}
 
@@ -64,9 +65,6 @@ public class ConfigurationTests extends AbstractReconcilerTest {
 	 * from the site we need to ensure the plug-ins are removed from the install.
 	 */
 	public void test_247095() {
-		// TODO enable once bug 247095 is fixed
-		if (true)
-			return;
 		Configuration configuration = getConfiguration();
 		File temp = getTempFolder();
 		toRemove.add(temp);
@@ -112,5 +110,65 @@ public class ConfigurationTests extends AbstractReconcilerTest {
 		configuration = getConfiguration();
 		removeSite(configuration, siteLocation);
 		save("99.2", configuration);
+		reconcile("99.3");
+		assertDoesNotExistInBundlesInfo("99.4", "ccc", "1.0.0");
 	}
+
+	/*
+	 * Same but delete the files from disk. (other test cases doesn't delete the files... simulates
+	 * the use of a shared bundle pool)
+	 */
+	public void test_247095b() {
+		Configuration configuration = getConfiguration();
+		File temp = getTempFolder();
+		toRemove.add(temp);
+		String siteLocation = null;
+		try {
+			siteLocation = new File(temp, "eclipse").toURL().toExternalForm();
+		} catch (MalformedURLException e) {
+			fail("0.9", e);
+		}
+
+		// copy the data to the temp folder
+		File source = getTestData("1.0", "testData/reconciler/247095");
+		copy("1.1", source, temp);
+
+		/* this is the entry to add to the site.xml file
+			<site enabled="true" policy="USER-INCLUDE" updateable="false"
+					url="file:C:/share/1/" list="plugins/hello_1.0.0.jar" >
+				<feature id="hello_feature" version="1.0.0" />
+			</site>
+		 */
+		Site site = createSite("USER-INCLUDE", true, false, siteLocation, new String[] {"plugins/bbb_1.0.0.jar,plugins/ccc_1.0.0.jar"});
+		Feature feature = createFeature(site, "bbb.feature", "1.0.0", "features/bbb.feature_1.0.0/");
+		site.addFeature(feature);
+		configuration.add(site);
+		save("5.0", configuration);
+		reconcile("6.0");
+		assertExistsInBundlesInfo("7.0", "bbb", "1.0.0");
+		assertExistsInBundlesInfo("7.1", "ccc", "1.0.0");
+		configuration = getConfiguration();
+		assertFeatureExists("7.2", configuration, "bbb.feature", "1.0.0");
+
+		// remove the feature and its bundle from the platform.xml but leave the second bundle
+		configuration = getConfiguration();
+		assertTrue("9.0", removeSite(configuration, siteLocation));
+		site = createSite("USER-INCLUDE", true, false, siteLocation, new String[] {"plugins/ccc_1.0.0.jar"});
+		configuration.add(site);
+		save("9.1", configuration);
+		File parent = new File(temp, "eclipse");
+		assertTrue("9.2", delete(new File(parent, "plugins/bbb_1.0.0.jar")));
+		assertTrue("9.3", delete(new File(parent, "features/bbb.feature_1.0.0")));
+		reconcile("10.0");
+		assertDoesNotExistInBundlesInfo("10.1", "bbb", "1.0.0");
+		assertExistsInBundlesInfo("10.2", "ccc", "1.0.0");
+
+		// cleanup
+		configuration = getConfiguration();
+		removeSite(configuration, siteLocation);
+		save("99.2", configuration);
+		reconcile("99.3");
+		assertDoesNotExistInBundlesInfo("99.4", "ccc", "1.0.0");
+	}
+
 }
