43fa97883d06e8ad99959a8452b90942e80add72
cvs2svn <platform-releng-dev@eclipse.org>
2008-11-14 21:18:28 +0000

    This commit was manufactured by cvs2svn to create branch 'path_fun_2008'.
    Cherrypick from master 2008-11-14 21:18:26 UTC Simon Kaegi <skaegi> 'Rolling back changes that should have been in path_fun_2008 branch only':
        bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/internal/p2/publisher/eclipse/DataLoader.java
        bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/p2/publisher/eclipse/ConfigCUsAction.java

---
+++ bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/internal/p2/publisher/eclipse/DataLoader.java
@@ -0,0 +1,230 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Code 9 and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   Code 9 - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.equinox.internal.p2.publisher.eclipse;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.equinox.internal.frameworkadmin.equinox.EquinoxFwConfigFileParser;
+import org.eclipse.equinox.internal.frameworkadmin.equinox.EquinoxManipulatorImpl;
+import org.eclipse.equinox.internal.frameworkadmin.utils.Utils;
+import org.eclipse.equinox.internal.p2.core.helpers.LogHelper;
+import org.eclipse.equinox.internal.p2.core.helpers.ServiceHelper;
+import org.eclipse.equinox.internal.p2.publisher.Activator;
+import org.eclipse.equinox.internal.provisional.frameworkadmin.*;
+import org.osgi.framework.*;
+import org.osgi.service.packageadmin.PackageAdmin;
+
+public class DataLoader {
+
+	private final static String FILTER_OBJECTCLASS = "(" + Constants.OBJECTCLASS + "=" + FrameworkAdmin.class.getName() + ")"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	private final static String filterFwName = "(" + FrameworkAdmin.SERVICE_PROP_KEY_FW_NAME + "=Equinox)"; //$NON-NLS-1$ //$NON-NLS-2$
+	//String filterFwVersion = "(" + FrameworkAdmin.SERVICE_PROP_KEY_FW_VERSION + "=" + props.getProperty("equinox.fw.version") + ")";
+	private final static String filterLauncherName = "(" + FrameworkAdmin.SERVICE_PROP_KEY_LAUNCHER_NAME + "=Eclipse.exe)"; //$NON-NLS-1$ //$NON-NLS-2$
+	//String filterLauncherVersion = "(" + FrameworkAdmin.SERVICE_PROP_KEY_LAUNCHER_VERSION + "=" + props.getProperty("equinox.launcher.version") + ")";
+	private final static String frameworkAdminFillter = "(&" + FILTER_OBJECTCLASS + filterFwName + filterLauncherName + ")"; //$NON-NLS-1$ //$NON-NLS-2$
+
+	private static final String ORG_ECLIPSE_EQUINOX_SIMPLECONFIGURATOR_CONFIGURL = "org.eclipse.equinox.simpleconfigurator.configUrl"; //$NON-NLS-1$
+	private static final String ORG_ECLIPSE_EQUINOX_SIMPLECONFIGURATOR_MANIPULATOR = "org.eclipse.equinox.simpleconfigurator.manipulator"; //$NON-NLS-1$
+	private static final String ORG_ECLIPSE_EQUINOX_FRAMEWORKADMIN_EQUINOX = "org.eclipse.equinox.frameworkadmin.equinox"; //$NON-NLS-1$
+	private static final String PARAMETER_BASEURL = "org.eclipse.equinox.simpleconfigurator.baseUrl"; //$NON-NLS-1$
+
+	private Manipulator manipulator;
+	private File configurationLocation;
+
+	public static BundleInfo[] readConfiguration(URL url) throws IOException {
+		List bundles = new ArrayList();
+		try {
+			// System.out.println("readConfiguration(URL url):url()=" + url);
+			// URL configFileUrl = getConfigFileUrl();
+			// URL configFileUrl = Utils.getUrl("file",null,
+			// inputFile.getAbsolutePath());
+			BufferedReader r = new BufferedReader(new InputStreamReader(url.openStream()));
+			// BufferedReader r = new BufferedReader(new FileReader(inputFile));
+
+			String line;
+			try {
+				URL baseUrl = new URL(url, "./"); //$NON-NLS-1$
+				while ((line = r.readLine()) != null) {
+					if (line.startsWith("#")) //$NON-NLS-1$
+						continue;
+					line = line.trim();// symbolicName,version,location,startlevel,expectedState
+					if (line.length() == 0)
+						continue;
+
+					// (expectedState is an integer).
+					if (line.startsWith(PARAMETER_BASEURL + "=")) { //$NON-NLS-1$
+						String baseUrlSt = line.substring((PARAMETER_BASEURL + "=").length()); //$NON-NLS-1$
+						if (!baseUrlSt.endsWith("/")) //$NON-NLS-1$
+							baseUrlSt += "/"; //$NON-NLS-1$
+						baseUrl = new URL(url, baseUrlSt);
+						continue;
+					}
+					StringTokenizer tok = new StringTokenizer(line, ",", true); //$NON-NLS-1$
+					String symbolicName = tok.nextToken();
+					if (symbolicName.equals(",")) //$NON-NLS-1$
+						symbolicName = null;
+					else
+						tok.nextToken(); // ,
+
+					String version = tok.nextToken();
+					if (version.equals(",")) //$NON-NLS-1$
+						version = null;
+					else
+						tok.nextToken(); // ,
+
+					String urlSt = tok.nextToken();
+					if (urlSt.equals(",")) { //$NON-NLS-1$
+						if (symbolicName != null && version != null)
+							urlSt = symbolicName + "_" + version + ".jar"; //$NON-NLS-1$ //$NON-NLS-2$
+						else
+							urlSt = null;
+					} else
+						tok.nextToken(); // ,
+					try {
+						new URL(urlSt);
+					} catch (MalformedURLException e) {
+						urlSt = Utils.getUrlInFull(urlSt, baseUrl).toExternalForm();
+					}
+
+					int sl = Integer.parseInt(tok.nextToken().trim());
+					tok.nextToken(); // ,
+					boolean markedAsStarted = Boolean.valueOf(tok.nextToken()).booleanValue();
+
+					BundleInfo bInfo = new BundleInfo(symbolicName, version, urlSt, sl, markedAsStarted);
+					bundles.add(bInfo);
+				}
+			} finally {
+				try {
+					r.close();
+				} catch (IOException ex) {
+					// ignore
+				}
+			}
+		} catch (MalformedURLException e) {
+			// TODO log something
+		}
+		return (BundleInfo[]) bundles.toArray(new BundleInfo[bundles.size()]);
+	}
+
+	/**
+	 * 
+	 * @param configurationLocation configuration file (i.e. config.ini).
+	 * @param executableLocation executable file (i.e. eclipse.exe). 
+	 */
+	public DataLoader(File configurationLocation, File executableLocation) {
+		this.configurationLocation = configurationLocation;
+		initializeFrameworkManipulator(configurationLocation.getParentFile(), executableLocation);
+	}
+
+	private void initializeFrameworkManipulator(File config, File executable) {
+		getFrameworkManipulator();
+
+		LauncherData launcherData = manipulator.getLauncherData();
+		launcherData.setFwPersistentDataLocation(config, true);
+		launcherData.setLauncher(executable);
+		try {
+			manipulator.load();
+		} catch (IllegalStateException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} catch (FrameworkAdminRuntimeException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+	}
+
+	public ConfigData getConfigData() {
+		if (manipulator == null)
+			return null;
+
+		EquinoxFwConfigFileParser parser = new EquinoxFwConfigFileParser(Activator.getContext());
+		try {
+			if (configurationLocation != null && configurationLocation.exists())
+				parser.readFwConfig(manipulator, configurationLocation);
+		} catch (IOException e) {
+			LogHelper.log(new Status(IStatus.ERROR, Activator.ID, "Error loading config.", e)); //$NON-NLS-1$ //TODO: Fix error string
+		}
+		ConfigData data = manipulator.getConfigData();
+		String value = data.getFwIndependentProp(ORG_ECLIPSE_EQUINOX_SIMPLECONFIGURATOR_CONFIGURL);
+		if (value != null) {
+			try {
+				//config.ini uses simpleconfigurator, read the bundles.info and replace the bundle infos
+				BundleInfo[] bundleInfos = readConfiguration(new URL(value));
+				data.setBundles(bundleInfos);
+			} catch (MalformedURLException e1) {
+				// ignore
+			} catch (IOException e1) {
+				// ignore
+			}
+			try {
+				data.setFwIndependentProp(ORG_ECLIPSE_EQUINOX_SIMPLECONFIGURATOR_CONFIGURL, EquinoxManipulatorImpl.makeRelative(value, configurationLocation.toURL()));
+			} catch (MalformedURLException e) {
+				//ignore
+			}
+		}
+
+		return data;
+	}
+
+	public LauncherData getLauncherData() {
+		return manipulator == null ? null : manipulator.getLauncherData();
+	}
+
+	/**
+	 * Obtains the framework manipulator instance. Throws an exception
+	 * if it could not be created.
+	 */
+	private void getFrameworkManipulator() {
+		FrameworkAdmin admin = getFrameworkAdmin();
+		if (admin == null)
+			throw new RuntimeException("Framework admin service not found"); //$NON-NLS-1$
+		manipulator = admin.getManipulator();
+		if (manipulator == null)
+			throw new RuntimeException("Framework manipulator not found"); //$NON-NLS-1$
+	}
+
+	private FrameworkAdmin getFrameworkAdmin() {
+		FrameworkAdmin frameworkAdmin = (FrameworkAdmin) ServiceHelper.getService(Activator.getContext(), FrameworkAdmin.class.getName(), frameworkAdminFillter);
+		if (frameworkAdmin == null) {
+			startBundle(ORG_ECLIPSE_EQUINOX_FRAMEWORKADMIN_EQUINOX);
+			startBundle(ORG_ECLIPSE_EQUINOX_SIMPLECONFIGURATOR_MANIPULATOR);
+			frameworkAdmin = (FrameworkAdmin) ServiceHelper.getService(Activator.getContext(), FrameworkAdmin.class.getName(), frameworkAdminFillter);
+		}
+		return frameworkAdmin;
+	}
+
+	private boolean startBundle(String bundleId) {
+		PackageAdmin packageAdmin = (PackageAdmin) ServiceHelper.getService(Activator.getContext(), PackageAdmin.class.getName());
+		if (packageAdmin == null)
+			return false;
+
+		Bundle[] bundles = packageAdmin.getBundles(bundleId, null);
+		if (bundles != null && bundles.length > 0) {
+			for (int i = 0; i < bundles.length; i++) {
+				try {
+					if ((bundles[0].getState() & Bundle.RESOLVED) > 0) {
+						bundles[0].start();
+						return true;
+					}
+				} catch (BundleException e) {
+					// failed, try next bundle
+				}
+			}
+		}
+		return false;
+	}
+}
---
+++ bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/p2/publisher/eclipse/ConfigCUsAction.java
@@ -0,0 +1,378 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Code 9 and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   Code 9 - initial API and implementation
+ *   IBM - ongoing development
+ ******************************************************************************/
+package org.eclipse.equinox.p2.publisher.eclipse;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+import java.util.Map.Entry;
+import org.eclipse.core.runtime.*;
+import org.eclipse.equinox.internal.p2.publisher.eclipse.GeneratorBundleInfo;
+import org.eclipse.equinox.internal.provisional.frameworkadmin.BundleInfo;
+import org.eclipse.equinox.internal.provisional.p2.metadata.*;
+import org.eclipse.equinox.internal.provisional.p2.metadata.MetadataFactory.InstallableUnitDescription;
+import org.eclipse.equinox.internal.provisional.p2.metadata.query.InstallableUnitQuery;
+import org.eclipse.equinox.internal.provisional.p2.query.Collector;
+import org.eclipse.equinox.internal.provisional.p2.query.Query;
+import org.eclipse.equinox.p2.publisher.*;
+import org.eclipse.equinox.spi.p2.publisher.PublisherHelper;
+import org.eclipse.osgi.util.ManifestElement;
+import org.osgi.framework.*;
+
+/**
+ * Publish CUs for all the configuration data in the current result.
+ * This adds config-specific CUs to capture start levels etc found in the config.ini
+ * etc for is os, ws, arch combination seen so far.
+ */
+public class ConfigCUsAction extends AbstractPublisherAction {
+
+	protected static final String ORG_ECLIPSE_UPDATE_CONFIGURATOR = "org.eclipse.update.configurator"; //$NON-NLS-1$
+	private static Collection PROPERTIES_TO_SKIP;
+	private static HashSet PROGRAM_ARGS_TO_SKIP;
+	protected Version version;
+	protected String id;
+	protected String flavor;
+
+	// TODO consider moving this filtering to the LaunchingAdvice and ConfigAdvice so 
+	// it is not hardcoded in the action.
+	static {
+		PROPERTIES_TO_SKIP = new HashSet();
+		PROPERTIES_TO_SKIP.add("osgi.frameworkClassPath"); //$NON-NLS-1$
+		PROPERTIES_TO_SKIP.add("osgi.framework"); //$NON-NLS-1$
+		PROPERTIES_TO_SKIP.add("osgi.bundles"); //$NON-NLS-1$
+		PROPERTIES_TO_SKIP.add("eof"); //$NON-NLS-1$
+		PROPERTIES_TO_SKIP.add("eclipse.p2.profile"); //$NON-NLS-1$
+		PROPERTIES_TO_SKIP.add("eclipse.p2.data.area"); //$NON-NLS-1$
+		PROPERTIES_TO_SKIP.add("osgi.launcherPath"); //$NON-NLS-1$
+		PROPERTIES_TO_SKIP.add("org.eclipse.update.reconcile"); //$NON-NLS-1$
+		PROPERTIES_TO_SKIP.add("org.eclipse.equinox.simpleconfigurator.configUrl"); //$NON-NLS-1$
+
+		PROGRAM_ARGS_TO_SKIP = new HashSet();
+		PROGRAM_ARGS_TO_SKIP.add("--launcher.library"); //$NON-NLS-1$
+		PROGRAM_ARGS_TO_SKIP.add("-startup"); //$NON-NLS-1$
+		PROGRAM_ARGS_TO_SKIP.add("-configuration"); //$NON-NLS-1$
+	}
+
+	public static String getCUId(String id, String type, String flavor, String configSpec) {
+		return flavor + id + "." + type + "." + AbstractPublisherAction.createIdString(configSpec); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	public static String getAbstractCUCapabilityNamespace(String id, String type, String flavor, String configSpec) {
+		return flavor + id;
+	}
+
+	public static String getAbstractCUCapabilityId(String id, String type, String flavor, String configSpec) {
+		return id + "." + type;
+	}
+
+	/**
+	 * Returns the id of the top level IU published by this action for the given id and flavor.
+	 * @param id the id of the application being published
+	 * @param flavor the flavor being published
+	 * @return the if for ius published by this action
+	 */
+	public static String computeIUId(String id, String flavor) {
+		return flavor + id + ".configuration"; //$NON-NLS-1$
+	}
+
+	public ConfigCUsAction(IPublisherInfo info, String flavor, String id, Version version) {
+		this.flavor = flavor;
+		this.id = id;
+		this.version = version;
+	}
+
+	public IStatus perform(IPublisherInfo info, IPublisherResult results, IProgressMonitor monitor) {
+		IPublisherResult innerResult = new PublisherResult();
+		// we have N platforms, generate a CU for each
+		// TODO try and find common properties across platforms
+		String[] configSpecs = info.getConfigurations();
+		for (int i = 0; i < configSpecs.length; i++) {
+			if (monitor.isCanceled())
+				return Status.CANCEL_STATUS;
+			String configSpec = configSpecs[i];
+			Collection configAdvice = info.getAdvice(configSpec, false, null, null, IConfigAdvice.class);
+			BundleInfo[] bundles = fillInBundles(configAdvice, results);
+			publishBundleCUs(info, bundles, configSpec, innerResult);
+			publishConfigIUs(configAdvice, innerResult, configSpec);
+			Collection launchingAdvice = info.getAdvice(configSpec, false, null, null, ILaunchingAdvice.class);
+			publishIniIUs(launchingAdvice, innerResult, configSpec);
+		}
+		// merge the IUs  into the final result as non-roots and create a parent IU that captures them all
+		results.merge(innerResult, IPublisherResult.MERGE_ALL_NON_ROOT);
+		publishTopLevelConfigurationIU(innerResult.getIUs(null, IPublisherResult.ROOT), results);
+		return Status.OK_STATUS;
+	}
+
+	private void publishTopLevelConfigurationIU(Collection children, IPublisherResult result) {
+		InstallableUnitDescription descriptor = createParentIU(children, computeIUId(id, flavor), version);
+		descriptor.setSingleton(true);
+		IInstallableUnit rootIU = MetadataFactory.createInstallableUnit(descriptor);
+		if (rootIU == null)
+			return;
+		result.addIU(rootIU, IPublisherResult.ROOT);
+	}
+
+	// there seem to be cases where the bundle infos are not filled in with symbolic name and version.
+	// fill in the missing data.
+	private BundleInfo[] fillInBundles(Collection configAdvice, IPublisherResult results) {
+		ArrayList result = new ArrayList();
+		for (Iterator j = configAdvice.iterator(); j.hasNext();) {
+			IConfigAdvice advice = (IConfigAdvice) j.next();
+			BundleInfo[] bundles = advice.getBundles();
+			for (int i = 0; i < bundles.length; i++) {
+				BundleInfo bundleInfo = bundles[i];
+				// prime the result with the current info.  This will be replaced if there is more info...
+				if ((bundleInfo.getSymbolicName() != null && bundleInfo.getVersion() != null) || bundleInfo.getLocation() == null)
+					result.add(bundles[i]);
+				else {
+					try {
+						File location = new File(new URL(bundleInfo.getLocation()).getPath());
+						Dictionary manifest = BundlesAction.loadManifest(location);
+						if (manifest == null)
+							continue;
+						GeneratorBundleInfo newInfo = new GeneratorBundleInfo(bundleInfo);
+						ManifestElement[] element = ManifestElement.parseHeader("dummy-bsn", (String) manifest.get(Constants.BUNDLE_SYMBOLICNAME)); //$NON-NLS-1$
+						newInfo.setSymbolicName(element[0].getValue());
+						newInfo.setVersion((String) manifest.get(Constants.BUNDLE_VERSION));
+						result.add(newInfo);
+					} catch (BundleException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return (BundleInfo[]) result.toArray(new BundleInfo[result.size()]);
+	}
+
+	/**
+	 * Publish the IUs that capture the eclipse.ini information such as vmargs and program args, etc
+	 */
+	private void publishIniIUs(Collection launchingAdvice, IPublisherResult results, String configSpec) {
+		if (launchingAdvice.isEmpty())
+			return;
+
+		String configureData = ""; //$NON-NLS-1$
+		String unconfigureData = ""; //$NON-NLS-1$
+		if (!launchingAdvice.isEmpty()) {
+			String[] dataStrings = getLauncherConfigStrings(launchingAdvice);
+			configureData += dataStrings[0];
+			unconfigureData += dataStrings[1];
+		}
+		// if there is nothing to configure or unconfigure, then don't even bother generating this IU
+		if (configureData.length() == 0 && unconfigureData.length() == 0)
+			return;
+
+		Map touchpointData = new HashMap();
+		touchpointData.put("configure", configureData); //$NON-NLS-1$
+		touchpointData.put("unconfigure", unconfigureData); //$NON-NLS-1$
+		IInstallableUnit cu = createCU(id, version, "ini", flavor, configSpec, touchpointData); //$NON-NLS-1$
+		results.addIU(cu, IPublisherResult.ROOT);
+	}
+
+	/**
+	 * Publish the IUs that capture the config.ini information such as properties etc
+	 */
+	private void publishConfigIUs(Collection configAdvice, IPublisherResult results, String configSpec) {
+		if (configAdvice.isEmpty())
+			return;
+
+		String configureData = ""; //$NON-NLS-1$
+		String unconfigureData = ""; //$NON-NLS-1$
+		if (!configAdvice.isEmpty()) {
+			String[] dataStrings = getConfigurationStrings(configAdvice);
+			configureData += dataStrings[0];
+			unconfigureData += dataStrings[1];
+		}
+		// if there is nothing to configure or unconfigure, then don't even bother generating this IU
+		if (configureData.length() == 0 && unconfigureData.length() == 0)
+			return;
+
+		Map touchpointData = new HashMap();
+		touchpointData.put("configure", configureData); //$NON-NLS-1$
+		touchpointData.put("unconfigure", unconfigureData); //$NON-NLS-1$
+		IInstallableUnit cu = createCU(id, version, "config", flavor, configSpec, touchpointData); //$NON-NLS-1$
+		results.addIU(cu, IPublisherResult.ROOT);
+	}
+
+	/**
+	 * Create a CU whose id is flavor+id.type.configspec with the given version. 
+	 * The resultant IU has the self capability and an abstract capabilty in the flavor+id namespace
+	 * with the name id.type and the given version.  This allows others to create an abstract
+	 * dependency on having one of these things around but not having to list out the configs.
+	 */
+	private IInstallableUnit createCU(String id, Version version, String type, String flavor, String configSpec, Map touchpointData) {
+		InstallableUnitDescription cu = new InstallableUnitDescription();
+		String resultId = getCUId(id, type, flavor, configSpec);
+		cu.setId(resultId);
+		cu.setVersion(version);
+		cu.setFilter(AbstractPublisherAction.createFilterSpec(configSpec));
+		cu.setProperty(IInstallableUnit.PROP_TYPE_FRAGMENT, Boolean.TRUE.toString());
+		ProvidedCapability selfCapability = PublisherHelper.createSelfCapability(resultId, version);
+		String namespace = getAbstractCUCapabilityNamespace(id, type, flavor, configSpec);
+		String abstractId = getAbstractCUCapabilityId(id, type, flavor, configSpec);
+		ProvidedCapability abstractCapability = MetadataFactory.createProvidedCapability(namespace, abstractId, version);
+		cu.setCapabilities(new ProvidedCapability[] {selfCapability, abstractCapability});
+		cu.addTouchpointData(MetadataFactory.createTouchpointData(touchpointData));
+		cu.setTouchpointType(PublisherHelper.TOUCHPOINT_OSGI);
+		return MetadataFactory.createInstallableUnit(cu);
+	}
+
+	protected String[] getConfigurationStrings(Collection configAdvice) {
+		String configurationData = ""; //$NON-NLS-1$
+		String unconfigurationData = ""; //$NON-NLS-1$
+		for (Iterator i = configAdvice.iterator(); i.hasNext();) {
+			IConfigAdvice advice = (IConfigAdvice) i.next();
+			for (Iterator iterator = advice.getProperties().entrySet().iterator(); iterator.hasNext();) {
+				Entry aProperty = (Entry) iterator.next();
+				String key = ((String) aProperty.getKey());
+				if (shouldPublishProperty(key)) {
+					configurationData += "setProgramProperty(propName:" + key + ", propValue:" + ((String) aProperty.getValue()) + ");"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+					unconfigurationData += "setProgramProperty(propName:" + key + ", propValue:);"; //$NON-NLS-1$ //$NON-NLS-2$
+				}
+			}
+		}
+		return new String[] {configurationData, unconfigurationData};
+	}
+
+	private boolean shouldPublishProperty(String key) {
+		return !PROPERTIES_TO_SKIP.contains(key);
+	}
+
+	private boolean shouldPublishJvmArg(String key) {
+		return true;
+	}
+
+	private boolean shouldPublishProgramArg(String key) {
+		return !PROGRAM_ARGS_TO_SKIP.contains(key);
+	}
+
+	protected String[] getLauncherConfigStrings(Collection launchingAdvice) {
+		String configurationData = ""; //$NON-NLS-1$
+		String unconfigurationData = ""; //$NON-NLS-1$
+
+		for (Iterator j = launchingAdvice.iterator(); j.hasNext();) {
+			ILaunchingAdvice advice = (ILaunchingAdvice) j.next();
+			String[] jvmArgs = advice.getVMArguments();
+			for (int i = 0; i < jvmArgs.length; i++)
+				if (shouldPublishJvmArg(jvmArgs[i])) {
+					configurationData += "addJvmArg(jvmArg:" + jvmArgs[i] + ");"; //$NON-NLS-1$ //$NON-NLS-2$
+					unconfigurationData += "removeJvmArg(jvmArg:" + jvmArgs[i] + ");"; //$NON-NLS-1$ //$NON-NLS-2$
+				}
+			String[] programArgs = advice.getProgramArguments();
+			for (int i = 0; i < programArgs.length; i++)
+				if (shouldPublishProgramArg(programArgs[i])) {
+					configurationData += "addProgramArg(programArg:" + programArgs[i] + ");"; //$NON-NLS-1$ //$NON-NLS-2$
+					unconfigurationData += "removeProgramArg(programArg:" + programArgs[i] + ");"; //$NON-NLS-1$ //$NON-NLS-2$
+				} else
+					// if we are not publishing then skip over the following arg as it is assumed to be a parameter
+					// to this command line arg.
+					i++;
+		}
+		return new String[] {configurationData, unconfigurationData};
+	}
+
+	/**
+	 * Publish the CUs related to the given set of bundles.  This generally covers the start-level and 
+	 * and whether or not the bundle is to be started.
+	 */
+	protected void publishBundleCUs(IPublisherInfo info, BundleInfo[] bundles, String configSpec, IPublisherResult result) {
+		if (bundles == null)
+			return;
+
+		String cuIdPrefix = ""; //$NON-NLS-1$
+		String filter = null;
+		if (configSpec != null) {
+			cuIdPrefix = createIdString(configSpec);
+			filter = createFilterSpec(configSpec);
+		}
+
+		for (int i = 0; i < bundles.length; i++) {
+			GeneratorBundleInfo bundle = createGeneratorBundleInfo(info, bundles[i], result);
+			if (bundle == null)
+				continue;
+
+			// TODO need to factor this out into its own action
+			if (bundle.getSymbolicName().equals(ORG_ECLIPSE_UPDATE_CONFIGURATOR)) {
+				bundle.setStartLevel(BundleInfo.NO_LEVEL);
+				bundle.setMarkedAsStarted(false);
+				bundle.setSpecialConfigCommands("setProgramProperty(propName:org.eclipse.update.reconcile, propValue:false);"); //$NON-NLS-1$
+				bundle.setSpecialUnconfigCommands("setProgramProperty(propName:org.eclipse.update.reconcile, propValue:);"); //$NON-NLS-1$
+			} else if (bundle.getStartLevel() == BundleInfo.NO_LEVEL && !bundle.isMarkedAsStarted()) {
+				// this bundle does not require any particular configuration, the plug-in default IU will handle installing it
+				continue;
+			}
+
+			IInstallableUnit cu = BundlesAction.createBundleConfigurationUnit(bundle.getSymbolicName(), new Version(bundle.getVersion()), false, bundle, flavor + cuIdPrefix, filter);
+			if (cu != null) {
+				// Product Query will run against the repo, make sure these CUs are in before then
+				// TODO review the aggressive addition to the metadata repo.  perhaps the query can query the result as well.
+				//				IMetadataRepository metadataRepository = info.getMetadataRepository();
+				//				if (metadataRepository != null) {
+				//					metadataRepository.addInstallableUnits(new IInstallableUnit[] {cu});
+				//				}
+				result.addIU(cu, IPublisherResult.ROOT);
+			}
+		}
+	}
+
+	protected GeneratorBundleInfo createGeneratorBundleInfo(IPublisherInfo info, BundleInfo bundleInfo, IPublisherResult result) {
+		if (bundleInfo.getLocation() != null)
+			return new GeneratorBundleInfo(bundleInfo);
+
+		String name = bundleInfo.getSymbolicName();
+
+		//easy case: do we have a matching IU?
+		IInstallableUnit iu = result.getIU(name, null);
+		if (iu != null) {
+			bundleInfo.setVersion(iu.getVersion().toString());
+			return new GeneratorBundleInfo(bundleInfo);
+		}
+
+		//harder: try id_version
+		int i = name.indexOf('_');
+		while (i > -1) {
+			Version version = null;
+			try {
+				version = new Version(name.substring(i));
+				bundleInfo.setSymbolicName(name.substring(0, i));
+				bundleInfo.setVersion(version.toString());
+				return new GeneratorBundleInfo(bundleInfo);
+			} catch (IllegalArgumentException e) {
+				// the '_' found was probably part of the symbolic id
+				i = name.indexOf('_', i);
+			}
+		}
+
+		//Query the repo
+		Query query = new InstallableUnitQuery(name);
+		Collector collector = new Collector();
+		Iterator matches = info.getMetadataRepository().query(query, collector, null).iterator();
+		//pick the newest match
+		IInstallableUnit newest = null;
+		while (matches.hasNext()) {
+			IInstallableUnit candidate = (IInstallableUnit) matches.next();
+			if (newest == null || (newest.getVersion().compareTo(candidate.getVersion()) < 0))
+				newest = candidate;
+		}
+		if (newest != null) {
+			bundleInfo.setVersion(newest.getVersion().toString());
+			return new GeneratorBundleInfo(bundleInfo);
+		}
+
+		return null;
+	}
+
+}
