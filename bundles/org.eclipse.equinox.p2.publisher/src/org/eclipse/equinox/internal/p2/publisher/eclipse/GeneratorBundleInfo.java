8528f3e825445748989e045bee0bf161286865a3
John Arthorne <johna>
2008-12-12 21:12:09 +0000

    Code cleanup

--- bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/internal/p2/publisher/eclipse/GeneratorBundleInfo.java
+++ bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/internal/p2/publisher/eclipse/GeneratorBundleInfo.java
@@ -13,15 +13,6 @@ package org.eclipse.equinox.internal.p2.publisher.eclipse;
 import org.eclipse.equinox.internal.provisional.frameworkadmin.BundleInfo;
 
 public class GeneratorBundleInfo extends BundleInfo {
-	//	public static final int NO_LEVEL = -1;
-
-	//	private String symbolicName;
-	//	private String version;
-	//	private String location;
-	//	private int expectedState;
-	//	private int startLevel = NO_LEVEL;
-	//	private String manifest;
-
 	private String specialConfigCommands;
 	private String specialUnconfigCommands;
 
@@ -37,67 +28,9 @@ public class GeneratorBundleInfo extends BundleInfo {
 	}
 
 	public GeneratorBundleInfo() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
-	//	/* (non-Javadoc)
-	//	 * @see java.lang.Object#hashCode()
-	//	 */
-	//	public int hashCode() {
-	//		int result = symbolicName == null ? 0 : symbolicName.hashCode();
-	//		result = result + (version == null ? 0 : version.hashCode());
-	//		result = result + (location == null ? 0 : location.hashCode());
-	//		return result;
-	//	}
-	//
-	//	public String getSymbolicName() {
-	//		return symbolicName;
-	//	}
-	//
-	//	public String getVersion() {
-	//		return version;
-	//	}
-	//
-	//	public int expectedState() {
-	//		return expectedState;
-	//	}
-	//
-	//	public int getStartLevel() {
-	//		return startLevel;
-	//	}
-	//
-	//	public String getLocation() {
-	//		return location;
-	//	}
-	//
-	//	public void setSymbolicName(String id) {
-	//		symbolicName = id;
-	//	}
-	//
-	//	public void setVersion(String version) {
-	//		this.version = version;
-	//	}
-	//
-	//	public void setExpectedState(int state) {
-	//		expectedState = state;
-	//	}
-	//
-	//	public void setStartLevel(int level) {
-	//		this.startLevel = level;
-	//	}
-	//
-	//	public void setLocation(String location) {
-	//		this.location = location;
-	//	}
-	//
-	//	public void setManifest(String manifest) {
-	//		this.manifest = manifest;
-	//	}
-	//	
-	//	public String getManifest() {
-	//		return manifest;
-	//	}
-	//	
 	public String getSpecialConfigCommands() {
 		return specialConfigCommands;
 	}
@@ -113,18 +46,6 @@ public class GeneratorBundleInfo extends BundleInfo {
 	public void setSpecialUnconfigCommands(String specialUnconfigCommands) {
 		this.specialUnconfigCommands = specialUnconfigCommands;
 	}
-
-	//	/* (non-Javadoc)
-	//	 * @see java.lang.Object#equals(java.lang.Object)
-	//	 */
-	//	public boolean equals(Object toCompare) {
-	//		if (toCompare instanceof GeneratorBundleInfo) {
-	//			GeneratorBundleInfo info = (GeneratorBundleInfo) toCompare;
-	//			if (info.symbolicName.equals(symbolicName) && info.version.equals(version) && (info.location == null || location == null ? true : info.location.equals(location)))
-	//				return true;
-	//		}
-	//		return false;
-	//	}
 
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
