ed8207857b6c6029ada78ee97cc153c2cc68c53c
John Arthorne <johna>
2009-01-08 19:41:21 +0000

    Fixed javadoc error

--- bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/provisional/p2/ui/operations/ProvisioningOperation.java
+++ bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/provisional/p2/ui/operations/ProvisioningOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * Copyright (c) 2007, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -46,8 +46,6 @@ public abstract class ProvisioningOperation {
 	 * 
 	 * @param monitor
 	 *            the progress monitor to use for the operation
-	 * @param shell
-	 *            the shell to use for reporting errors
 	 * @throws ProvisionException
 	 *             propagates any ProvisionException thrown
 	 * 
