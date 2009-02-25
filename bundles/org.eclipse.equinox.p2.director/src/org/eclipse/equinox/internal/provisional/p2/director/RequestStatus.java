384172b4a8b66bd2e239988b2ca94f59cccde31e
Pascal Rapicault <prapicau>
2009-02-25 02:48:29 +0000

    Add support for getting explanation

--- bundles/org.eclipse.equinox.p2.director/src/org/eclipse/equinox/internal/provisional/p2/director/RequestStatus.java
+++ bundles/org.eclipse.equinox.p2.director/src/org/eclipse/equinox/internal/provisional/p2/director/RequestStatus.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others. All rights reserved. This
+ * Copyright (c) 2008-2009 IBM Corporation and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -9,6 +9,8 @@
  ******************************************************************************/
 package org.eclipse.equinox.internal.provisional.p2.director;
 
+import java.util.Collections;
+import java.util.Set;
 import org.eclipse.equinox.internal.provisional.p2.metadata.IInstallableUnit;
 
 public class RequestStatus {
@@ -35,5 +37,23 @@ public class RequestStatus {
 
 	public int getSeverity() {
 		return severity;
+	}
+
+	//Return the already installed roots with which this IU is in conflict
+	//Return an empty set if there is no conflict
+	public Set getConflictsWithInstalledRoots() {
+		return Collections.EMPTY_SET;
+	}
+
+	//Return the already installed roots with which this IU is in conflict
+	//Return an empty set if there is no conflict
+	public Set getConflictsWithAnyRoots() {
+		return Collections.EMPTY_SET;
+	}
+
+	//Return an explanation as to why this IU can not be resolved.
+	public Set getExplanations() {
+		//To start with, this does not have to return the most specific explanation. If it simply returns an global explanation it is good enough.
+		return null;
 	}
 }
