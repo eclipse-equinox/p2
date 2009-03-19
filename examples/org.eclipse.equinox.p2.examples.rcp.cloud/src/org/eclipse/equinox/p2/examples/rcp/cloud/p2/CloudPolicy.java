38975efbe1ed2b17493745b9971058261a9da105
Susan Franklin <sfranklin>
2009-03-19 23:22:30 +0000

    view by category when no dialog setting present

--- examples/org.eclipse.equinox.p2.examples.rcp.cloud/src/org/eclipse/equinox/p2/examples/rcp/cloud/p2/CloudPolicy.java
+++ examples/org.eclipse.equinox.p2.examples.rcp.cloud/src/org/eclipse/equinox/p2/examples/rcp/cloud/p2/CloudPolicy.java
@@ -10,12 +10,12 @@
  *******************************************************************************/
 package org.eclipse.equinox.p2.examples.rcp.cloud.p2;
 
+import org.eclipse.equinox.internal.provisional.p2.ui.policy.IUViewQueryContext;
 import org.eclipse.equinox.internal.provisional.p2.ui.policy.Policy;
 
 /**
- * CloudPolicy defines the RCP Cloud Example policies for the
- * p2 UI.  The policy is declared as an OSGi service in
- * the policy_component.xml file.
+ * CloudPolicy defines the RCP Cloud Example policies for the p2 UI. The policy
+ * is declared as an OSGi service in the policy_component.xml file.
  * 
  * @since 3.5
  */
@@ -23,5 +23,10 @@ public class CloudPolicy extends Policy {
 	public CloudPolicy() {
 		// XXX User has no access to manipulate repositories
 		setRepositoryManipulator(null);
-}
+
+		// XXX Default view is by category
+		IUViewQueryContext queryContext = new IUViewQueryContext(
+				IUViewQueryContext.AVAILABLE_VIEW_BY_CATEGORY);
+		setQueryContext(queryContext);
+	}
 }
