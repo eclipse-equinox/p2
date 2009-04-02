ab40d2b37b440ea89b501e86558e8c85244a6037
John Arthorne <johna>
2009-04-02 13:49:52 +0000

    Removed unused messages

--- bundles/org.eclipse.equinox.p2.repository/src/org/eclipse/equinox/internal/p2/repository/Messages.java
+++ bundles/org.eclipse.equinox.p2.repository/src/org/eclipse/equinox/internal/p2/repository/Messages.java
@@ -17,45 +17,12 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.equinox.internal.p2.repository.messages"; //$NON-NLS-1$
 
 	public static String artifact_not_found;
-	public static String available_already_in;
-	public static String downloading;
-	public static String error_closing_stream;
 	public static String io_failedRead;
 	public static String ecf_configuration_error;
-	public static String io_incompatibleVersion;
-	public static String io_invalidLocation;
-	public static String SignatureVerification_failedRead;
-	public static String SignatureVerification_invalidContent;
-	public static String SignatureVerification_invalidFileContent;
-
-	public static String SignatureVerifier_OutOfMemory;
-	public static String io_parseError;
-	public static String mirroring;
 	public static String repoMan_internalError;
-	public static String repoFailedWrite;
-	public static String repoReadOnly;
 	public static String repo_loading;
 
-	public static String sar_downloading;
-	public static String sar_downloadJobName;
-	public static String sar_failedMkdir;
-	public static String sar_reportStatus;
-
-	public static String mirror_alreadyExists;
-	public static String message_mirroringStatus;
-	public static String message_childrenRepos;
-	public static String exception_comparatorNotFound;
-	public static String exception_noComparators;
-	public static String exception_destinationNotModifiable;
-	public static String exception_needSourceDestination;
 	public static String exception_malformedRepoURI;
-	public static String exception_unsupportedAddToComposite;
-
-	public static String exception_unsupportedGetOutputStream;
-	public static String exception_unsupportedRemoveFromComposite;
-
-	public static String Mirroring_NO_MATCHING_DESCRIPTOR;
-
 	public static String TransportErrorTranslator_400;
 	public static String TransportErrorTranslator_401;
 	public static String TransportErrorTranslator_402;
