d1f06e6c8e39312e74708845b923ab6056550d6c
Pascal Rapicault <prapicau>
2009-04-17 01:34:27 +0000

    Bug 271954 - [planner] The installer plan removes unexpected IUs

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/AllTests.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/AllTests.java
@@ -38,6 +38,7 @@ public class AllTests extends TestCase {
 		suite.addTestSuite(Bug262580.class);
 		suite.addTestSuite(Bug270656.class);
 		suite.addTestSuite(Bug271067.class);
+		suite.addTestSuite(Bug271954.class);
 		suite.addTestSuite(DependencyOnSelf.class);
 		suite.addTestSuite(DropinsScenario.class);
 		suite.addTestSuite(ExplanationDeepConflict.class);
