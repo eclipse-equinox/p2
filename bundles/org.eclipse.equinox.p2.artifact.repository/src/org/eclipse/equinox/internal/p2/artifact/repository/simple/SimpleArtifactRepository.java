b231958edc36c8e03cd83df8526f375a94465389
John Arthorne <johna>
2009-04-29 16:22:00 +0000

    Update copyrights

--- bundles/org.eclipse.equinox.p2.artifact.repository/src/org/eclipse/equinox/internal/p2/artifact/repository/simple/SimpleArtifactRepository.java
+++ bundles/org.eclipse.equinox.p2.artifact.repository/src/org/eclipse/equinox/internal/p2/artifact/repository/simple/SimpleArtifactRepository.java
@@ -1,12 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others. All rights reserved. This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+ *  Copyright (c) 2007, 2009 IBM Corporation and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: IBM Corporation - initial API and implementation
- * 						Genuitec, LLC - support for multi-threaded downloads
- ******************************************************************************/
+ *  Contributors:
+ * 	IBM Corporation - initial API and implementation
+ * 	Genuitec, LLC - support for multi-threaded downloads
+ *******************************************************************************/
 package org.eclipse.equinox.internal.p2.artifact.repository.simple;
 
 import java.io.*;
