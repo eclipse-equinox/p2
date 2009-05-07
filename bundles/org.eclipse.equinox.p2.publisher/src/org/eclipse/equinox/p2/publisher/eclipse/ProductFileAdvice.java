85effbc7be4b14be6677e1bef3918417cbaf116f
Andrew Niefer <aniefer>
2009-05-07 20:55:56 +0000

    bug 262464 - custom config.ini on linux

--- bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/p2/publisher/eclipse/ProductFileAdvice.java
+++ bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/p2/publisher/eclipse/ProductFileAdvice.java
@@ -10,8 +10,6 @@
  ******************************************************************************/
 package org.eclipse.equinox.p2.publisher.eclipse;
 
-import org.eclipse.equinox.internal.provisional.p2.core.VersionedName;
-
 import java.io.File;
 import java.util.*;
 import org.eclipse.equinox.internal.p2.publisher.eclipse.DataLoader;
@@ -19,6 +17,7 @@ import org.eclipse.equinox.internal.p2.publisher.eclipse.IProductDescriptor;
 import org.eclipse.equinox.internal.provisional.frameworkadmin.BundleInfo;
 import org.eclipse.equinox.internal.provisional.frameworkadmin.ConfigData;
 import org.eclipse.equinox.internal.provisional.p2.core.Version;
+import org.eclipse.equinox.internal.provisional.p2.core.VersionedName;
 import org.eclipse.equinox.p2.publisher.AbstractAdvice;
 import org.eclipse.equinox.p2.publisher.AbstractPublisherAction;
 import org.eclipse.equinox.p2.publisher.actions.ILicenseAdvice;
@@ -275,8 +274,9 @@ public class ProductFileAdvice extends AbstractAdvice implements ILicenseAdvice,
 		File configFile = new File(location);
 
 		// We are assuming we are always relative from the product file
-		// However PDE tooling puts us relative from the workspace
-		if (!configFile.isAbsolute())
+		// However PDE tooling puts us relative from the workspace, that "relative" path also looks like an absolute path on linux
+		// Build may have copied the file to the correct place for us
+		if (!configFile.isAbsolute() || !configFile.exists())
 			configFile = new File(product.getLocation().getParentFile(), location);
 		// TODO need to figure out what to do for the launcher location here...
 		// for now just give any old path that has a parent
