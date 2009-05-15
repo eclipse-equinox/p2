a1432f4d30ae160aba4cdc7677cb4d182fd0610a
John Arthorne <johna>
2009-05-15 15:13:38 +0000

    Tests for bug 276457 Install fails without a trace using I20090514-2000.

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/repository/FileReaderTest.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/repository/FileReaderTest.java
@@ -11,11 +11,9 @@
 
 package org.eclipse.equinox.p2.tests.repository;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.*;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.*;
 import org.eclipse.equinox.internal.p2.repository.RepositoryTransport;
 import org.eclipse.equinox.p2.tests.testserver.helper.AbstractTestServerClientCase;
 
@@ -43,6 +41,30 @@ public class FileReaderTest extends AbstractTestServerClientCase {
 		assertEquals("Should be an error", status.getSeverity(), IStatus.ERROR);
 		assertTrue("Should be a connect exception", status.getException() instanceof ConnectException);
 		assertTrue("Should begin with 'Connection refused'", status.getException().getMessage().startsWith("Connection refused"));
+	}
+
+	/**
+	 * Tests a successful read.
+	 */
+	public void testReadStream() throws URISyntaxException, CoreException, IOException {
+		RepositoryTransport transport = RepositoryTransport.getInstance();
+		URI toDownload = new URI("http://localhost:8080/public/index.html");
+		final NullProgressMonitor monitor = new NullProgressMonitor();
+		InputStream stream = transport.stream(toDownload, monitor);
+		stream.close();
+		assertFalse("1.0", monitor.isCanceled());
+	}
+
+	/**
+	 * Tests a successful read.
+	 */
+	public void testRead() throws URISyntaxException, CoreException, IOException {
+		RepositoryTransport transport = RepositoryTransport.getInstance();
+		URI toDownload = new URI("http://localhost:8080/public/index.html");
+		OutputStream target = new ByteArrayOutputStream();
+		final NullProgressMonitor monitor = new NullProgressMonitor();
+		IStatus result = transport.download(toDownload, target, monitor);
+		assertTrue("1.0", result.isOK());
 	}
 	// TODO: test
 	// timeout, cancel of timeout (TimeoutTest)
