d693d6b1d803a5c46af1956921466284d53f662f
John Arthorne <johna>
2009-05-20 04:10:55 +0000

    Update copyrights

--- bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/internal/p2/publisher/eclipse/ExecutablesDescriptor.java
+++ bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/internal/p2/publisher/eclipse/ExecutablesDescriptor.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ *  Copyright (c) 2008, 2009 EclipseSource and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *      Eclipse Source - initial API and implementation
+ *      IBM Corporation - ongoing development
+ *******************************************************************************/
 package org.eclipse.equinox.internal.p2.publisher.eclipse;
 
 import java.io.*;
