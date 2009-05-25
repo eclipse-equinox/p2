b8516cbfa8c63ed012a0ccd26450ba3350fc5110
John Arthorne <johna>
2009-05-25 21:48:59 +0000

    Update copyrights

--- bundles/org.eclipse.equinox.p2.engine/src/org/eclipse/equinox/internal/provisional/p2/engine/IEngine.java
+++ bundles/org.eclipse.equinox.p2.engine/src/org/eclipse/equinox/internal/provisional/p2/engine/IEngine.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2008 Band XI International, LLC and others.
+ * Copyright (c) 2008, 2009 Band XI International, LLC and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Band XI - initial API and implementation
+ *   Band XI - initial API and implementation
+ *   IBM - ongoing development
  *******************************************************************************/
 package org.eclipse.equinox.internal.provisional.p2.engine;
 
