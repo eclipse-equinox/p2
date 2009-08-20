1cf21ee804cc2c30e2b1e4271585dcc3233c9be1
John Arthorne <johna>
2009-08-20 14:45:44 +0000

    Bug 286942 Test failures in NonMinimalState

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/NonMinimalState.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/NonMinimalState.java
@@ -36,7 +36,7 @@ public class NonMinimalState extends AbstractProvisioningTest {
 		File tempFolder = getTempFolder();
 		copy("0.2", reporegistry1, tempFolder);
 		SimpleProfileRegistry registry = new SimpleProfileRegistry(tempFolder, null, false);
-		profile = registry.getProfile("SDKProfile");
+		profile = registry.getProfile("NonMinimalState");
 		getMetadataRepositoryManager().addRepository(getTestData("nonMinimalState-galileoM7", "testData/galileoM7/").toURI());
 		assertNotNull(profile);
 	}
