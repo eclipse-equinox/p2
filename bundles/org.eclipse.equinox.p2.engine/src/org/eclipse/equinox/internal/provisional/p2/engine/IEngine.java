86bd021e478982e5d0b279d99b30071b13451225
John Arthorne <johna>
2009-10-07 20:54:11 +0000

    Added comment

--- bundles/org.eclipse.equinox.p2.engine/src/org/eclipse/equinox/internal/provisional/p2/engine/IEngine.java
+++ bundles/org.eclipse.equinox.p2.engine/src/org/eclipse/equinox/internal/provisional/p2/engine/IEngine.java
@@ -15,6 +15,9 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 
 public interface IEngine {
+	/**
+	 * Service name constant for the engine service.
+	 */
 	public static final String SERVICE_NAME = IEngine.class.getName();
 
 	public IStatus perform(IProfile profile, PhaseSet phaseSet, Operand[] operands, ProvisioningContext context, IProgressMonitor monitor);
