9bf07d634b4c5d0add69c68a75066acf7d4380fb
Susan Franklin <sfranklin>
2009-11-19 17:41:15 +0000

    javadoc cleanup

--- bundles/org.eclipse.equinox.p2.common/src/org/eclipse/equinox/p2/common/LicenseManager.java
+++ bundles/org.eclipse.equinox.p2.common/src/org/eclipse/equinox/p2/common/LicenseManager.java
@@ -14,34 +14,70 @@ import java.io.*;
 import org.eclipse.equinox.internal.provisional.p2.metadata.ILicense;
 
 /**
- * Abstract class for a license manager which tracks which 
- * licenses have been accepted in a provisioning client.
+ * LicenseManager defines a service which records the licenses that have been
+ * accepted in the course of installing or updating software. It can be used to determine
+ * whether a particular license should be presented to a user for acceptance, and
+ * to record the user's decision.
  * 
  * @since 2.0
  */
 public abstract class LicenseManager {
 
+	/**
+	 * Record the acceptance of the specified license.
+	 * 
+	 * @param license the license to be accepted
+	 * 
+	 * @return <code>true</code> if the license was recorded as accepted, <code>false</code> if
+	 * it was not.
+	 * 
+	 */
 	public abstract boolean accept(ILicense license);
 
+	/**
+	 * Record the rejection of the specified license.
+	 * 
+	 * @param license the license to be rejected
+	 * 
+	 * @return <code>true</code> if the license was recorded as rejected, <code>false</code> if
+	 * it was not.
+	 * 
+	 */
 	public abstract boolean reject(ILicense license);
 
+	/**
+	 * Return a boolean indicating whether a particular license has previously
+	 * been accepted.
+	 * 
+	 * @param license the license in question
+	 * 
+	 * @return <code>true</code> if the license has previously been accepted,
+	 * <code>false</code> if it has not been accepted before.
+	 * 
+	 */
 	public abstract boolean isAccepted(ILicense license);
 
+	/**
+	 * Return a boolean indicating whether any licenses have been
+	 * accepted.
+	 * 
+	 * @return <code>true</code> if accepted licenses have been recorded,
+	 * <code>false</code> if there have been no licenses accepted.
+
+	 */
 	public abstract boolean hasAcceptedLicenses();
 
 	/**
-	 * 
-	 * @param stream
+	 * Write the accepted licenses list to the specified stream.
+	 * @param stream the stream to which licenses should be written
 	 * @throws IOException 
-	 * @since 2.0
 	 */
 	public abstract void write(OutputStream stream) throws IOException;
 
 	/**
-	 * 
-	 * @param stream
+	 * Read the accepted licenses list from the specified stream.
+	 * @param stream the stream from which to read the licenses
 	 * @throws IOException 
-	 * @since 2.0
 	 */
 	public abstract void read(InputStream stream) throws IOException;
 
