2215514fb28bd9d3e6b06aba1932ba577b856691
John Arthorne <johna>
2010-01-13 21:36:44 +0000

    javadoc

--- bundles/org.eclipse.equinox.p2.metadata/src/org/eclipse/equinox/p2/query/IQuery.java
+++ bundles/org.eclipse.equinox.p2.metadata/src/org/eclipse/equinox/p2/query/IQuery.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,15 +11,22 @@
  *******************************************************************************/
 package org.eclipse.equinox.p2.query;
 
-
 import java.util.Iterator;
 
 /**
- * The superclass of all queries that can be performed on an {@link IQueryable}.
+ * The root interface for all queries that can be performed on an {@link IQueryable}.
+ * A query is a piece of logic that selects some objects from a list of provided
+ * inputs using some established criteria.
  * <p>
+ * Any given query must be stable - running the same query on the same inputs
+ * must return an equal query result each time the query is executed. Thus a client
+ * that has performed a query can freely cache the result as long as they know the
+ * query input has not changed.
+ * </p>
  * 
- * @noimplement This interface is not intended to be implemented by clients.
- * @noextend This interface is not intended to be extended by clients.
+ * @param <T> The type of input object that this query accepts
+ * @noimplement This interface is not intended to be implemented directly by clients.
+ * @noextend This interface is not intended to be extended directly by clients.
  * @since 2.0
  */
 public interface IQuery<T> {
@@ -28,19 +35,21 @@ public interface IQuery<T> {
 	 * Evaluates the query for a specific input.  
 	 * 
 	 * @param iterator The elements for which to evaluate the query on
-	 * @return The results of the query.  The collector returned must be
-	 * the collector passed in.
+	 * @return The results of the query.
 	 */
 	public abstract IQueryResult<T> perform(Iterator<T> iterator);
 
 	/**
-	 * Gets the ID for this Query. 
+	 * Gets a unique identifier for this query.
+	 * @return a unique identifier for this query
 	 */
 	public String getId();
 
 	/**
 	 * Gets a particular property of the query.
 	 * @param property The property to retrieve 
+	 * @return The value of the retrieved property, or <code>null</code>
+	 * if no such property is defined.
 	 */
 	public Object getProperty(String property);
 }
