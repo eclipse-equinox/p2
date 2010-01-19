d5eb4a55dc22ef69eaed87c6759b126e23e4547d
Pascal Rapicault <prapicau>
2010-01-19 16:47:43 +0000

    *** empty log message ***

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/AnotherSingleton.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/AnotherSingleton.java
@@ -10,20 +10,27 @@
  *******************************************************************************/
 package org.eclipse.equinox.p2.tests.planner;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.equinox.internal.provisional.p2.director.*;
-import org.eclipse.equinox.p2.engine.*;
-import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.metadata.Version;
+import org.eclipse.equinox.p2.metadata.VersionRange;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.equinox.internal.p2.metadata.IRequiredCapability;
+import org.eclipse.equinox.internal.provisional.p2.director.IPlanner;
+import org.eclipse.equinox.internal.provisional.p2.director.ProfileChangeRequest;
+import org.eclipse.equinox.internal.provisional.p2.metadata.*;
+import org.eclipse.equinox.p2.engine.IProfile;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.tests.AbstractProvisioningTest;
 
 public class AnotherSingleton extends AbstractProvisioningTest {
 	IInstallableUnit a1;
 	IInstallableUnit a2;
+	IInstallableUnit a3;
+	IInstallableUnit a4;
+	IInstallableUnit u, v, w, x, y, z;
 
 	IPlanner planner;
 	IProfile profile;
-	private IEngine engine;
 
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -31,41 +38,22 @@ public class AnotherSingleton extends AbstractProvisioningTest {
 
 		a2 = createIU("A", Version.create("2.0.0"), true);
 
-		createTestMetdataRepository(new IInstallableUnit[] {a1, a2});
+		IRequiredCapability c1 = (IRequiredCapability) MetadataFactory.createRequiredCapability(IInstallableUnit.NAMESPACE_IU_ID, "A", new VersionRange("[1.0.0, 1.0.0]"), null, false, false);
+		x = createIU("X", Version.createOSGi(2, 0, 0), new IRequiredCapability[] {c1});
+
+		IRequiredCapability c2 = (IRequiredCapability) MetadataFactory.createRequiredCapability(IInstallableUnit.NAMESPACE_IU_ID, "A", new VersionRange("[2.0.0, 2.0.0]"), null, false, false);
+		y = createIU("Y", Version.createOSGi(2, 0, 0), new IRequiredCapability[] {c2});
+
+		createTestMetdataRepository(new IInstallableUnit[] {a1, a2, x, y});
 
 		profile = createProfile("TestProfile." + getName());
 		planner = createPlanner();
-		engine = createEngine();
+
 	}
 
 	public void test1() {
 		ProfileChangeRequest req = new ProfileChangeRequest(profile);
-		req.addInstallableUnits(new IInstallableUnit[] {a1});
-		req.setInstallableUnitInclusionRules(a1, PlannerHelper.createOptionalInclusionRule(a1));
-		IProvisioningPlan plan = planner.getProvisioningPlan(req, null, null);
-		engine.perform(plan, new NullProgressMonitor());
-		assertProfileContains("foo", profile, new IInstallableUnit[] {a1});
-
-		ProfileChangeRequest req2 = new ProfileChangeRequest(profile);
-		req2.addInstallableUnits(new IInstallableUnit[] {a2});
-		req2.setInstallableUnitInclusionRules(a2, PlannerHelper.createOptionalInclusionRule(a2));
-		IProvisioningPlan plan2 = planner.getProvisioningPlan(req2, null, null);
-		engine.perform(plan2, new NullProgressMonitor());
-		assertProfileContains("foo", profile, new IInstallableUnit[] {a2});
-
-		ProfileChangeRequest req3 = new ProfileChangeRequest(profile);
-		req3.addInstallableUnits(new IInstallableUnit[] {a1});
-		req3.setInstallableUnitInclusionRules(a1, PlannerHelper.createOptionalInclusionRule(a1));
-		IProvisioningPlan plan3 = planner.getProvisioningPlan(req3, null, null);
-		engine.perform(plan3, new NullProgressMonitor());
-		assertProfileContains("foo", profile, new IInstallableUnit[] {a1}); //Here we really expect a2
-
-		ProfileChangeRequest req4 = new ProfileChangeRequest(profile);
-		req4.addInstallableUnits(new IInstallableUnit[] {a2});
-		req4.setInstallableUnitInclusionRules(a2, PlannerHelper.createOptionalInclusionRule(a2));
-		IProvisioningPlan plan4 = planner.getProvisioningPlan(req4, null, null);
-		engine.perform(plan4, new NullProgressMonitor());
-		assertProfileContains("foo", profile, new IInstallableUnit[] {a2});
-
+		req.addInstallableUnits(new IInstallableUnit[] {x, y});
+		assertEquals(IStatus.ERROR, planner.getProvisioningPlan(req, null, null).getStatus().getSeverity());
 	}
 }
