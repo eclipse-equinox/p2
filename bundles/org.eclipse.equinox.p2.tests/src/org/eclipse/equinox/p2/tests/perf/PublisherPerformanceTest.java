eaf1ceaeaeff4b116b13353bd3cac618728c2ce3
John Arthorne <johna>
2010-01-26 16:48:54 +0000

    Perf test for bug 300417

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/perf/PublisherPerformanceTest.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/perf/PublisherPerformanceTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,6 +16,7 @@ import org.eclipse.equinox.p2.metadata.query.InstallableUnitQuery;
 import org.eclipse.equinox.p2.publisher.IPublisherResult;
 import org.eclipse.equinox.p2.publisher.PublisherResult;
 import org.eclipse.equinox.p2.query.IQuery;
+import org.eclipse.equinox.p2.query.LimitQuery;
 
 /**
  * Performance tests for the p2 publisher
@@ -48,6 +49,32 @@ public class PublisherPerformanceTest extends ProvisioningPerformanceTest {
 				}
 			}
 		}.run(this, "Test query PublisherResult for " + IU_COUNT + " ius", REPEATS, 10);
+	}
 
+	public void testLimitQueryPublisherResult() {
+		final int IU_COUNT = 3000;
+		new PerformanceTestRunner() {
+			IQuery[] queries = new IQuery[IU_COUNT];
+			PublisherResult result;
+
+			protected void setUp() {
+				IInstallableUnit[] ius = new IInstallableUnit[IU_COUNT];
+				result = new PublisherResult();
+				for (int i = 0; i < ius.length; i++) {
+					ius[i] = generateIU(i);
+					result.addIU(ius[i], IPublisherResult.ROOT);
+					queries[i] = new LimitQuery(new InstallableUnitQuery(ius[i].getId(), ius[i].getVersion()), 1);
+				}
+			}
+
+			protected void tearDown() {
+			}
+
+			protected void test() {
+				for (int i = 0; i < queries.length; i++) {
+					result.query(queries[i], null);
+				}
+			}
+		}.run(this, "Test query PublisherResult for " + IU_COUNT + " ius", REPEATS, 10);
 	}
 }
