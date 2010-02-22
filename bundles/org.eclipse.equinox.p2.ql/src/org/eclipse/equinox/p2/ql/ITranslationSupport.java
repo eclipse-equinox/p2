7c675d12c672512e84509dd882c4320b676b9ff6
Thomas Hallgren <thallgren>
2010-02-22 13:40:56 +0000

    Removed obsolete interfaces.

--- bundles/org.eclipse.equinox.p2.ql/src/org/eclipse/equinox/p2/ql/ITranslationSupport.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.equinox.p2.ql;
-
-import org.eclipse.equinox.p2.metadata.IInstallableUnit;
-
-public interface ITranslationSupport {
-	String getIUProperty(IInstallableUnit iu, String key);
-}
