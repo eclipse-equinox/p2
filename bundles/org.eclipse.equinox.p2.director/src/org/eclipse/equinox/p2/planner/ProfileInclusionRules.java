eb944fdff7f940cd431bbed60192174a97218ace
Pascal Rapicault <prapicau>
2010-03-02 22:33:26 +0000

    *** empty log message ***

--- bundles/org.eclipse.equinox.p2.director/src/org/eclipse/equinox/p2/planner/ProfileInclusionRules.java
+++ bundles/org.eclipse.equinox.p2.director/src/org/eclipse/equinox/p2/planner/ProfileInclusionRules.java
@@ -7,20 +7,35 @@
  * 
  *  Contributors:
  *      IBM Corporation - initial API and implementation
+ *      Sonatype Inc - Refactoring
  *******************************************************************************/
 package org.eclipse.equinox.p2.planner;
 
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 
+/**
+ * Helper method to decide on the way the installable units are being included.
+ * @since 2.0
+ */
 public class ProfileInclusionRules {
 	private ProfileInclusionRules() {
 		//Can't instantiate profile inclusion rules
 	}
 
+	/**
+	 * Provide an inclusion rule to for the installation of the given installable unit
+	 * @param iu the iu to be installed.
+	 * @return an opaque token to be passed to the {@link IProfileChangeRequest#setInstallableUnitInclusionRules(IInstallableUnit, String)}
+	 */
 	public static String createStrictInclusionRule(IInstallableUnit iu) {
 		return "STRICT"; //$NON-NLS-1$
 	}
 
+	/**
+	 * Provide an inclusion rule to optionally install the installable unit being specified
+	 * @param iu the iu to be installed.
+	 * @return an opaque token to be passed to the {@link IProfileChangeRequest#setInstallableUnitInclusionRules(IInstallableUnit, String)}
+	 */
 	public static String createOptionalInclusionRule(IInstallableUnit iu) {
 		return "OPTIONAL"; //$NON-NLS-1$
 	}
