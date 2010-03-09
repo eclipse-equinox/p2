6198661aa0e407b834c8a1b57325814209b9ee8c
Pascal Rapicault <prapicau>
2010-03-09 16:51:22 +0000

    Polish the test

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/Bug259537.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/Bug259537.java
@@ -16,6 +16,7 @@ import org.eclipse.equinox.p2.engine.IEngine;
 import org.eclipse.equinox.p2.engine.IProfile;
 import org.eclipse.equinox.p2.metadata.*;
 import org.eclipse.equinox.p2.planner.IPlanner;
+import org.eclipse.equinox.p2.query.QueryUtil;
 import org.eclipse.equinox.p2.tests.AbstractProvisioningTest;
 
 public class Bug259537 extends AbstractProvisioningTest {
@@ -51,6 +52,10 @@ public class Bug259537 extends AbstractProvisioningTest {
 		req.addInstallableUnits(new IInstallableUnit[] {f1});
 		ProvisioningPlan provisioningPlan = (ProvisioningPlan) planner.getProvisioningPlan(req, null, null);
 		assertOK("Plan not OK", provisioningPlan.getStatus());
+		assertFalse(provisioningPlan.getAdditions().query(QueryUtil.createIUQuery(f1), null).isEmpty());
+		assertFalse(provisioningPlan.getAdditions().query(QueryUtil.createIUQuery(a1), null).isEmpty());
+		assertFalse(provisioningPlan.getAdditions().query(QueryUtil.createIUQuery(b1), null).isEmpty());
+
 		assertOK("Engine failed", engine.perform(provisioningPlan, null));
 
 		a2 = createIU("A", Version.create("1.1.0"), true);
@@ -58,12 +63,14 @@ public class Bug259537 extends AbstractProvisioningTest {
 		IRequirement c1 = MetadataFactory.createRequirement(IInstallableUnit.NAMESPACE_IU_ID, "A", new VersionRange("[1.0.0, 2.0.0)"), null, false, false);
 		IRequirement c2 = MetadataFactory.createRequirement(IInstallableUnit.NAMESPACE_IU_ID, "B", new VersionRange("[2.0.0, 3.0.0)"), null, false, false);
 		f2 = createIU("F", Version.createOSGi(2, 1, 0), new IRequirement[] {c1, c2});
-
 		createTestMetdataRepository(new IInstallableUnit[] {a2, b2, f2});
 		ProfileChangeRequest req2 = new ProfileChangeRequest(getProfile("TestProfile." + getName()));
 		req2.addInstallableUnits(f2);
 		req2.remove(f1);
 		ProvisioningPlan provisioningPlan2 = (ProvisioningPlan) planner.getProvisioningPlan(req2, null, null);
+		assertFalse(provisioningPlan2.getAdditions().query(QueryUtil.createIUQuery(f2), null).isEmpty());
+		assertFalse(provisioningPlan2.getAdditions().query(QueryUtil.createIUQuery(a2), null).isEmpty());
+		assertFalse(provisioningPlan2.getAdditions().query(QueryUtil.createIUQuery(b2), null).isEmpty());
 		assertOK("Plan not OK", provisioningPlan2.getStatus());
 	}
 }
