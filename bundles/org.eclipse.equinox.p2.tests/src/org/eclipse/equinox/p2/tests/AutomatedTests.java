bd0af9d0ae5f5c6d57abe416dfa589a452ae5b1e
DJ Houghton <dj>
2010-03-10 21:59:52 +0000

    Uncommented tests.

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/AutomatedTests.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/AutomatedTests.java
@@ -49,9 +49,9 @@ public class AutomatedTests extends TestCase {
 		suite.addTest(org.eclipse.equinox.p2.tests.updatesite.AllTests.suite());
 		suite.addTest(org.eclipse.equinox.p2.tests.touchpoint.eclipse.AllTests.suite());
 		suite.addTest(org.eclipse.equinox.p2.tests.touchpoint.natives.AllTests.suite());
-		//suite.addTest(org.eclipse.equinox.p2.tests.reconciler.dropins.AllTests.suite());
+		suite.addTest(org.eclipse.equinox.p2.tests.reconciler.dropins.AllTests.suite());
 		suite.addTest(org.eclipse.equinox.p2.tests.updatechecker.AllTests.suite());
-		//suite.addTest(org.eclipse.equinox.p2.tests.full.AllTests.suite());
+		suite.addTest(org.eclipse.equinox.p2.tests.full.AllTests.suite());
 		return suite;
 	}
 }
