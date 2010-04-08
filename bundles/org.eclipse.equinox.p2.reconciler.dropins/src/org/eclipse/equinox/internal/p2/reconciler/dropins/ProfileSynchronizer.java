de0fa192ce7f5a588cd8fccfd9525f03435fc14b
DJ Houghton <dj>
2010-04-08 23:11:03 +0000

    Bug 308576 - [reconciler] Unable to install test framework via drop-ins

--- bundles/org.eclipse.equinox.p2.reconciler.dropins/src/org/eclipse/equinox/internal/p2/reconciler/dropins/ProfileSynchronizer.java
+++ bundles/org.eclipse.equinox.p2.reconciler.dropins/src/org/eclipse/equinox/internal/p2/reconciler/dropins/ProfileSynchronizer.java
@@ -379,8 +379,12 @@ public class ProfileSynchronizer {
 				// if the IU is already installed in the profile then check to see if it was moved.
 				String one = iu.getProperty(RepositoryListener.FILE_NAME);
 				String two = existing.getProperty(RepositoryListener.FILE_NAME);
-				// if we have an IU which has been moved, keep track of it
-				if (one != null && !one.equals(two))
+				// if we have an IU which has been moved, keep track of it.
+				//
+				// cheat here... since we always set the filename property for bundles in the dropins,
+				// if the existing IU's filename is null then it isn't from the dropins. a better
+				// (and more expensive) way to find this out is to do an IU profile property query.
+				if (one != null && two != null && !one.equals(two))
 					toMove.add(iu);
 			}
 		}
