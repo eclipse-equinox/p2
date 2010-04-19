b9490511e7fa910eb108f31f5b492c9883753b16
Susan Franklin <sfranklin>
2010-04-19 23:45:28 +0000

    Bug 307428 -  [ui] Contact all update sites not taken into account

--- bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/p2/ui/dialogs/ProvisioningOperationWizard.java
+++ bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/p2/ui/dialogs/ProvisioningOperationWizard.java
@@ -161,7 +161,7 @@ public abstract class ProvisioningOperationWizard extends Wizard {
 		if (currentProvisioningContext == null && provisioningContext == null)
 			return false;
 		if (currentProvisioningContext != null && provisioningContext != null)
-			return currentProvisioningContext.equals(provisioningContext);
+			return !currentProvisioningContext.equals(provisioningContext);
 		// One is null and the other is not
 		return true;
 	}
