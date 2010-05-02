f2157c12e5f5580cb6d9a5eeb9ff6fe79e2670a3
Pascal Rapicault <prapicau>
2010-05-02 20:39:47 +0000

    remove references to non local repos

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/Bug306279.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/Bug306279.java
@@ -12,7 +12,6 @@
 package org.eclipse.equinox.p2.tests.planner;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Set;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
@@ -28,10 +27,12 @@ import org.eclipse.equinox.p2.tests.AbstractProvisioningTest;
 
 public class Bug306279 extends AbstractProvisioningTest {
 
-	public void testGreedy() throws ProvisionException, OperationCanceledException, URISyntaxException {
-		IMetadataRepository repo1 = getMetadataRepositoryManager().loadRepository(new URI("http://download.eclipse.org/releases/helios"), null);
+	public void testGreedy() throws ProvisionException, OperationCanceledException {
+		URI heliosRepo = getTestData("helios", "testData/bug306279/repo/helios").toURI();
+		URI rienaRepo2 = getTestData("rienatoolbox-a", "testData/bug306279/repo/rienatoolbox-a").toURI();
+		IMetadataRepository repo1 = getMetadataRepositoryManager().loadRepository(heliosRepo, null);
 		assertFalse(repo1.query(QueryUtil.createIUQuery("org.eclipse.rap.jface.databinding"), new NullProgressMonitor()).isEmpty());
-		IMetadataRepository repo2 = getMetadataRepositoryManager().loadRepository(new URI("http://download.eclipse.org/rt/riena/updatesites/rienatoolbox-a"), null);
+		IMetadataRepository repo2 = getMetadataRepositoryManager().loadRepository(rienaRepo2, null);
 
 		IPlanner planner = getPlanner(getAgent());
 		IProfile profile = createProfile(getName());
@@ -39,7 +40,7 @@ public class Bug306279 extends AbstractProvisioningTest {
 		Set<IInstallableUnit> ius = repo2.query(QueryUtil.createIUQuery("org.eclipse.riena.toolbox.feature.feature.group"), new NullProgressMonitor()).toUnmodifiableSet();
 		request.addAll(ius);
 		ProvisioningContext ctx = new ProvisioningContext(getAgent());
-		ctx.setMetadataRepositories(new URI[] {new URI("http://download.eclipse.org/releases/helios"), new URI("http://download.eclipse.org/rt/riena/updatesites/rienatoolbox-a")});
+		ctx.setMetadataRepositories(new URI[] {heliosRepo, rienaRepo2});
 		IProvisioningPlan plan = planner.getProvisioningPlan(request, ctx, new NullProgressMonitor());
 
 		assertOK("resolution failed", plan.getStatus());
--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/Bug306279b.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/Bug306279b.java
@@ -12,7 +12,6 @@
 package org.eclipse.equinox.p2.tests.planner;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Set;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
@@ -30,10 +29,10 @@ import org.eclipse.equinox.p2.tests.AbstractProvisioningTest;
 
 public class Bug306279b extends AbstractProvisioningTest {
 
-	public void testGreedy() throws ProvisionException, OperationCanceledException, URISyntaxException {
+	public void testGreedy() throws ProvisionException, OperationCanceledException {
 		IProvisioningAgent agent = getAgentProvider().createAgent(getTestData("test data bug306279", "testData/bug306279/p2").toURI());
 
-		IMetadataRepository repo1 = ((IMetadataRepositoryManager) agent.getService(IMetadataRepositoryManager.SERVICE_NAME)).loadRepository(new URI("http://download.eclipse.org/releases/helios"), null);
+		IMetadataRepository repo1 = ((IMetadataRepositoryManager) agent.getService(IMetadataRepositoryManager.SERVICE_NAME)).loadRepository(getTestData("helios", "testData/bug306279/repo/helios").toURI(), null);
 		assertFalse(repo1.query(QueryUtil.createIUQuery("org.eclipse.rap.jface.databinding"), new NullProgressMonitor()).isEmpty());
 
 		URI rienaRepo = getTestData("repo for bug306279", "testData/bug306279/repo/riena").toURI();
@@ -45,7 +44,7 @@ public class Bug306279b extends AbstractProvisioningTest {
 		Set<IInstallableUnit> ius = repo2.query(QueryUtil.createIUQuery("org.eclipse.riena.toolbox.feature.feature.group", Version.create("2.0.0.201003181312")), new NullProgressMonitor()).toUnmodifiableSet();
 		request.addAll(ius);
 		ProvisioningContext ctx = new ProvisioningContext(getAgent());
-		ctx.setMetadataRepositories(new URI[] {new URI("http://download.eclipse.org/releases/helios"), rienaRepo});
+		ctx.setMetadataRepositories(new URI[] {getTestData("helios", "testData/bug306279/repo/helios").toURI(), rienaRepo});
 		IProvisioningPlan plan = planner.getProvisioningPlan(request, ctx, new NullProgressMonitor());
 
 		assertOK("resolution failed", plan.getStatus());
