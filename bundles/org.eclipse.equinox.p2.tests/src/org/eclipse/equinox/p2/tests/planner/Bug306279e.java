173f7a4a91c36c268f1a7c1ec996f9f258260fb4
Pascal Rapicault <prapicau>
2010-05-02 12:31:45 +0000

    *** empty log message ***

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/Bug306279e.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/Bug306279e.java
@@ -1,11 +1,15 @@
 package org.eclipse.equinox.p2.tests.planner;
 
 import java.net.URI;
+import java.util.ArrayList;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.equinox.internal.p2.metadata.RequiredCapability;
 import org.eclipse.equinox.p2.core.*;
 import org.eclipse.equinox.p2.engine.*;
+import org.eclipse.equinox.p2.metadata.*;
 import org.eclipse.equinox.p2.planner.IPlanner;
 import org.eclipse.equinox.p2.planner.IProfileChangeRequest;
+import org.eclipse.equinox.p2.query.QueryUtil;
 import org.eclipse.equinox.p2.tests.AbstractProvisioningTest;
 
 public class Bug306279e extends AbstractProvisioningTest {
@@ -16,16 +20,17 @@ public class Bug306279e extends AbstractProvisioningTest {
 		IProfileChangeRequest request = planner.createChangeRequest(((IProfileRegistry) agent.getService(IProfileRegistry.SERVICE_NAME)).getProfile("SDKProfile"));
 
 		// Force negation of rwt.
-		//		RequiredCapability req1 = new RequiredCapability(IInstallableUnit.NAMESPACE_IU_ID, "org.eclipse.rap.jface", new VersionRange("[1.1.0, 1.4.0)"), null, 0, 0, false, null);
-		//		ArrayList<IRequirement> reqs = new ArrayList();
-		//		reqs.add(req1);
-		//		request.addExtraRequirements(reqs);
+		RequiredCapability req1 = new RequiredCapability(IInstallableUnit.NAMESPACE_IU_ID, "org.eclipse.rap.jface", new VersionRange("[1.1.0, 1.4.0)"), null, 0, 0, false, null);
+		ArrayList<IRequirement> reqs = new ArrayList();
+		reqs.add(req1);
+		request.addExtraRequirements(reqs);
 
 		ProvisioningContext pc = new ProvisioningContext(agent);
 		pc.setMetadataRepositories(new URI[0]);
 		pc.setArtifactRepositories(new URI[0]);
 
 		IProvisioningPlan plan = planner.getProvisioningPlan(request, pc, new NullProgressMonitor());
+		assertFalse("should remove rap.jface", plan.getRemovals().query(QueryUtil.createIUQuery("org.eclipse.rap.jface"), null).isEmpty());
 		assertOK("plan is not ok", plan.getStatus());
 	}
 }
