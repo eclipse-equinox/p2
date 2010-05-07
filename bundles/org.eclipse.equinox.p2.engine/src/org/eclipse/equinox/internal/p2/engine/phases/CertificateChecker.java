ad5efb515b313367e9fb2d1200eb0bf904deef79
John Arthorne <johna>
2010-05-07 19:17:48 +0000

    Bug 311968 -  [engine] Only one trust engine is checked to determine trusted signer

--- bundles/org.eclipse.equinox.p2.engine/src/org/eclipse/equinox/internal/p2/engine/phases/CertificateChecker.java
+++ bundles/org.eclipse.equinox.p2.engine/src/org/eclipse/equinox/internal/p2/engine/phases/CertificateChecker.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 IBM Corporation and others.
+ * Copyright (c) 2008, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,9 +10,6 @@
  *******************************************************************************/
 package org.eclipse.equinox.internal.p2.engine.phases;
 
-import org.eclipse.equinox.p2.core.UIServices;
-import org.eclipse.equinox.p2.core.UIServices.TrustInfo;
-
 import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -22,12 +19,14 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.equinox.internal.p2.engine.EngineActivator;
 import org.eclipse.equinox.internal.p2.engine.Messages;
-import org.eclipse.equinox.p2.core.IProvisioningAgent;
+import org.eclipse.equinox.p2.core.*;
+import org.eclipse.equinox.p2.core.UIServices.TrustInfo;
 import org.eclipse.osgi.service.security.TrustEngine;
 import org.eclipse.osgi.signedcontent.*;
 import org.eclipse.osgi.util.NLS;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
 
 public class CertificateChecker {
 	private ArrayList<File> artifacts;
@@ -46,17 +45,14 @@ public class CertificateChecker {
 		final BundleContext context = EngineActivator.getContext();
 		ServiceReference contentFactoryRef = context.getServiceReference(SignedContentFactory.class.getName());
 		SignedContentFactory verifierFactory = (SignedContentFactory) context.getService(contentFactoryRef);
-		ServiceReference trustEngineRef = context.getServiceReference(TrustEngine.class.getName());
-		TrustEngine trustEngine = (TrustEngine) context.getService(trustEngineRef);
 		try {
-			return checkCertificates(trustEngine, verifierFactory);
+			return checkCertificates(verifierFactory);
 		} finally {
 			context.ungetService(contentFactoryRef);
-			context.ungetService(trustEngineRef);
 		}
 	}
 
-	private IStatus checkCertificates(TrustEngine trustEngine, SignedContentFactory verifierFactory) {
+	private IStatus checkCertificates(SignedContentFactory verifierFactory) {
 		UIServices serviceUI = (UIServices) agent.getService(UIServices.SERVICE_NAME);
 		SignedContent content = null;
 		SignerInfo[] signerInfo = null;
@@ -80,17 +76,12 @@ public class CertificateChecker {
 				return new Status(IStatus.ERROR, EngineActivator.ID, Messages.CertificateChecker_SignedContentIOError, e);
 			}
 			for (int i = 0; i < signerInfo.length; i++) {
-				Certificate[] certificateChain = signerInfo[i].getCertificateChain();
-				try {
-					Certificate trustAnchor = trustEngine.findTrustAnchor(certificateChain);
-					if (trustAnchor == null) {
-						if (!untrusted.contains(certificateChain[0])) {
-							untrusted.add(certificateChain[0]);
-							untrustedChain.add(certificateChain);
-						}
+				if (!signerInfo[i].isTrusted()) {
+					Certificate[] certificateChain = signerInfo[i].getCertificateChain();
+					if (!untrusted.contains(certificateChain[0])) {
+						untrusted.add(certificateChain[0]);
+						untrustedChain.add(certificateChain);
 					}
-				} catch (IOException e) {
-					return new Status(IStatus.ERROR, EngineActivator.ID, Messages.CertificateChecker_KeystoreConnectionError, e);
 				}
 			}
 		}
@@ -145,20 +136,43 @@ public class CertificateChecker {
 		if (untrusted.size() > 0)
 			return new Status(IStatus.CANCEL, EngineActivator.ID, Messages.CertificateChecker_CertificateRejected);
 		// If we should persist the trusted certificates, add them to the trust engine
-		if (trustInfo.persistTrust()) {
-			for (int i = 0; i < trustedCertificates.length; i++) {
-				try {
-					trustEngine.addTrustAnchor(trustedCertificates[i], trustedCertificates[i].toString());
-				} catch (IOException e) {
-					//just return an INFO so the user can proceed with the install
-					return new Status(IStatus.INFO, EngineActivator.ID, Messages.CertificateChecker_KeystoreConnectionError, e);
-				} catch (GeneralSecurityException e) {
-					return new Status(IStatus.INFO, EngineActivator.ID, Messages.CertificateChecker_CertificateError, e);
-				}
-			}
-		}
+		if (trustInfo.persistTrust())
+			return persistTrustedCertificates(trustedCertificates);
 
 		return status;
+	}
+
+	private IStatus persistTrustedCertificates(Certificate[] trustedCertificates) {
+		if (trustedCertificates == null)
+			// I'm pretty sure this would be a bug; trustedCertificates should never be null here.
+			return new Status(IStatus.INFO, EngineActivator.ID, Messages.CertificateChecker_CertificateRejected);
+		ServiceTracker trustEngineTracker = new ServiceTracker(EngineActivator.getContext(), TrustEngine.class.getName(), null);
+		trustEngineTracker.open();
+		Object[] trustEngines = trustEngineTracker.getServices();
+		try {
+			if (trustEngines == null)
+				return null;
+			for (Certificate trustedCertificate : trustedCertificates) {
+				for (Object engine : trustEngines) {
+					TrustEngine trustEngine = (TrustEngine) engine;
+					if (trustEngine.isReadOnly())
+						continue;
+					try {
+						trustEngine.addTrustAnchor(trustedCertificate, trustedCertificate.toString());
+						// this should mean we added an anchor successfully; continue to next cert
+						break;
+					} catch (IOException e) {
+						//just return an INFO so the user can proceed with the install
+						return new Status(IStatus.INFO, EngineActivator.ID, Messages.CertificateChecker_KeystoreConnectionError, e);
+					} catch (GeneralSecurityException e) {
+						return new Status(IStatus.INFO, EngineActivator.ID, Messages.CertificateChecker_CertificateError, e);
+					}
+				}
+			}
+		} finally {
+			trustEngineTracker.close();
+		}
+		return Status.OK_STATUS;
 	}
 
 	/**
