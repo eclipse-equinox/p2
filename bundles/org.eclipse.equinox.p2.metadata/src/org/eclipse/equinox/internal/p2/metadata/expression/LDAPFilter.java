42d484fab23aec3eb3e1352b7043e9ec7c3eb747
John Arthorne <johna>
2010-08-31 13:52:19 +0000

    Bug 324081 -  Compile errors in HEAD

--- bundles/org.eclipse.equinox.p2.metadata/src/org/eclipse/equinox/internal/p2/metadata/expression/LDAPFilter.java
+++ bundles/org.eclipse.equinox.p2.metadata/src/org/eclipse/equinox/internal/p2/metadata/expression/LDAPFilter.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Cloudsmith Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Cloudsmith Inc. - initial API and implementation
+ *     IBM - ongoing development
+ *******************************************************************************/
 package org.eclipse.equinox.internal.p2.metadata.expression;
 
 import java.util.Dictionary;
@@ -56,6 +67,10 @@ public class LDAPFilter extends Unary implements IFilterExpression {
 
 	public boolean matchCase(Map<String, ? extends Object> map) {
 		return isMatch(map == null ? MemberProvider.emptyProvider() : MemberProvider.create(map, false));
+	}
+
+	public boolean matches(Map<String, ?> map) {
+		return matchCase(map);
 	}
 
 	@SuppressWarnings("rawtypes")
