0f18308a1f67d540219bf22b92564cd30c59e4a1
DJ Houghton <dj>
2010-09-09 21:30:34 +0000

    Bug 300206 - [ui] install history not refreshed properly after deletion of entry

--- bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/p2/ui/RevertProfilePage.java
+++ bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/p2/ui/RevertProfilePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2007, 2009 IBM Corporation and others.
+ *  Copyright (c) 2007, 2010 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -391,13 +391,13 @@ public class RevertProfilePage extends InstallationPage implements ICopyable {
 					if (registry != null) {
 						try {
 							registry.removeProfile(profileId, snapshot.getTimestamp());
-							configsViewer.refresh();
 						} catch (ProvisionException e) {
 							ProvUI.handleException(e, null, StatusManager.SHOW | StatusManager.LOG);
 						}
 					}
 				}
 			}
+			configsViewer.refresh();
 		}
 	}
 
