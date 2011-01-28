ea4c70236c31bec52114376fc0cec7d41efc4881
DJ Houghton <dj>
2011-01-28 18:14:37 +0000

    Bug 331893 - Use mkdirs workaround also in case of file based downloaded bundles

--- bundles/org.eclipse.equinox.p2.artifact.repository/src/org/eclipse/equinox/internal/p2/artifact/repository/simple/SimpleArtifactRepository.java
+++ bundles/org.eclipse.equinox.p2.artifact.repository/src/org/eclipse/equinox/internal/p2/artifact/repository/simple/SimpleArtifactRepository.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 IBM Corporation and others.
+ * Copyright (c) 2007, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -780,7 +780,7 @@ public class SimpleArtifactRepository extends AbstractArtifactRepository impleme
 			} else {
 				// file based
 				File parent = outputFile.getParentFile();
-				parent.mkdirs();
+				mkdirs(parent);
 				if (!parent.isDirectory())
 					throw failedWrite(new IOException(NLS.bind(Messages.sar_failedMkdir, parent.toString())));
 				target = new FileOutputStream(file);
