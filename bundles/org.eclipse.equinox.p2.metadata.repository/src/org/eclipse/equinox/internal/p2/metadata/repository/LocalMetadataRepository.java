e1b3cc634dbf6f7edab51087a702dd6749d98742
DJ Houghton <dj>
2011-03-02 14:03:51 +0000

    Bug 338600 - [perf] Serialization optimizations for LocalMetadataRepository

--- bundles/org.eclipse.equinox.p2.metadata.repository/src/org/eclipse/equinox/internal/p2/metadata/repository/LocalMetadataRepository.java
+++ bundles/org.eclipse.equinox.p2.metadata.repository/src/org/eclipse/equinox/internal/p2/metadata/repository/LocalMetadataRepository.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2007, 2010 IBM Corporation and others.
+ *  Copyright (c) 2007, 2011 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -118,8 +118,9 @@ public class LocalMetadataRepository extends AbstractMetadataRepository implemen
 	@Override
 	public void addReferences(Collection<? extends IRepositoryReference> references) {
 		assertModifiable();
-		repositories.addAll(references);
-		save();
+		// only save if we really made changes
+		if (repositories.addAll(references))
+			save();
 	}
 
 	/* (non-Javadoc)
