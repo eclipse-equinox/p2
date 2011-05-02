2333d81a3f405da591be1b9c61999221e89b6ba0
Ian Bull <irbull>
2011-05-02 22:52:38 +0000

    copyright update

--- bundles/org.eclipse.equinox.p2.artifact.optimizers/src/org/eclipse/equinox/internal/p2/artifact/optimizers/OptimizerApplication.java
+++ bundles/org.eclipse.equinox.p2.artifact.optimizers/src/org/eclipse/equinox/internal/p2/artifact/optimizers/OptimizerApplication.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Cloudsmith - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.equinox.internal.p2.artifact.optimizers;
 
 import org.eclipse.equinox.app.IApplication;
