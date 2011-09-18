ca7c0a1c092328e111f1f51c3fb2021a961b5493
Pascal Rapicault <pascal@sonatype.com>
2011-09-18 16:31:29 -0400

    add test

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/FindRootsAfterUpdate.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/planner/FindRootsAfterUpdate.java
@@ -19,15 +19,18 @@ import org.eclipse.equinox.p2.query.QueryUtil;
 import org.eclipse.equinox.p2.tests.AbstractProvisioningTest;
 
 public class FindRootsAfterUpdate extends AbstractProvisioningTest {
+	IInstallableUnit a1;
 	IInstallableUnit b1;
 	IInstallableUnit b2;
 	IInstallableUnit b3;
+	IInstallableUnit x1;
 
 	IProfile profile;
 	IPlanner planner;
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		a1 = createIU("A", Version.create("1.0.0"), createRequiredCapabilities(IInstallableUnit.NAMESPACE_IU_ID, "B", new VersionRange("[1.0.0, 4.0.0)")));
 
 		b1 = createIU("B", Version.create("1.0.0"), true);
 
@@ -35,14 +38,16 @@ public class FindRootsAfterUpdate extends AbstractProvisioningTest {
 
 		b3 = createIU("B", Version.create("3.0.0"), true);
 
-		createTestMetdataRepository(new IInstallableUnit[] {b1, b2, b3});
+		x1 = createIU("X", Version.createOSGi(2, 0, 0), createRequiredCapabilities(IInstallableUnit.NAMESPACE_IU_ID, "B", new VersionRange("[2.0.0, 2.0.0]")));
+
+		createTestMetdataRepository(new IInstallableUnit[] {a1, b1, b2, b3, x1});
 
 		profile = createProfile("TestProfile." + getName());
 		planner = createPlanner();
 
 	}
 
-	public void testInstallA1() {
+	public void testInstallB3() {
 		ProfileChangeRequest req = new ProfileChangeRequest(profile);
 		ProvisioningContext ctx = new ProvisioningContext(getAgent());
 		IRequirement[] reqs = createRequiredCapabilities(IInstallableUnit.NAMESPACE_IU_ID, "B", new VersionRange("[2.0.0, 4.0.0]"));
@@ -51,5 +56,16 @@ public class FindRootsAfterUpdate extends AbstractProvisioningTest {
 		assertEquals(IStatus.OK, plan.getStatus().getSeverity());
 		assertInstallOperand(plan, b3);
 		assertContains(plan.getFutureState().query(QueryUtil.createMatchQuery(reqs[0].getMatches()), null), b3);
+	}
+
+	public void testConflictThroughExtraRequirements() {
+		ProfileChangeRequest req = new ProfileChangeRequest(profile);
+		ProvisioningContext ctx = new ProvisioningContext(getAgent());
+		IRequirement reqOnB = createRequiredCapabilities(IInstallableUnit.NAMESPACE_IU_ID, "B", new VersionRange("[3.0.0, 4.0.0]"))[0];
+		IRequirement reqOnX = createRequiredCapabilities(IInstallableUnit.NAMESPACE_IU_ID, "X", new VersionRange("[2.0.0, 2.0.0]"))[0];
+		IRequirement[] extraReqs = new IRequirement[] {reqOnB, reqOnX};
+		req.addExtraRequirements(Arrays.<IRequirement> asList(extraReqs));
+		IProvisioningPlan plan = planner.getProvisioningPlan(req, ctx, null);
+		assertNotOK(plan.getStatus());
 	}
 }
