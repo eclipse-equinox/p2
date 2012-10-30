99d7eb61f20739c313031de8ecfca64ec0275e99
Tobias Oberlies <tobias.oberlies@sap.com>
2012-10-30 18:07:09 +0100

    393177 Move tests with UI dependency out of org.eclipse.equinox.p2.tests

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/AutomatedTests.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/AutomatedTests.java
@@ -52,7 +52,6 @@ public class AutomatedTests extends TestCase {
 		suite.addTest(org.eclipse.equinox.p2.tests.reconciler.dropins.AllTests.suite());
 		suite.addTest(org.eclipse.equinox.p2.tests.updatechecker.AllTests.suite());
 		suite.addTest(org.eclipse.equinox.p2.tests.full.AllTests.suite());
-		suite.addTest(org.eclipse.equinox.p2.tests.importexport.AllTests.suite());
 		return suite;
 	}
 }
