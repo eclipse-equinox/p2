fe481d16792024cddc65527f1e40b3b159f0d968
Pascal Rapicault <pascal@rapicorp.com>
2014-11-07 16:21:37 -0500

    change type from Object to String

--- bundles/org.eclipse.equinox.p2.touchpoint.natives/src/org/eclipse/equinox/internal/p2/touchpoint/natives/actions/ActionConstants.java
+++ bundles/org.eclipse.equinox.p2.touchpoint.natives/src/org/eclipse/equinox/internal/p2/touchpoint/natives/actions/ActionConstants.java
@@ -34,7 +34,8 @@ public class ActionConstants {
 	public static final String PARM_COPY_OVERWRITE = "overwrite"; //$NON-NLS-1$
 	public static final String PARM_OPTIONS = "options"; //$NON-NLS-1$
 	public static final String PARM_ABSOLUTE_FILES = "absoluteFiles"; //$NON-NLS-1$
-	public static final Object PARM_LINUX_DISTRO = "distro"; //$NON-NLS-1$
-	public static final Object PARM_LINUX_PACKAGE_NAME = "package"; //$NON-NLS-1$
-	public static final Object PARM_LINUX_PACKAGE_VERSION = "version"; //$NON-NLS-1$
+	public static final String PARM_LINUX_DISTRO = "distro"; //$NON-NLS-1$
+	public static final String PARM_LINUX_PACKAGE_NAME = "package"; //$NON-NLS-1$
+	public static final String PARM_LINUX_PACKAGE_VERSION = "version"; //$NON-NLS-1$
+	public static final String PACKAGE_VERSION_CONSTRAINT = "constraint"; //$NON-NLS-1$
 }
