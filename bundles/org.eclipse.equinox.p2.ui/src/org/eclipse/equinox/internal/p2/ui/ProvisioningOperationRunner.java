450f4fc1828920724074d3f19853e2afd91fd8a2
Thomas Watson <tjwatson@us.ibm.com>
2014-12-08 15:05:52 -0600

    Bug 256145 - [WorkbenchLauncher] Don't prompt for workspace on plugin install restart

--- bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/p2/ui/ProvisioningOperationRunner.java
+++ bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/p2/ui/ProvisioningOperationRunner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2007, 2010 IBM Corporation and others.
+ *  Copyright (c) 2007, 2014 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -88,7 +88,7 @@ public class ProvisioningOperationRunner {
 				if (retCode == ApplyProfileChangesDialog.PROFILE_APPLYCHANGES) {
 					applyProfileChanges();
 				} else if (retCode == ApplyProfileChangesDialog.PROFILE_RESTART) {
-					PlatformUI.getWorkbench().restart();
+					PlatformUI.getWorkbench().restart(true);
 				}
 			}
 		});
