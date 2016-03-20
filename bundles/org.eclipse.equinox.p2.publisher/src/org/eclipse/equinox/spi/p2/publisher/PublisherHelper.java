5b2f061cd8ecf96a37783657e7ee7a0110c9d26d
Mykola Nikishov <mn@mn.com.ua>
2016-03-20 13:05:14 +0200

    Bug 490028 - Extract MD5 checksum calculation into a separate class
    MD5 checksum is calculated in (almost) the same way in two places:
    org.eclipse.equinox.p2.internal.repository.tools.RepositoryUtilities and
    org.eclipse.equinox.spi.p2.publisher.PublisherHelper.
    
    The latter uses 4K byte buffer instead of byte-by-byte update which
    makes MD5 calculation for a 300k jar file 5x faster, see bug #405716 for
    more details. Due to the duplication, this has been fixed in
    PublisherHelper only but not in RepositoryUtilities.
    
    Eliminate code duplication by extracting MD5 checksum calculation into a
    separate class, ChecksumProducer. To make it consimable by
    org.eclipse.equinox.p2.publisher and
    org.eclipse.equinox.p2.repository.tools bundles but not exposing too
    much internals, move it to the x-friend'ed
    org.eclipse.equinox.internal.p2.repository.helpers package of
    org.eclipse.equinox.p2.repository bundle.
    
    Change-Id: Ic8b45a47700f35af1fb69f534dec7b32a2f7714a
    Signed-off-by: Mykola Nikishov <mn@mn.com.ua>

--- bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/spi/p2/publisher/PublisherHelper.java
+++ bundles/org.eclipse.equinox.p2.publisher/src/org/eclipse/equinox/spi/p2/publisher/PublisherHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2007, 2011 IBM Corporation and others.
+ *  Copyright (c) 2007, 2016 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -9,16 +9,16 @@
  *     IBM Corporation - initial API and implementation
  *     Genuitec, LLC - added license support
  *     Code 9 - Ongoing development
-#      SAP AG - consolidation of publishers for PDE formats
+ *     SAP AG - consolidation of publishers for PDE formats
+ *     Mykola Nikishov - extract MD5 checksum calculation
  *******************************************************************************/
 package org.eclipse.equinox.spi.p2.publisher;
 
-import java.io.*;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.io.File;
 import java.util.*;
 import org.eclipse.equinox.internal.p2.metadata.ArtifactKey;
 import org.eclipse.equinox.internal.p2.metadata.BasicVersion;
+import org.eclipse.equinox.internal.p2.repository.helpers.ChecksumProducer;
 import org.eclipse.equinox.p2.metadata.*;
 import org.eclipse.equinox.p2.metadata.MetadataFactory.InstallableUnitDescription;
 import org.eclipse.equinox.p2.metadata.MetadataFactory.InstallableUnitFragmentDescription;
@@ -108,52 +108,12 @@ public class PublisherHelper {
 				descriptor.setProperty(IArtifactDescriptor.DOWNLOAD_SIZE, Long.toString(pathOnDisk.length()));
 			}
 			if (info == null || (info.getArtifactOptions() & IPublisherInfo.A_NO_MD5) == 0) {
-				String md5 = computeMD5(pathOnDisk);
+				String md5 = ChecksumProducer.computeMD5(pathOnDisk);
 				if (md5 != null)
 					descriptor.setProperty(IArtifactDescriptor.DOWNLOAD_MD5, md5);
 			}
 		}
 		return result;
-	}
-
-	private static String computeMD5(File file) {
-		if (file == null || file.isDirectory() || !file.exists())
-			return null;
-		MessageDigest md5Checker;
-		try {
-			md5Checker = MessageDigest.getInstance("MD5"); //$NON-NLS-1$
-		} catch (NoSuchAlgorithmException e) {
-			return null;
-		}
-		InputStream fis = null;
-		try {
-			fis = new BufferedInputStream(new FileInputStream(file));
-			int read = -1;
-			final int bufferSize = 4 * 1024;
-			byte[] buffer = new byte[bufferSize];
-			while ((read = fis.read(buffer, 0, bufferSize)) != -1) {
-				md5Checker.update(buffer, 0, read);
-			}
-			byte[] digest = md5Checker.digest();
-			StringBuffer buf = new StringBuffer();
-			for (int i = 0; i < digest.length; i++) {
-				if ((digest[i] & 0xFF) < 0x10)
-					buf.append('0');
-				buf.append(Integer.toHexString(digest[i] & 0xFF));
-			}
-			return buf.toString();
-		} catch (FileNotFoundException e) {
-			return null;
-		} catch (IOException e) {
-			return null;
-		} finally {
-			if (fis != null)
-				try {
-					fis.close();
-				} catch (IOException e) {
-					// ignore
-				}
-		}
 	}
 
 	public static IProvidedCapability makeTranslationCapability(String hostId, Locale locale) {
