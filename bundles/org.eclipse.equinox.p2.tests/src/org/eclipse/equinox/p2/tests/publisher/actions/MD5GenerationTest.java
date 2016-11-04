61729b60464bdfbdbfef9eed015b66bce7285782
Mykola Nikishov <mn@mn.com.ua>
2016-11-04 23:17:11 +0200

    Add tests for IPublisherInfo.A_NO_MD5 contract
    When creating an artifact descriptor with PublisherHelper,
    createArtifactDescriptor(IPublisherInfo, IArtifactRepository,
    IArtifactKey, File) will generate MD5 hash if
    
        (info == null || (info.getArtifactOptions() & IPublisherInfo.A_NO_MD5) == 0)
    
    is true.
    
    As current tests cover only 1 out of 4 this condition's branches, add
    tests for missing branches that assert IPublisherInfo.A_NO_MD5 contract.
    
    Change-Id: I9912f01294cee8288a61520086d590f62524fff9
    Signed-off-by: Mykola Nikishov <mn@mn.com.ua>

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/publisher/actions/MD5GenerationTest.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/publisher/actions/MD5GenerationTest.java
@@ -12,6 +12,8 @@ package org.eclipse.equinox.p2.tests.publisher.actions;
 
 import org.eclipse.equinox.internal.p2.metadata.ArtifactKey;
 import org.eclipse.equinox.p2.metadata.Version;
+import org.eclipse.equinox.p2.publisher.IPublisherInfo;
+import org.eclipse.equinox.p2.publisher.PublisherInfo;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
 import org.eclipse.equinox.p2.tests.AbstractProvisioningTest;
 import org.eclipse.equinox.spi.p2.publisher.PublisherHelper;
@@ -20,6 +22,21 @@ public class MD5GenerationTest extends AbstractProvisioningTest {
 	public void testGenerationFile() {
 		IArtifactDescriptor ad = PublisherHelper.createArtifactDescriptor(new ArtifactKey("classifierTest", "idTest", Version.createOSGi(1, 0, 0)), getTestData("Artifact to generate from", "testData/artifactRepo/simpleWithMD5/plugins/aaPlugin_1.0.0.jar"));
 		assertEquals("50d4ea58b02706ab373a908338877e02", ad.getProperty(IArtifactDescriptor.DOWNLOAD_MD5));
+	}
+
+	public void testGenerationFile_emptyPublisherInfo() {
+		ArtifactKey key = new ArtifactKey("classifierTest", "idTest", Version.createOSGi(1, 0, 0));
+		IPublisherInfo publisherInfo = new PublisherInfo();
+		IArtifactDescriptor ad = PublisherHelper.createArtifactDescriptor(publisherInfo, key, getTestData("Artifact to generate from", "testData/artifactRepo/simpleWithMD5/plugins/aaPlugin_1.0.0.jar"));
+		assertEquals("50d4ea58b02706ab373a908338877e02", ad.getProperty(IArtifactDescriptor.DOWNLOAD_MD5));
+	}
+
+	public void testGenerationFile_noMd5() {
+		ArtifactKey key = new ArtifactKey("classifierTest", "idTest", Version.createOSGi(1, 0, 0));
+		PublisherInfo publisherInfo = new PublisherInfo();
+		publisherInfo.setArtifactOptions(IPublisherInfo.A_NO_MD5);
+		IArtifactDescriptor ad = PublisherHelper.createArtifactDescriptor(publisherInfo, key, getTestData("Artifact to generate from", "testData/artifactRepo/simpleWithMD5/plugins/aaPlugin_1.0.0.jar"));
+		assertNull(ad.getProperty(IArtifactDescriptor.DOWNLOAD_MD5));
 	}
 
 	public void testGenerationFolder() {
