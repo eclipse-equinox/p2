1c986bddd9de0ef58d3a591bc7a92c45fecff325
Thomas Watson <tjwatson@us.ibm.com>
2017-05-17 12:58:08 -0500

    [Bug 516827] Disable uses conflict checking while resolving
    Change-Id: Ib511e1a210f57473602851d9c7e98f2e3694c81d
    Signed-off-by: Thomas Watson <tjwatson@us.ibm.com>

--- bundles/org.eclipse.equinox.frameworkadmin.equinox/src/org/eclipse/equinox/internal/frameworkadmin/equinox/EquinoxBundlesState.java
+++ bundles/org.eclipse.equinox.frameworkadmin.equinox/src/org/eclipse/equinox/internal/frameworkadmin/equinox/EquinoxBundlesState.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2015 IBM Corporation and others.
+ * Copyright (c) 2007, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -638,6 +638,8 @@ public class EquinoxBundlesState implements BundlesState {
 				}
 			}
 		}
+		// TODO ignore uses directive for resolution here
+		platformProperties.put("osgi.resolver.usesMode", "ignore"); //$NON-NLS-1$ //$NON-NLS-2$
 		state.setPlatformProperties(platformProperties);
 	}
 
