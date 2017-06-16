0bd6de95a5caa4ebc3f9d05a2453b945ef393621
Thomas Watson <tjwatson@us.ibm.com>
2017-06-16 07:58:27 -0500

    Bug 518375 - Update test implementations of ServiceReference to account for new R7 method
    Change-Id: I42b1f4ba599faf273c967b3a35a4640aa773b73f
    Signed-off-by: Thomas Watson <tjwatson@us.ibm.com>

--- bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/metadata/expression/FilterTest.java
+++ bundles/org.eclipse.equinox.p2.tests/src/org/eclipse/equinox/p2/tests/metadata/expression/FilterTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2010, 2015 IBM Corporation and others.
+ *  Copyright (c) 2010, 2017 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -71,6 +71,10 @@ public class FilterTest extends TestCase {
 
 		public boolean isAssignableTo(Bundle bundle, String className) {
 			throw new UnsupportedOperationException();
+		}
+
+		public Dictionary getProperties() {
+			return new Hashtable(dictionary);
 		}
 	}
 
