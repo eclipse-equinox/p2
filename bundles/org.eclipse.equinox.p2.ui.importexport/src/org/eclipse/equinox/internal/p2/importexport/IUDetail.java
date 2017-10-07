b58fc846d236c3732e8976f5e203c57e0863e2be
Alexander Kurtakov <akurtako@redhat.com>
2017-10-07 12:40:14 +0300

    Bug 521147 - Move p2 ui.importexport to Java 8
    Add missing Override annotation.
    
    Change-Id: I8a7155e5355679d52a8caf9b3036fe9f9613dbcf
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.equinox.p2.ui.importexport/src/org/eclipse/equinox/internal/p2/importexport/IUDetail.java
+++ bundles/org.eclipse.equinox.p2.ui.importexport/src/org/eclipse/equinox/internal/p2/importexport/IUDetail.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 WindRiver Corporation and others.
+ * Copyright (c) 2011, 2017 WindRiver Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -33,6 +33,7 @@ public class IUDetail implements IAdaptable {
 		return referredRepo;
 	}
 
+	@Override
 	@SuppressWarnings("unchecked")
 	public <T> T getAdapter(Class<T> adapter) {
 		if (IInstallableUnit.class.equals(adapter))
