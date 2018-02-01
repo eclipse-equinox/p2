ec65bd6a75cfaab71bee21b1f1a96c95dbe51539
Karsten Thoms <karsten.thoms@itemis.de>
2018-02-01 09:50:52 +0100

    Bug 530592 - Fixed typo in temp file name
    Change-Id: I86648442f44dfbc1abf1293a7c9dd895cf46c98f
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- bundles/org.eclipse.equinox.p2.director.app/src/org/eclipse/equinox/internal/p2/director/app/DirectorApplication.java
+++ bundles/org.eclipse.equinox.p2.director.app/src/org/eclipse/equinox/internal/p2/director/app/DirectorApplication.java
@@ -1399,9 +1399,9 @@ public class DirectorApplication implements IApplication, ProvisioningListener {
 		try {
 			// we use the .dll suffix to properly test on Vista virtual directories
 			// on Vista you are not allowed to write executable files on virtual directories like "Program Files"
-			fileTest = File.createTempFile("writtableArea", ".dll", installDir); //$NON-NLS-1$ //$NON-NLS-2$
+			fileTest = File.createTempFile("writableArea", ".dll", installDir); //$NON-NLS-1$ //$NON-NLS-2$
 		} catch (IOException e) {
-			//If an exception occured while trying to create the file, it means that it is not writtable
+			//If an exception occured while trying to create the file, it means that it is not writable
 			return false;
 		} finally {
 			if (fileTest != null)
