6a7323a8994ea3ff257548f61319e1384e8f3294
Alexander Kurtakov <akurtako@redhat.com>
2018-07-10 15:40:51 +0300

    Try-with-resources in p2.repository.tools.
    Enabled the compiler warning and changed the formatter to standard
    eclipse one so lines are not endless.
    
    Change-Id: I90519c176f366e847ba4f24021a8ed94790a7c8e
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.equinox.p2.repository.tools/src/org/eclipse/equinox/p2/internal/repository/tools/XZCompressor.java
+++ bundles/org.eclipse.equinox.p2.repository.tools/src/org/eclipse/equinox/p2/internal/repository/tools/XZCompressor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2017 Rapicorp, Inc and others.
+ * Copyright (c) 2015, 2018 Rapicorp, Inc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,15 +19,16 @@ import org.eclipse.osgi.util.NLS;
 import org.tukaani.xz.*;
 
 /**
- * A class taking care of creating the XZ'ed version of a repository.
- * It takes as input the folder where a p2 repository is stored and produces the new files in this folder.
+ * A class taking care of creating the XZ'ed version of a repository. It takes
+ * as input the folder where a p2 repository is stored and produces the new
+ * files in this folder.
  * 
- * The files expected in to be found as input are content.xml or content.jar, or artifacts.xml or artifacts.jar. 
- * Note that the tool does not require both the a metadata repository and an artifact repository.
- * The output will be <fileName>.xml.xz, and a p2.index
+ * The files expected in to be found as input are content.xml or content.jar, or
+ * artifacts.xml or artifacts.jar. Note that the tool does not require both the
+ * a metadata repository and an artifact repository. The output will be
+ * <fileName>.xml.xz, and a p2.index
  *
  */
-
 public class XZCompressor {
 	private static final String CONTENT_XML_XZ = "content.xml.xz"; //$NON-NLS-1$
 	private static final String ARTIFACTS_XML_XZ = "artifacts.xml.xz"; //$NON-NLS-1$
@@ -57,38 +58,30 @@ public class XZCompressor {
 	}
 
 	private File uncompressJar(File jarFile, String fileToExtract) throws IOException {
-		JarInputStream jarStream = new JarInputStream(new FileInputStream(jarFile));
-		JarEntry jarEntry = jarStream.getNextJarEntry();
-		while (jarEntry != null && (!fileToExtract.equals(jarEntry.getName()))) {
-			jarEntry = jarStream.getNextJarEntry();
-		}
-		//if there is a jar but the entry is missing or invalid, treat this as an invalid repository
-		if (jarEntry == null) {
-			jarStream.close();
-			throw new IOException(NLS.bind(Messages.repoMan_invalidLocation, jarFile.getAbsolutePath()));
-		}
-
-		BufferedInputStream input = null;
-		OutputStream output = null;
-		File extractedFile = new File(jarFile.getAbsoluteFile().getParent(), fileToExtract);
-		try {
-			input = new BufferedInputStream(jarStream);
-			output = new BufferedOutputStream(new FileOutputStream(extractedFile));
-
-			byte[] buffer = new byte[8192];
-			int bytesRead = 0;
-			while ((bytesRead = input.read(buffer)) != -1)
-				output.write(buffer, 0, bytesRead);
-		} finally {
-			if (input != null) {
-				input.close();
+		try (JarInputStream jarStream = new JarInputStream(new FileInputStream(jarFile))) {
+			JarEntry jarEntry = jarStream.getNextJarEntry();
+			while (jarEntry != null && (!fileToExtract.equals(jarEntry.getName()))) {
+				jarEntry = jarStream.getNextJarEntry();
 			}
-			if (output != null) {
-				output.close();
+			// if there is a jar but the entry is missing or invalid, treat this as an
+			// invalid repository
+			if (jarEntry == null) {
+				jarStream.close();
+				throw new IOException(NLS.bind(Messages.repoMan_invalidLocation, jarFile.getAbsolutePath()));
 			}
+
+			File extractedFile = new File(jarFile.getAbsoluteFile().getParent(), fileToExtract);
+			try (BufferedInputStream input = new BufferedInputStream(jarStream);
+					OutputStream output = new BufferedOutputStream(new FileOutputStream(extractedFile))) {
+
+				byte[] buffer = new byte[8192];
+				int bytesRead = 0;
+				while ((bytesRead = input.read(buffer)) != -1)
+					output.write(buffer, 0, bytesRead);
+			}
+			filesToDelete.add(extractedFile);
+			return extractedFile;
 		}
-		filesToDelete.add(extractedFile);
-		return extractedFile;
 	}
 
 	private File getMetadataFile(String prefix) throws IOException {
@@ -140,22 +133,15 @@ public class XZCompressor {
 			options.setMatchFinder(LZMA2Options.MF_BT4);
 			options.setDepthLimit(512);
 		} catch (UnsupportedOptionsException e) {
-			//Can't happen 
+			// Can't happen
 		}
-		XZOutputStream out = null;
-		java.io.FileInputStream is = null;
-		try {
-			out = new XZOutputStream(new FileOutputStream(output), options);
-			is = new FileInputStream(input);
+		try (XZOutputStream out = new XZOutputStream(new FileOutputStream(output), options);
+				FileInputStream is = new FileInputStream(input)) {
+
 			byte[] buf = new byte[8192];
 			int size;
 			while ((size = is.read(buf)) != -1)
 				out.write(buf, 0, size);
-		} finally {
-			if (out != null)
-				out.close();
-			if (is != null)
-				is.close();
 		}
 
 	}
@@ -175,13 +161,9 @@ public class XZCompressor {
 				p2Index.setProperty("artifact.repository.factory.order", "artifacts.xml.xz,!"); //$NON-NLS-1$//$NON-NLS-2$
 		}
 		p2Index.setProperty("version", "1"); //$NON-NLS-1$//$NON-NLS-2$
-		OutputStream output = null;
-		try {
-			output = new FileOutputStream(new File(repoFolder, "p2.index")); //$NON-NLS-1$
+		try (OutputStream output = new FileOutputStream(new File(repoFolder, "p2.index"));) { //$NON-NLS-1$
+			// $NON-NLS-1$
 			p2Index.store(output, null);
-		} finally {
-			if (output != null)
-				output.close();
 		}
 	}
 }
