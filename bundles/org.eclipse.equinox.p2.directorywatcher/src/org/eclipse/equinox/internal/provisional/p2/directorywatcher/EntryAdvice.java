cfed53d0a8786ba1c1f683d5275472254f38b024
Lars Vogel <Lars.Vogel@vogella.com>
2018-08-22 15:01:27 +0200

    Bug 535802 - Update p2 EPL-2.0 for its plug-ins - Part 2
    Change-Id: I1f223baf52ff9a41f374a5f3d71410243d5cb6a4
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.equinox.p2.directorywatcher/src/org/eclipse/equinox/internal/provisional/p2/directorywatcher/EntryAdvice.java
+++ bundles/org.eclipse.equinox.p2.directorywatcher/src/org/eclipse/equinox/internal/provisional/p2/directorywatcher/EntryAdvice.java
@@ -1,10 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2017 Code 9 and others. 
- * All rights reserved. This
+ *
+ * This
  * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * the Eclipse Public License 2.0 which accompanies this distribution, and is
  * available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  * 
  * Contributors: 
  *   Code 9 - initial API and implementation
