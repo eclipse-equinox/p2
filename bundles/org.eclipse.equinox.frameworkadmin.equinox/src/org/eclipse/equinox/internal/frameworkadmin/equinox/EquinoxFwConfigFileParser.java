b3990169360c5fc4ee27224a2c4c3aebc408043e
Jörg Kubitz <jkubitz-eclipse@gmx.de>
2023-02-09 16:45:21 +0100

    EquinoxFwConfigFileParser: add null check - fixes #219
    avoids warning "Failed creating shared configuration url for null."

--- bundles/org.eclipse.equinox.frameworkadmin.equinox/src/org/eclipse/equinox/internal/frameworkadmin/equinox/EquinoxFwConfigFileParser.java
+++ bundles/org.eclipse.equinox.frameworkadmin.equinox/src/org/eclipse/equinox/internal/frameworkadmin/equinox/EquinoxFwConfigFileParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2017 IBM Corporation and others.
+ * Copyright (c) 2007, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -330,6 +330,8 @@ public class EquinoxFwConfigFileParser {
 	}
 
 	private File findSharedConfigIniFile(File base, String sharedConfigurationArea) {
+		if (sharedConfigurationArea == null)
+			return null;
 		if (base == null)
 			return null;
 		URL rootURL;
