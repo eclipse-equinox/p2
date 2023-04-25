fe812b78bd15ccb247644e424a298f4345b3e78d
Vasili Gulevich <vasili.gulevich@xored.com>
2023-04-25 19:32:03 +0400

    Notify user about updates for write-protected components (#254) (#258)
    Function is enabled with JVM argument
    -Dorg.eclipse.equinox.p2.ui.sdk.scheduler.warnLocked=true
    
    Fixes #254.

--- bundles/org.eclipse.equinox.p2.ui.sdk.scheduler/src/org/eclipse/equinox/internal/p2/ui/sdk/scheduler/AutomaticUpdatesPopup.java
+++ bundles/org.eclipse.equinox.p2.ui.sdk.scheduler/src/org/eclipse/equinox/internal/p2/ui/sdk/scheduler/AutomaticUpdatesPopup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2018 IBM Corporation and others.
+ * Copyright (c) 2007, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -11,37 +11,31 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Johannes Michler <orgler@gmail.com> - Bug 321568 -  [ui] Preference for automatic-update-reminder doesn't work in multilanguage-environments
+ *     Vasili Gulevich (Spirent Communications) - factor out UpdatesPopup
  *******************************************************************************/
 package org.eclipse.equinox.internal.p2.ui.sdk.scheduler;
 
 import org.eclipse.core.runtime.*;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.IDialogSettings;
-import org.eclipse.jface.dialogs.PopupDialog;
-import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.ui.dialogs.PreferencesUtil;
 import org.eclipse.ui.progress.WorkbenchJob;
 
 /**
- * AutomaticUpdatesPopup is an async popup dialog for notifying the user of
- * updates.
+ * Async popup dialog for notifying the user of eligible updates.
  * 
  * @since 3.4
  */
-public class AutomaticUpdatesPopup extends PopupDialog {
+public class AutomaticUpdatesPopup extends UpdatesPopup {
 	public static final String[] ELAPSED_VALUES = { PreferenceConstants.PREF_REMIND_30Minutes,
 			PreferenceConstants.PREF_REMIND_60Minutes, PreferenceConstants.PREF_REMIND_240Minutes };
 	public static final String[] ELAPSED_LOCALIZED_STRINGS = {
@@ -51,49 +45,27 @@ public class AutomaticUpdatesPopup extends PopupDialog {
 	private static final long MINUTE = 60 * 1000L;
 	private static final String PREFS_HREF = "PREFS"; //$NON-NLS-1$
 	private static final String DIALOG_SETTINGS_SECTION = "AutomaticUpdatesPopup"; //$NON-NLS-1$
-	private static final int POPUP_OFFSET = 20;
 
 	IPreferenceStore prefs;
 	long remindDelay = -1L;
 	IPropertyChangeListener prefListener;
 	WorkbenchJob remindJob;
 	boolean downloaded;
-	Composite dialogArea;
 	Link remindLink;
-	MouseListener clickListener;
 
 	public AutomaticUpdatesPopup(Shell parentShell, boolean alreadyDownloaded, IPreferenceStore prefs) {
-		super(parentShell, PopupDialog.INFOPOPUPRESIZE_SHELLSTYLE | SWT.MODELESS, false, true, true, false, false,
-				AutomaticUpdateMessages.AutomaticUpdatesPopup_UpdatesAvailableTitle, null);
+		super(parentShell, alreadyDownloaded ? AutomaticUpdateMessages.AutomaticUpdatesPopup_ClickToReviewDownloaded
+				: AutomaticUpdateMessages.AutomaticUpdatesPopup_ClickToReviewNotDownloaded);
 		downloaded = alreadyDownloaded;
 		this.prefs = prefs;
 		remindDelay = computeRemindDelay();
-		clickListener = MouseListener
-				.mouseDownAdapter(e -> AutomaticUpdatePlugin.getDefault().getAutomaticUpdater().launchUpdate());
 	}
 
 	@Override
-	protected Control createDialogArea(Composite parent) {
-		dialogArea = new Composite(parent, SWT.NONE);
-		dialogArea.setLayoutData(new GridData(GridData.FILL_BOTH));
-		GridLayout layout = new GridLayout();
-		layout.numColumns = 1;
-		dialogArea.setLayout(layout);
-		dialogArea.addMouseListener(clickListener);
-
-		// The "click to update" label
-		Label infoLabel = new Label(dialogArea, SWT.NONE);
-		if (downloaded)
-			infoLabel.setText(AutomaticUpdateMessages.AutomaticUpdatesPopup_ClickToReviewDownloaded);
-		else
-			infoLabel.setText(AutomaticUpdateMessages.AutomaticUpdatesPopup_ClickToReviewNotDownloaded);
-		infoLabel.setLayoutData(new GridData(GridData.FILL_BOTH));
-		infoLabel.addMouseListener(clickListener);
-
-		createRemindSection(dialogArea);
-
-		return dialogArea;
-
+	protected Composite createDialogArea(Composite parent) {
+		Composite result = super.createDialogArea(parent);
+		createRemindSection(result);
+		return result;
 	}
 
 	private void createRemindSection(Composite parent) {
@@ -204,31 +176,6 @@ public class AutomaticUpdatesPopup extends PopupDialog {
 		}
 	}
 
-	@Override
-	protected void configureShell(Shell newShell) {
-		super.configureShell(newShell);
-		newShell.setText(AutomaticUpdateMessages.AutomaticUpdatesPopup_UpdatesAvailableTitle);
-	}
-
-	@Override
-	protected Point getInitialLocation(Point initialSize) {
-		Shell parent = getParentShell();
-		Point parentSize, parentLocation;
-
-		if (parent != null) {
-			parentSize = parent.getSize();
-			parentLocation = parent.getLocation();
-		} else {
-			Rectangle bounds = getShell().getDisplay().getBounds();
-			parentSize = new Point(bounds.width, bounds.height);
-			parentLocation = new Point(0, 0);
-		}
-		// We have to take parent location into account because SWT considers all
-		// shell locations to be in display coordinates, even if the shell is parented.
-		return new Point(parentSize.x - initialSize.x + parentLocation.x - POPUP_OFFSET,
-				parentSize.y - initialSize.y + parentLocation.y - POPUP_OFFSET);
-	}
-
 	void handlePreferenceChange(PropertyChangeEvent event) {
 		if (PreferenceConstants.PREF_REMIND_SCHEDULE.equals(event.getProperty())) {
 			// Reminders turned on
@@ -253,42 +200,6 @@ public class AutomaticUpdatesPopup extends PopupDialog {
 			computeRemindDelay();
 			scheduleRemindJob();
 		}
-	}
-
-	/*
-	 * Overridden so that clicking in the title menu area closes the dialog. Also
-	 * creates a close box menu in the title area.
-	 */
-	@Override
-	protected Control createTitleMenuArea(Composite parent) {
-		Composite titleComposite = (Composite) super.createTitleMenuArea(parent);
-		titleComposite.addMouseListener(clickListener);
-
-		ToolBar toolBar = new ToolBar(titleComposite, SWT.FLAT);
-		ToolItem closeButton = new ToolItem(toolBar, SWT.PUSH, 0);
-
-		GridDataFactory.fillDefaults().align(SWT.END, SWT.CENTER).applyTo(toolBar);
-		closeButton.setImage(
-				AutomaticUpdatePlugin.getDefault().getImageRegistry().get((AutomaticUpdatePlugin.IMG_TOOL_CLOSE)));
-		closeButton.setHotImage(
-				AutomaticUpdatePlugin.getDefault().getImageRegistry().get((AutomaticUpdatePlugin.IMG_TOOL_CLOSE_HOT)));
-		closeButton.addSelectionListener(SelectionListener.widgetSelectedAdapter(e -> close()));
-		// See https://bugs.eclipse.org/bugs/show_bug.cgi?id=177183
-		toolBar.addMouseListener(MouseListener.mouseDownAdapter(e -> close()));
-		return titleComposite;
-	}
-
-	/*
-	 * Overridden to adjust the span of the title label. Reachy, reachy....
-	 */
-	@Override
-	protected Control createTitleControl(Composite parent) {
-		Control control = super.createTitleControl(parent);
-		Object data = control.getLayoutData();
-		if (data instanceof GridData) {
-			((GridData) data).horizontalSpan = 1;
-		}
-		return control;
 	}
 
 	public static String getElapsedTimeString(String elapsedTimeKey) {
