50589248afa97451c947cb15efbf5eb23f1dd1ad
Hannes Wellmann <wellmann.hannes1@gmx.net>
2023-09-02 15:17:44 +0200

    Clean-up IQueryResult and its implementations

--- bundles/org.eclipse.equinox.p2.metadata/src/org/eclipse/equinox/p2/query/IQueryResult.java
+++ bundles/org.eclipse.equinox.p2.metadata/src/org/eclipse/equinox/p2/query/IQueryResult.java
@@ -18,17 +18,18 @@ import java.util.Iterator;
 import java.util.Set;
 
 /**
- * An IQueryResult represents the results of a query.  
+ * An IQueryResult represents the results of a query.
  * 
  * @since 2.0
  */
 public interface IQueryResult<T> extends IQueryable<T>, Iterable<T> {
 	/**
-	 * Returns whether this QueryResult  is empty.
-	 * @return <code>true</code> if this QueryResult has accepted any results,
-	 * and <code>false</code> otherwise.
+	 * Returns whether this QueryResult is empty.
+	 * 
+	 * @return <code>true</code> if this QueryResult has accepted any results, and
+	 *         <code>false</code> otherwise.
 	 */
-	public boolean isEmpty();
+	boolean isEmpty();
 
 	/**
 	 * Returns an iterator on the collected objects.
@@ -36,7 +37,7 @@ public interface IQueryResult<T> extends IQueryable<T>, Iterable<T> {
 	 * @return an iterator of the collected objects.
 	 */
 	@Override
-	public Iterator<T> iterator();
+	Iterator<T> iterator();
 
 	/**
 	 * Returns the collected objects as an array
@@ -46,18 +47,20 @@ public interface IQueryResult<T> extends IQueryable<T>, Iterable<T> {
 	 * @throws ArrayStoreException the runtime type of the specified array is
 	 *         not a super-type of the runtime type of every collected object
 	 */
-	public T[] toArray(Class<T> clazz);
+	T[] toArray(Class<T> clazz);
 
 	/**
-	 * Creates a new Set copy with the contents of this query result. The
-	 * copy can be altered without any side effects on its origin.
+	 * Creates a new Set copy with the contents of this query result. The copy can
+	 * be altered without any side effects on its origin.
+	 * 
 	 * @return A detached copy of the result.
 	 */
-	public Set<T> toSet();
+	Set<T> toSet();
 
 	/**
 	 * Returns a Set backed by this query result. The set is immutable.
+	 * 
 	 * @return A Set backed by this query result.
 	 */
-	public Set<T> toUnmodifiableSet();
+	Set<T> toUnmodifiableSet();
 }
