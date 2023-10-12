2b1bbe674023cb7a8ef01cfe2d2b7af6dbf4310b
Ed Merks <ed.merks@gmail.com>
2023-10-12 09:07:35 +0200

    Prevent trust dialogs from being hidden behind the application window
    Use SWT.ON_TOP for the shell in order to keep the dialogs visible,
    because generally the user cannot proceed with their activity
    (update/install) until the dialog is completed.
    
    A modal dialog is avoided because these dialogs are asynchronously
    opened from a background job, which blocks, so there is no knowing what
    UI activity might currently be under way.
    
    https://github.com/eclipse-oomph/oomph/issues/11

--- bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/p2/ui/dialogs/TrustCertificateDialog.java
+++ bundles/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/p2/ui/dialogs/TrustCertificateDialog.java
@@ -82,7 +82,7 @@ public class TrustCertificateDialog extends SelectionDialog {
 	public TrustCertificateDialog(Shell parentShell, Object input) {
 		super(parentShell);
 
-		setShellStyle(SWT.DIALOG_TRIM | SWT.MODELESS | SWT.RESIZE | SWT.MAX | getDefaultOrientation());
+		setShellStyle(SWT.DIALOG_TRIM | SWT.MODELESS | SWT.RESIZE | SWT.MAX | SWT.ON_TOP | getDefaultOrientation());
 
 		if (input instanceof TreeNode[]) {
 			init(null, (TreeNode[]) input);
