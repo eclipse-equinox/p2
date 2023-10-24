61950f6f67b6b0d5a15d5b27fe7f11dc394741d0
Hannes Wellmann <wellmann.hannes1@gmx.net>
2023-10-24 23:13:26 +0200

    Revert "Add IInstallableUnitQueryable interface to allow easier adaption"
    This reverts commit 9005901168a05471b8ceb94b977b60d7b045c204,
    respectively https://github.com/eclipse-equinox/p2/pull/348

--- bundles/org.eclipse.equinox.p2.repository/src/org/eclipse/equinox/p2/repository/metadata/IMetadataRepository.java
+++ bundles/org.eclipse.equinox.p2.repository/src/org/eclipse/equinox/p2/repository/metadata/IMetadataRepository.java
@@ -18,7 +18,6 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.equinox.p2.core.IPool;
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
-import org.eclipse.equinox.p2.metadata.IInstallableUnitQueryable;
 import org.eclipse.equinox.p2.repository.*;
 import org.eclipse.equinox.p2.repository.metadata.spi.AbstractMetadataRepository;
 
@@ -32,7 +31,7 @@ import org.eclipse.equinox.p2.repository.metadata.spi.AbstractMetadataRepository
  * @noimplement This interface is not intended to be implemented by clients. Instead subclass {@link AbstractMetadataRepository}
  * @since 2.0
  */
-public interface IMetadataRepository extends IRepository<IInstallableUnit>, IInstallableUnitQueryable {
+public interface IMetadataRepository extends IRepository<IInstallableUnit> {
 
 	/**
 	 * Add the given installable units to this repository.
