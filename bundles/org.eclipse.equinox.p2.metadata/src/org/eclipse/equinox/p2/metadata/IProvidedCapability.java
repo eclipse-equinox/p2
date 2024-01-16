27d77a02ca881bc5b76b69bd0ab2fc2257080048
Christoph Läubrich <laeubi@laeubi-soft.de>
2024-01-16 12:40:07 +0100

    Remove @noreference from IProvidedCapability

--- bundles/org.eclipse.equinox.p2.metadata/src/org/eclipse/equinox/p2/metadata/IProvidedCapability.java
+++ bundles/org.eclipse.equinox.p2.metadata/src/org/eclipse/equinox/p2/metadata/IProvidedCapability.java
@@ -20,12 +20,12 @@ import java.util.Map;
 import org.eclipse.equinox.p2.metadata.expression.IFilterExpression;
 
 /**
- * Describes a capability that is exposed by an installable unit. These capabilities
- * can satisfy the dependencies of other installable units, causing the unit
- * providing the dependency to be installed.
+ * Describes a capability that is exposed by an installable unit. These
+ * capabilities can satisfy the dependencies of other installable units, causing
+ * the unit providing the dependency to be installed.
  * <p>
- * Instances of this class are handle objects and do not necessarily
- * reflect entities that exist in any particular profile or repository. These handle 
+ * Instances of this class are handle objects and do not necessarily reflect
+ * entities that exist in any particular profile or repository. These handle
  * objects can be created using {@link MetadataFactory}.
  * </p>
  * 
@@ -38,7 +38,8 @@ public interface IProvidedCapability {
 	/**
 	 * The name of the property under which the capability version is stored.
 	 * 
-	 * Can be used with {@link #getProperties()}. The same value can be obtained with {@link #getVersion()}
+	 * Can be used with {@link #getProperties()}. The same value can be obtained
+	 * with {@link #getVersion()}
 	 * 
 	 * @since 2.4
 	 */
@@ -47,32 +48,34 @@ public interface IProvidedCapability {
 	/**
 	 * 
 	 * @return String the namespace of this capability.
-	 * @noreference This method is not intended to be referenced by clients.
+	 * @since 2.9
 	 */
 	String getNamespace();
 
 	/**
 	 * 
-	 * @return String the attribute stored under a key equal to the {@link #getNamespace()} attribute of this capability.
-	 * @noreference This method is not intended to be referenced by clients.
+	 * @return String the attribute stored under a key equal to the
+	 *         {@link #getNamespace()} attribute of this capability.
+	 * @since 2.9
 	 */
 	String getName();
 
 	/**
 	 * 
-	 * @return String the special {@link #PROPERTY_VERSION} attribute of this capability.
-	 * @noreference This method is not intended to be referenced by clients.
+	 * @return String the special {@link #PROPERTY_VERSION} attribute of this
+	 *         capability.
+	 * @since 2.9
 	 */
 	Version getVersion();
 
 	/**
 	 * A full description of this capability including the name and the version.
 	 * <p>
-	 * Such a description can be used to match this capability with an {@link IFilterExpression LDAP filter} for example.
+	 * Such a description can be used to match this capability with an
+	 * {@link IFilterExpression LDAP filter} for example.
 	 * 
 	 * @return An unmodifiable map
-	 * @noreference This method is not intended to be referenced by clients.
-	 * @since 2.4
+	 * @since 2.9
 	 */
 	Map<String, Object> getProperties();
 
@@ -81,10 +84,11 @@ public interface IProvidedCapability {
 	 * 
 	 * This method returns <i>true</i> if:
 	 * <ul>
-	 *  <li>Both this object and the given object are of type IProvidedCapability</li>
-	 *  <li>The result of {@link #getNamespace()} on both objects are equal</li>
-	 *  <li>The result of {@link #getProperties()} on both objects are equal</li>
-	 * </ul> 
+	 * <li>Both this object and the given object are of type
+	 * IProvidedCapability</li>
+	 * <li>The result of {@link #getNamespace()} on both objects are equal</li>
+	 * <li>The result of {@link #getProperties()} on both objects are equal</li>
+	 * </ul>
 	 */
 	@Override
 	boolean equals(Object other);
