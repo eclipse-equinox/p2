dd5abe60dd913635a818abd5af9b45cd6066bf90
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-01-05 19:38:51 +0100

    Migrate MetadataRepositoryComponent to DS annotations and property type

--- bundles/org.eclipse.equinox.p2.metadata.repository/src/org/eclipse/equinox/internal/p2/metadata/repository/MetadataRepositoryComponent.java
+++ bundles/org.eclipse.equinox.p2.metadata.repository/src/org/eclipse/equinox/internal/p2/metadata/repository/MetadataRepositoryComponent.java
@@ -7,7 +7,7 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Sonatype Inc - ongoing development
@@ -15,12 +15,16 @@
 package org.eclipse.equinox.internal.p2.metadata.repository;
 
 import org.eclipse.equinox.p2.core.IProvisioningAgent;
+import org.eclipse.equinox.p2.core.spi.AgentServiceName;
 import org.eclipse.equinox.p2.core.spi.IAgentServiceFactory;
 import org.eclipse.equinox.p2.repository.metadata.IMetadataRepositoryManager;
+import org.osgi.service.component.annotations.Component;
 
 /**
  * Service factory for creating {@link IMetadataRepositoryManager} instances.
  */
+@Component(service = IAgentServiceFactory.class, name = "org.eclipse.equinox.p2.metadata.repository")
+@AgentServiceName(IMetadataRepositoryManager.class)
 public class MetadataRepositoryComponent implements IAgentServiceFactory {
 
 	@Override
