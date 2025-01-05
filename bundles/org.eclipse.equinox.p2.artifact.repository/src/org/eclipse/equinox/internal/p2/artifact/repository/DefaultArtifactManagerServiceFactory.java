823573bc17e2735732771efbd5dcad98dd732e23
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-01-05 09:22:52 +0100

    Add an ArtifactManager to allows unified managing of artifacts
    Caching can greatly improve performance, basically P2 has two concepts
    of where caching can occur, the transport and artifacts that can
    originate from different sources. Currently there is only a CacheManger
    for handling the first case and nothing for the second, maybe even more
    important case. Because of this there are different approaches out in
    the wild e.g. PDE Bundle Pools, Oomph shared P2 pools, and Tycho is
    using its own cache in the m2 repository, but all of them suffer from
    the fact that there is no real convenient entry point so different
    things needs to be enhanced an managed.
    
    This now adds a new abstraction service the ArtifactManager that allows
    to intercept the process of downloading a logical artifact from a given
    location and possibly providing an equivalent one that is already
    present locally. There is also a default implementation that simply
    delegates to the transport directly as before,

---
+++ bundles/org.eclipse.equinox.p2.artifact.repository/src/org/eclipse/equinox/internal/p2/artifact/repository/DefaultArtifactManagerServiceFactory.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ *  Copyright (c) 2025 Christoph Läubrich and others.
+ *
+ *  This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License 2.0
+ *  which accompanies this distribution, and is available at
+ *  https://www.eclipse.org/legal/epl-2.0/
+ *
+ *  SPDX-License-Identifier: EPL-2.0
+ *
+ *  Contributors:
+ *     Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.equinox.internal.p2.artifact.repository;
+
+import java.io.OutputStream;
+import java.net.URI;
+import org.eclipse.core.runtime.*;
+import org.eclipse.equinox.internal.p2.repository.Transport;
+import org.eclipse.equinox.p2.core.IProvisioningAgent;
+import org.eclipse.equinox.p2.core.spi.AgentServicename;
+import org.eclipse.equinox.p2.core.spi.IAgentServiceFactory;
+import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * The default implementation of a {@link ArtifactManager} simply delegates to
+ * the transport.
+ */
+@Component(service = IAgentServiceFactory.class)
+@AgentServicename(ArtifactManager.class)
+public class DefaultArtifactManagerServiceFactory implements IAgentServiceFactory {
+
+	@Override
+	public Object createService(IProvisioningAgent agent) {
+		return new DefaultArtifactManager(agent);
+	}
+
+	private static final class DefaultArtifactManager implements ArtifactManager {
+		private IProvisioningAgent agent;
+
+		public DefaultArtifactManager(IProvisioningAgent agent) {
+			this.agent = agent;
+		}
+
+		@Override
+		public IStatus getArtifact(URI source, OutputStream target, IArtifactDescriptor descriptor,
+				IProgressMonitor monitor) {
+			Transport transport = agent.getService(Transport.class);
+			if (transport == null) {
+				return Status.CANCEL_STATUS;
+			}
+			return transport.downloadArtifact(source, target, descriptor, monitor);
+		}
+	}
+}
